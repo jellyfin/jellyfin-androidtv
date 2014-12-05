fb5c6d78d5057eb86b8d8a10398bdafb3b1ab1ee
Eric Reed <ebr@mediabrowser3.com>
2014-12-05 15:32:05 -0500

    Basic Item rows

---
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/StdItemQuery.java
@@ -0,0 +1,15 @@
+package tv.mediabrowser.mediabrowsertv;
+
+import mediabrowser.model.querying.ItemFields;
+import mediabrowser.model.querying.ItemQuery;
+
+/**
+ * Created by Eric on 12/5/2014.
+ */
+public class StdItemQuery extends ItemQuery {
+
+    public StdItemQuery() {
+        setUserId(TvApp.getApplication().getCurrentUser().getId());
+        setFields(new ItemFields[] {ItemFields.PrimaryImageAspectRatio});
+    }
+}
