89c77f69b31fc0a33d49bca5bd543eb60b173b81
Eric Reed <ebr@mediabrowser3.com>
2014-12-16 20:45:48 -0500

    Only show resume and latest on some types

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/GenericFolderFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/GenericFolderFragment.java
@@ -2,6 +2,8 @@ package tv.mediabrowser.mediabrowsertv;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+
 import mediabrowser.model.dto.BaseItemDto;
 import mediabrowser.model.entities.SortOrder;
 import mediabrowser.model.querying.ItemFields;
@@ -20,29 +22,38 @@ public class GenericFolderFragment extends BrowseFolderFragment {
 
     }
 
+    private static String[] showSpecialViewTypes = new String[] {"CollectionFolder", "Folder", "Channel", "ChannelFolderItem"};
+
     @Override
     protected void setupQueries(IRowLoader rowLoader) {
 
-        StdItemQuery resume = new StdItemQuery();
-        resume.setParentId(mFolder.getId());
-        resume.setLimit(50);
-        resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
-        resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
-        resume.setSortOrder(SortOrder.Descending);
-        mRows.add(new BrowseRowDef("Continue Watching", resume, 0));
+        boolean showSpecialViews = Arrays.asList(showSpecialViewTypes).contains(mFolder.getType());
 
-        StdItemQuery latest = new StdItemQuery();
-        latest.setParentId(mFolder.getId());
-        latest.setLimit(50);
-        latest.setFilters(new ItemFilter[]{ItemFilter.IsUnplayed});
-        latest.setSortBy(new String[]{ItemSortBy.DateCreated});
-        latest.setSortOrder(SortOrder.Descending);
-        mRows.add(new BrowseRowDef("Latest Additions", latest, 0));
+        if (showSpecialViews) {
+            StdItemQuery resume = new StdItemQuery();
+            resume.setParentId(mFolder.getId());
+            resume.setLimit(50);
+            resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
+            resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
+            resume.setSortOrder(SortOrder.Descending);
+            mRows.add(new BrowseRowDef("Continue Watching", resume, 0));
+
+            StdItemQuery latest = new StdItemQuery();
+            latest.setParentId(mFolder.getId());
+            latest.setLimit(50);
+            latest.setFilters(new ItemFilter[]{ItemFilter.IsUnplayed});
+            latest.setSortBy(new String[]{ItemSortBy.DateCreated});
+            latest.setSortOrder(SortOrder.Descending);
+            mRows.add(new BrowseRowDef("Latest Additions", latest, 0));
+
+        }
 
         StdItemQuery byName = new StdItemQuery();
         byName.setParentId(mFolder.getId());
         byName.setSortBy(new String[]{ItemSortBy.SortName});
         mRows.add(new BrowseRowDef("By Name", byName, 100));
+
+        if (mRows.size() < 2) setHeadersState(HEADERS_DISABLED);
 
         rowLoader.loadRows(mRows);
 
