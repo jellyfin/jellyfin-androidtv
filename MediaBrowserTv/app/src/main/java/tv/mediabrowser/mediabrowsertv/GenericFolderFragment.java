244f42fb518bcc3d9fb327e226213eca83b70ab4
Eric Reed <ebr@mediabrowser3.com>
2015-01-12 16:41:33 -0500

    Fix crash on empty folders

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/GenericFolderFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/GenericFolderFragment.java
@@ -27,36 +27,42 @@ public class GenericFolderFragment extends BrowseFolderFragment {
     @Override
     protected void setupQueries(IRowLoader rowLoader) {
 
-        boolean showSpecialViews = Arrays.asList(showSpecialViewTypes).contains(mFolder.getType());
+        if (mFolder.getChildCount() > 0) {
+            boolean showSpecialViews = Arrays.asList(showSpecialViewTypes).contains(mFolder.getType());
 
-        if (showSpecialViews) {
-            StdItemQuery resume = new StdItemQuery();
-            resume.setParentId(mFolder.getId());
-            resume.setLimit(50);
-            resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
-            resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
-            resume.setSortOrder(SortOrder.Descending);
-            mRows.add(new BrowseRowDef("Continue Watching", resume, 0));
+            if (showSpecialViews) {
+                StdItemQuery resume = new StdItemQuery();
+                resume.setParentId(mFolder.getId());
+                resume.setLimit(50);
+                resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
+                resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
+                resume.setSortOrder(SortOrder.Descending);
+                mRows.add(new BrowseRowDef("Continue Watching", resume, 0));
 
-            StdItemQuery latest = new StdItemQuery();
-            latest.setParentId(mFolder.getId());
-            latest.setLimit(50);
-            latest.setFilters(new ItemFilter[]{ItemFilter.IsUnplayed});
-            latest.setSortBy(new String[]{ItemSortBy.DateCreated});
-            latest.setSortOrder(SortOrder.Descending);
-            mRows.add(new BrowseRowDef("Latest Additions", latest, 0));
+                StdItemQuery latest = new StdItemQuery();
+                latest.setParentId(mFolder.getId());
+                latest.setLimit(50);
+                latest.setFilters(new ItemFilter[]{ItemFilter.IsUnplayed});
+                latest.setSortBy(new String[]{ItemSortBy.DateCreated});
+                latest.setSortOrder(SortOrder.Descending);
+                mRows.add(new BrowseRowDef("Latest Additions", latest, 0));
+
+            }
+
+
+            StdItemQuery byName = new StdItemQuery();
+            byName.setParentId(mFolder.getId());
+            byName.setSortBy(new String[]{ItemSortBy.SortName});
+            mRows.add(new BrowseRowDef("By Name", byName, 100));
+
+            if (mRows.size() < 2) setHeadersState(HEADERS_DISABLED);
+
+            rowLoader.loadRows(mRows);
 
         }
-
-        StdItemQuery byName = new StdItemQuery();
-        byName.setParentId(mFolder.getId());
-        byName.setSortBy(new String[]{ItemSortBy.SortName});
-        mRows.add(new BrowseRowDef("By Name", byName, 100));
-
-        if (mRows.size() < 2) setHeadersState(HEADERS_DISABLED);
-
-        rowLoader.loadRows(mRows);
-
+        else {
+            setHeadersState(HEADERS_DISABLED);
+        }
     }
 
 
