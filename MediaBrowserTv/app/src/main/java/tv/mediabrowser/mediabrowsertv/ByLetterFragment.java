ff206455dd2d8cdf0c72e2fbfbb9e28332c4008c
Eric Reed <ebr@mediabrowser3.com>
2015-01-16 15:56:48 -0500

    Update view icons

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/ByLetterFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/ByLetterFragment.java
@@ -12,11 +12,13 @@ import mediabrowser.model.querying.ItemSortBy;
  * Created by Eric on 12/4/2014.
  */
 public class ByLetterFragment extends BrowseFolderFragment {
+    private String includeType = "Movie";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+        includeType = getActivity().getIntent().getStringExtra("IncludeType");
+        TvApp.getApplication().getLogger().Debug("Item type: "+includeType);
         super.onActivityCreated(savedInstanceState);
-
     }
 
     private static String letters = TvApp.getApplication().getResources().getString(R.string.byletter_letters);
@@ -26,21 +28,23 @@ public class ByLetterFragment extends BrowseFolderFragment {
 
         if (mFolder.getChildCount() > 0) {
             //First add a '#' item
-            StdItemQuery byName = new StdItemQuery();
-            byName.setParentId(mFolder.getId());
-            byName.setSortBy(new String[]{ItemSortBy.SortName});
-            byName.setNameLessThan("A");
-            byName.setRecursive(true);
-            mRows.add(new BrowseRowDef("#", byName, 100));
+            StdItemQuery numbers = new StdItemQuery();
+            numbers.setParentId(mFolder.getId());
+            numbers.setSortBy(new String[]{ItemSortBy.SortName});
+            if (includeType != null) numbers.setIncludeItemTypes(new String[]{includeType});
+            numbers.setNameLessThan("A");
+            numbers.setRecursive(true);
+            mRows.add(new BrowseRowDef("#", numbers, 100));
 
             //Then all the defined letters
             for (Character letter : letters.toCharArray()) {
-                byName = new StdItemQuery();
-                byName.setParentId(mFolder.getId());
-                byName.setSortBy(new String[]{ItemSortBy.SortName});
-                byName.setNameStartsWith(letter.toString());
-                byName.setRecursive(true);
-                mRows.add(new BrowseRowDef(letter.toString(), byName, 100));
+                StdItemQuery letterQuery = new StdItemQuery();
+                letterQuery.setParentId(mFolder.getId());
+                letterQuery.setSortBy(new String[]{ItemSortBy.SortName});
+                if (includeType != null) letterQuery.setIncludeItemTypes(new String[]{includeType});
+                letterQuery.setNameStartsWith(letter.toString());
+                letterQuery.setRecursive(true);
+                mRows.add(new BrowseRowDef(letter.toString(), letterQuery, 100));
             }
 
             if (mRows.size() < 2) setHeadersState(HEADERS_DISABLED);
