ec02092139caa65803aa3567a81747322ec9666e
Eric Reed <ebr@mediabrowser3.com>
2015-01-20 20:50:41 -0500

    Auto login configuration

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/SettingsFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/SettingsFragment.java
@@ -4,6 +4,7 @@ package tv.mediabrowser.mediabrowsertv;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -11,6 +12,10 @@ import android.preference.PreferenceGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.acra.ACRA;
+
+import java.io.IOException;
 
 
 /**
@@ -24,12 +29,12 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
             Preference preference = getPreferenceScreen().getPreference(i);
             if (preference instanceof PreferenceGroup) {
@@ -52,13 +57,44 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        //If logon setting is changed to this user, save credentials
+        if (key.equals("pref_login_behavior")) {
+            ListPreference listPreference = (ListPreference) findPreference(key);
+            if (listPreference.getValue().equals("1")) {
+                try {
+                    Utils.SaveLoginCredentials(new LogonCredentials(TvApp.getApplication().getApiClient().getServerInfo(), TvApp.getApplication().getCurrentUser()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    ACRA.getErrorReporter().handleException(e);
+                }
+            }
+        }
+
         updatePreference(findPreference(key));
     }
 
     private void updatePreference(Preference preference) {
         if (preference instanceof ListPreference) {
             ListPreference listPreference = (ListPreference) preference;
-            listPreference.setSummary(listPreference.getEntry());
+            if (listPreference.getKey().equals("pref_login_behavior")) {
+                CheckBoxPreference pwPrompt = (CheckBoxPreference) findPreference("pref_auto_pw_prompt");
+                pwPrompt.setEnabled(TvApp.getApplication().getConfiguredAutoCredentials().getUserDto().getHasPassword());
+                String extra = "";
+                if (listPreference.getValue().equals("1")) {
+                    if (!TvApp.getApplication().getConfiguredAutoCredentials().getUserDto().getId().equals(TvApp.getApplication().getCurrentUser().getId())) {
+                        listPreference.setEnabled(false);
+                        pwPrompt.setEnabled(false);
+                        extra = " (login as "+TvApp.getApplication().getConfiguredAutoCredentials().getUserDto().getName()+" to change)";
+                    }
+                    listPreference.setSummary("Login as " + TvApp.getApplication().getConfiguredAutoCredentials().getUserDto().getName() + extra);
+                } else {
+                    listPreference.setSummary(listPreference.getEntry());
+                    pwPrompt.setEnabled(false);
+                }
+            } else {
+                listPreference.setSummary(listPreference.getEntry());
+            }
         }
     }
 }
