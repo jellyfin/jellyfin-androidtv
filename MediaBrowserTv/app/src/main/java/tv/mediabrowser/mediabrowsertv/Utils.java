b8b30f6cd3bcbfeda3824968ecb7a47f36593a6e
Eric Reed <ebr@mediabrowser3.com>
2015-02-10 13:26:06 -0500

    Fix npe with bad episode data

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/Utils.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/Utils.java
@@ -329,25 +329,30 @@ public class Utils {
             case "Episode":
                 items.add(serializer.SerializeToString(mainItem));
                 //add subsequent episodes
-                query.setParentId(mainItem.getSeasonId());
-                query.setIsMissing(false);
-                query.setIsVirtualUnaired(false);
-                query.setMinIndexNumber(mainItem.getIndexNumber() + 1);
-                query.setSortBy(new String[] {ItemSortBy.SortName});
-                query.setIncludeItemTypes(new String[]{"Episode"});
-                query.setFields(new ItemFields[] {ItemFields.MediaSources, ItemFields.Path, ItemFields.PrimaryImageAspectRatio});
-                query.setUserId(TvApp.getApplication().getCurrentUser().getId());
-                TvApp.getApplication().getApiClient().GetItemsAsync(query, new Response<ItemsResult>() {
-                    @Override
-                    public void onResponse(ItemsResult response) {
-                        for (BaseItemDto item : response.getItems()) {
-                            if (item.getIndexNumber() > mainItem.getIndexNumber()) {
-                                items.add(serializer.SerializeToString(item));
+                if (mainItem.getSeasonId() != null && mainItem.getIndexNumber() != null) {
+                    query.setParentId(mainItem.getSeasonId());
+                    query.setIsMissing(false);
+                    query.setIsVirtualUnaired(false);
+                    query.setMinIndexNumber(mainItem.getIndexNumber() + 1);
+                    query.setSortBy(new String[] {ItemSortBy.SortName});
+                    query.setIncludeItemTypes(new String[]{"Episode"});
+                    query.setFields(new ItemFields[] {ItemFields.MediaSources, ItemFields.Path, ItemFields.PrimaryImageAspectRatio});
+                    query.setUserId(TvApp.getApplication().getCurrentUser().getId());
+                    TvApp.getApplication().getApiClient().GetItemsAsync(query, new Response<ItemsResult>() {
+                        @Override
+                        public void onResponse(ItemsResult response) {
+                            for (BaseItemDto item : response.getItems()) {
+                                if (item.getIndexNumber() > mainItem.getIndexNumber()) {
+                                    items.add(serializer.SerializeToString(item));
+                                }
                             }
+                            outerResponse.onResponse(items.toArray(new String[items.size()]));
                         }
-                        outerResponse.onResponse(items.toArray(new String[items.size()]));
-                    }
-                });
+                    });
+                } else {
+                    TvApp.getApplication().getLogger().Info("Unable to add subsequent episodes due to lack of season or episode data.");
+                    outerResponse.onResponse(items.toArray(new String[items.size()]));
+                }
                 break;
             case "Series":
             case "Season":
