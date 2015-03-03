91e10ad81459f83eef60367276a0c707cc90184e
Eric Reed <ebr@mediabrowser3.com>
2015-03-03 14:46:04 -0500

    Create paid and supporter variants

---
+++ MediaBrowserTv/app/src/supporter/java/AppValidator.java
@@ -0,0 +1,19 @@
+package tv.mediabrowser.mediabrowsertv;
+
+import android.app.Activity;
+import android.text.format.DateUtils;
+
+import mediabrowser.apiinteraction.Response;
+import mediabrowser.model.configuration.ServerConfiguration;
+import mediabrowser.model.entities.MBRegistrationRecord;
+import tv.mediabrowser.mediabrowsertv.util.Utils;
+
+/**
+ * Created by Eric on 3/3/2015.
+ */
+public class AppValidator {
+
+    public static void validate(final Activity activity) {
+        TvApp.getApplication().getLogger().Info("Supporter app");
+    }
+}
