e632b92ee1202c635fd6d49c5b10e149376a1bd0
Eric Reed <ebr@mediabrowser3.com>
2015-03-13 14:50:32 -0400

    Always create new recs

--- MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/integration/RecommendationManager.java
+++ MediaBrowserTv/app/src/main/java/tv/mediabrowser/mediabrowsertv/integration/RecommendationManager.java
@@ -73,6 +73,9 @@ public class RecommendationManager {
             TvApp.getApplication().getLogger().Info("Recommendations re-set for user "+TvApp.getApplication().getCurrentUser().getName());
             return false;
         }
+        
+        createAll();
+        TvApp.getApplication().getLogger().Info("Recommendations re-created for user "+TvApp.getApplication().getCurrentUser().getName());
 
         return true;
     }
