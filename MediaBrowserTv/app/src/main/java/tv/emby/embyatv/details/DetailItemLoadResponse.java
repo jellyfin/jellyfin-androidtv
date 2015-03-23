ae1d14181072ee71af4b55959fc1790060726c8f
Eric Reed <ebr@mediabrowser3.com>
2015-03-23 14:48:22 -0400

    Eliminate details closure

---
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/details/DetailItemLoadResponse.java
@@ -0,0 +1,29 @@
+package tv.emby.embyatv.details;
+
+import mediabrowser.apiinteraction.Response;
+import mediabrowser.model.dto.BaseItemDto;
+import tv.emby.embyatv.TvApp;
+import tv.emby.embyatv.util.Utils;
+
+/**
+ * Created by Eric on 3/23/2015.
+ */
+public class DetailItemLoadResponse extends Response<BaseItemDto> {
+    private Response<BaseItemDto> response;
+    private BaseItemDetailsFragment detailFragment;
+
+    public DetailItemLoadResponse(BaseItemDetailsFragment fragment) {
+        this.detailFragment = fragment;
+    }
+
+    @Override
+    public void onResponse(BaseItemDto response) {
+        detailFragment.setBaseItem(response);
+    }
+
+    @Override
+    public void onError(Exception exception) {
+        TvApp.getApplication().getLogger().ErrorException("Error retrieving full object", exception);
+        Utils.reportError(detailFragment.getActivity(), "Error retrieving item");
+    }
+}
