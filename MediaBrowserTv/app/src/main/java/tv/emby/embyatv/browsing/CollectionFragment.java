2078107aa7f7462f590df5cb1c1121a1edc67be6
Eric Reed <ebr@mediabrowser3.com>
2015-04-01 10:34:44 -0400

    Fix crash on empty collections

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/CollectionFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/CollectionFragment.java
@@ -19,24 +19,27 @@ public class CollectionFragment extends BrowseFolderFragment {
     @Override
     protected void setupQueries(IRowLoader rowLoader) {
 
-        StdItemQuery movies = new StdItemQuery();
-        movies.setParentId(mFolder.getId());
-        movies.setIncludeItemTypes(new String[] {"Movie"});
-        mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_movies), movies, 100));
+        if (mFolder.getChildCount() > 0) {
+            StdItemQuery movies = new StdItemQuery();
+            movies.setParentId(mFolder.getId());
+            movies.setIncludeItemTypes(new String[]{"Movie"});
+            mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_movies), movies, 100));
 
-        StdItemQuery series = new StdItemQuery();
-        series.setParentId(mFolder.getId());
-        series.setIncludeItemTypes(new String[]{"Series"});
-        mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_tv_series), series, 100));
+            StdItemQuery series = new StdItemQuery();
+            series.setParentId(mFolder.getId());
+            series.setIncludeItemTypes(new String[]{"Series"});
+            mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_tv_series), series, 100));
 
-        StdItemQuery others = new StdItemQuery();
-        others.setParentId(mFolder.getId());
-        others.setExcludeItemTypes(new String[]{"Movie","Series"});
-        mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_other), others, 100));
+            StdItemQuery others = new StdItemQuery();
+            others.setParentId(mFolder.getId());
+            others.setExcludeItemTypes(new String[]{"Movie", "Series"});
+            mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_other), others, 100));
+
+
+            rowLoader.loadRows(mRows);
+        }
 
         setHeadersState(HEADERS_DISABLED);
-
-        rowLoader.loadRows(mRows);
 
     }
 
