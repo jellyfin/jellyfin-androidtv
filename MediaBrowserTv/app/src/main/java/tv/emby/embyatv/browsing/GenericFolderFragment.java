0524ce4eaa54258f7c99be41fc34abc25c77d3fc
Eric Reed <ebr@mediabrowser3.com>
2015-04-02 10:56:07 -0400

    Allow resume and latest for more types

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/GenericFolderFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/GenericFolderFragment.java
@@ -21,22 +21,24 @@ public class GenericFolderFragment extends BrowseFolderFragment {
 
     }
 
-    private static String[] showSpecialViewTypes = new String[] {"CollectionFolder", "Folder"};
+    private static String[] showSpecialViewTypes = new String[] {"CollectionFolder", "Folder", "UserView", "ChannelFolderItem"};
 
     @Override
     protected void setupQueries(IRowLoader rowLoader) {
 
         if (mFolder.getChildCount() > 0 || mFolder.getType().equals("Channel") || mFolder.getType().equals("ChannelFolderItem")) {
-            boolean showSpecialViews = Arrays.asList(showSpecialViewTypes).contains(mFolder.getType());
+            boolean showSpecialViews = Arrays.asList(showSpecialViewTypes).contains(mFolder.getType()) && !"channels".equals(mFolder.getCollectionType());
 
             if (showSpecialViews) {
-                StdItemQuery resume = new StdItemQuery();
-                resume.setParentId(mFolder.getId());
-                resume.setLimit(50);
-                resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
-                resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
-                resume.setSortOrder(SortOrder.Descending);
-                mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_continue_watching), resume, 0));
+                if (!mFolder.getType().equals("ChannelFolderItem")) {
+                    StdItemQuery resume = new StdItemQuery();
+                    resume.setParentId(mFolder.getId());
+                    resume.setLimit(50);
+                    resume.setFilters(new ItemFilter[]{ItemFilter.IsResumable});
+                    resume.setSortBy(new String[]{ItemSortBy.DatePlayed});
+                    resume.setSortOrder(SortOrder.Descending);
+                    mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_continue_watching), resume, 0));
+                }
 
                 StdItemQuery latest = new StdItemQuery();
                 latest.setParentId(mFolder.getId());
@@ -54,7 +56,7 @@ public class GenericFolderFragment extends BrowseFolderFragment {
             byName.setSortBy(new String[]{ItemSortBy.SortName});
             mRows.add(new BrowseRowDef(mApplication.getString(R.string.lbl_by_name), byName, 100));
 
-            if (mRows.size() < 2) setHeadersState(HEADERS_DISABLED);
+            if (mRows.size() < 3) setHeadersState(HEADERS_DISABLED);
 
             rowLoader.loadRows(mRows);
 
