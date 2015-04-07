57dc8318f2e3941f9ff7f3533e72086ac14f2d17
Eric Reed <ebr@mediabrowser3.com>
2015-04-07 18:30:15 -0400

    Stub out alternate search

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/search/SearchActivity.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/search/SearchActivity.java
@@ -1,6 +1,7 @@
 package tv.emby.embyatv.search;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 
 import tv.emby.embyatv.R;
@@ -10,6 +11,10 @@ public class SearchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        if (Build.VERSION.SDK_INT >= 21) {
+            setContentView(R.layout.activity_search);
+        } else {
+            setContentView(R.layout.activity_search_no_speech);
+        }
     }
 }
