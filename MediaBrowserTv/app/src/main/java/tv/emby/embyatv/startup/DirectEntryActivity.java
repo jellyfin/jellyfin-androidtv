7c7de2a97690d85bf2bcbaa9616dab8e41e46acc
Eric Reed <ebr@mediabrowser3.com>
2015-04-17 10:40:58 -0400

    Give direct entry a layout

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/startup/DirectEntryActivity.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/startup/DirectEntryActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import tv.emby.embyatv.R;
 import tv.emby.embyatv.TvApp;
 import tv.emby.embyatv.details.DetailsActivity;
 import tv.emby.embyatv.util.Utils;
@@ -15,6 +16,7 @@ public class DirectEntryActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_startup);
 
         if (TvApp.getApplication().getCurrentUser().getHasPassword()
                 && (!TvApp.getApplication().getIsAutoLoginConfigured()
