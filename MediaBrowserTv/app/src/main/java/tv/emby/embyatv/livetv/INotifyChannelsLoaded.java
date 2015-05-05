19a3911ac2239dd8b567babf420667f88066ae9c
Eric Reed <ebr@mediabrowser3.com>
2015-05-05 17:46:34 -0400

    Guide progress (focus problems)

---
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/livetv/INotifyChannelsLoaded.java
@@ -0,0 +1,8 @@
+package tv.emby.embyatv.livetv;
+
+/**
+ * Created by Eric on 5/5/2015.
+ */
+public interface INotifyChannelsLoaded {
+    public void notifyChannelsLoaded(int start, String[] channelIds);
+}
---
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/livetv/ProgramListAdapter.java
@@ -0,0 +1,75 @@
+package tv.emby.embyatv.livetv;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mediabrowser.model.livetv.ProgramInfoDto;
+import tv.emby.embyatv.TvApp;
+import tv.emby.embyatv.ui.ProgramGridCell;
+
+/**
+ * Created by Eric on 5/5/2015.
+ */
+public class ProgramListAdapter extends BaseAdapter {
+
+    Activity activity;
+    LinearLayout view;
+    List<List<ProgramInfoDto>> rows = new ArrayList<>();
+
+    public ProgramListAdapter(Activity activity, LinearLayout view) {
+        this.activity = activity;
+        this.view = view;
+    }
+
+    public void addRow(int ndx, List<ProgramInfoDto> programs) {
+        rows.add(ndx, programs);
+        view.addView(getView(ndx, null, view));
+        TvApp.getApplication().getLogger().Debug("Added "+programs.size()+ " programs at pos "+ndx);
+    }
+
+    @Override
+    public int getCount() {
+        return rows.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return rows.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null)
+            convertView = new LinearLayout(activity);
+
+        LinearLayout programRow = (LinearLayout) convertView;
+        programRow.removeAllViews();
+
+        for (ProgramInfoDto item : rows.get(position)) {
+            Long duration = item.getStartDate() != null && item.getEndDate() != null ? (item.getEndDate().getTime() - item.getStartDate().getTime()) / 60000 : 0;
+            //TvApp.getApplication().getLogger().Debug("Duration for "+item.getName()+" is "+duration.intValue());
+            if (duration > 0) {
+                ProgramGridCell program = new ProgramGridCell(activity, item);
+                program.setLayoutParams(new ViewGroup.LayoutParams(duration.intValue() * LiveTvGuideActivity.PIXELS_PER_MINUTE, LiveTvGuideActivity.ROW_HEIGHT));
+                program.setFocusable(true);
+
+                programRow.addView(program);
+
+            }
+
+        }
+
+        return convertView;
+    }
+}
