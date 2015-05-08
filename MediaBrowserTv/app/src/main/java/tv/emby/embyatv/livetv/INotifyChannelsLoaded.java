593920667ff60439eb57fbd2f798a8df7782ce9f
Eric Reed <ebr@mediabrowser3.com>
2015-05-08 11:35:40 -0400

    Guide paging

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/livetv/INotifyChannelsLoaded.java
+++
@@ -1,8 +0,0 @@
-package tv.emby.embyatv.livetv;
-
-/**
- * Created by Eric on 5/5/2015.
- */
-public interface INotifyChannelsLoaded {
-    public void notifyChannelsLoaded(int start, String[] channelIds);
-}
