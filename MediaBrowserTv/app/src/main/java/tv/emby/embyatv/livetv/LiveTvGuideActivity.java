62f6e7f4eb730bb3c5034edea694389761e2c185
Eric Reed <ebr@mediabrowser3.com>
2015-05-12 15:15:41 -0400

    Series recordings

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/livetv/LiveTvGuideActivity.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/livetv/LiveTvGuideActivity.java
@@ -1,7 +1,9 @@
 package tv.emby.embyatv.livetv;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
@@ -20,6 +22,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -31,6 +34,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -176,6 +180,8 @@ public class LiveTvGuideActivity extends BaseActivity {
 
         if (mDisplayProgramsTask != null) mDisplayProgramsTask.cancel(true);
         if (mDisplayChannelTask != null) mDisplayChannelTask.cancel(true);
+        if (mDetailPopup != null) mDetailPopup.dismiss();
+        if (mRecordPopup != null) mRecordPopup.dismiss();
     }
 
     private DetailPopup mDetailPopup;
@@ -214,7 +220,7 @@ public class LiveTvGuideActivity extends BaseActivity {
             mDSimilarRow = (LinearLayout) layout.findViewById(R.id.similarRow);
         }
 
-        public void setContent(ProgramInfoDto program) {
+        public void setContent(final ProgramInfoDto program) {
             mDTitle.setText(program.getName());
             mDSummary.setText(program.getOverview());
             if (mDSummary.getLineCount() < 2) {
@@ -291,11 +297,44 @@ public class LiveTvGuideActivity extends BaseActivity {
                         // cancel series button
                         Button cancel = new Button(mActivity);
                         cancel.setText(getString(R.string.lbl_cancel_series));
+                        cancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                new AlertDialog.Builder(mActivity)
+                                        .setTitle(getString(R.string.lbl_cancel_series))
+                                        .setMessage(getString(R.string.msg_cancel_entire_series))
+                                        .setNegativeButton(R.string.lbl_no, null)
+                                        .setPositiveButton(R.string.lbl_yes, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                TvApp.getApplication().getApiClient().CancelLiveTvSeriesTimerAsync(program.getSeriesTimerId(), new EmptyResponse() {
+                                                    @Override
+                                                    public void onResponse() {
+                                                        mSelectedProgramView.setRecSeriesIndicator(false);
+                                                        dismiss();
+                                                        Utils.showToast(mActivity, R.string.msg_recording_cancelled);
+                                                    }
+
+                                                    @Override
+                                                    public void onError(Exception ex) {
+                                                        Utils.showToast(mActivity, R.string.msg_unable_to_cancel);
+                                                    }
+                                                });
+                                            }
+                                        }).show();
+                            }
+                        });
                         mDButtonRow.addView(cancel);
                     }else {
                         // record series button
                         Button rec = new Button(mActivity);
                         rec.setText(getString(R.string.lbl_record_series));
+                        rec.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                showRecordingOptions(true);
+                            }
+                        });
                         mDButtonRow.addView(rec);
                     }
                 }
@@ -351,6 +390,7 @@ public class LiveTvGuideActivity extends BaseActivity {
     class RecordPopup {
         final int SERIES_HEIGHT = Utils.convertDpToPixel(TvApp.getApplication(), 540);
         final int NORMAL_HEIGHT = Utils.convertDpToPixel(TvApp.getApplication(), 400);
+        final List<String> DayValues = Arrays.asList("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 
         PopupWindow mPopup;
         String mProgramId;
@@ -360,10 +400,16 @@ public class LiveTvGuideActivity extends BaseActivity {
         TextView mDTitle;
         TextView mDSummary;
         LinearLayout mDTimeline;
+        View mSeriesOptions;
+        GridLayout mWeekdayOptions;
+        CheckBox[] mWeekdayChecks = new CheckBox[7];
         EditText mPrePadding;
         EditText mPostPadding;
         CheckBox mPreRequired;
         CheckBox mPostRequired;
+        CheckBox mOnlyNew;
+        CheckBox mAnyTime;
+        CheckBox mAnyChannel;
         Button mOkButton;
         Button mCancelButton;
 
@@ -385,6 +431,26 @@ public class LiveTvGuideActivity extends BaseActivity {
             mPreRequired = (CheckBox) layout.findViewById(R.id.prePadReq);
             mPostRequired = (CheckBox) layout.findViewById(R.id.postPadReq);
 
+            mOnlyNew = (CheckBox) layout.findViewById(R.id.onlyNew);
+            mAnyChannel = (CheckBox) layout.findViewById(R.id.anyChannel);
+            mAnyTime = (CheckBox) layout.findViewById(R.id.anyTime);
+
+            mSeriesOptions = layout.findViewById(R.id.seriesOptions);
+            mWeekdayOptions = (GridLayout) layout.findViewById(R.id.weekdayOptions);
+            int i = -1;
+            for (String day : DateFormatSymbols.getInstance().getWeekdays()) {
+                if (i < 0) {
+                    //first one is blank
+                    i++;
+                    continue;
+                }
+                CheckBox cbx = new CheckBox(mActivity);
+                cbx.setText(day);
+                cbx.setTextSize(14);
+                mWeekdayChecks[i++] = cbx;
+                mWeekdayOptions.addView(cbx);
+            }
+
             mOkButton = (Button) layout.findViewById(R.id.okButton);
             mOkButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -398,6 +464,29 @@ public class LiveTvGuideActivity extends BaseActivity {
                             response.setIsPostPaddingRequired(mPostRequired.isChecked());
 
                             if (mRecordSeries) {
+                                response.setDays(new ArrayList<String>());
+                                for (int i = 0; i < 7; i++) {
+                                    if (mWeekdayChecks[i].isChecked()) response.getDays().add(DayValues.get(i));
+                                }
+                                response.setRecordNewOnly(mOnlyNew.isChecked());
+                                response.setRecordAnyChannel(mAnyChannel.isChecked());
+                                response.setRecordAnyTime(mAnyTime.isChecked());
+
+                                TvApp.getApplication().getApiClient().CreateLiveTvSeriesTimerAsync(response, new EmptyResponse() {
+                                    @Override
+                                    public void onResponse() {
+                                        mPopup.dismiss();
+                                        dismissProgramOptions();
+                                        mSelectedProgramView.setRecSeriesIndicator(true);
+                                        Utils.showToast(mActivity, R.string.msg_set_to_record);
+                                    }
+
+                                    @Override
+                                    public void onError(Exception ex) {
+                                        Utils.showToast(mActivity, R.string.msg_unable_to_create_recording);
+                                    }
+
+                                });
 
                             } else {
                                 TvApp.getApplication().getApiClient().CreateLiveTvTimerAsync(response, new EmptyResponse() {
@@ -456,12 +545,37 @@ public class LiveTvGuideActivity extends BaseActivity {
             mPreRequired.setChecked(defaults.getIsPrePaddingRequired());
             mPostRequired.setChecked(defaults.getIsPostPaddingRequired());
 
+            if (recordSeries) {
+                mPopup.setHeight(SERIES_HEIGHT);
+                mSeriesOptions.setVisibility(View.VISIBLE);
+
+                // select proper days
+                int i = 0;
+                for (CheckBox day : mWeekdayChecks) {
+                    day.setChecked(defaults.getDays().contains(DayValues.get(i)));
+                    i++;
+                }
+
+                // and other options
+                mAnyChannel.setChecked(defaults.getRecordAnyChannel());
+                mOnlyNew.setChecked(defaults.getRecordNewOnly());
+                mAnyTime.setChecked(defaults.getRecordAnyTime());
+
+            } else {
+                mPopup.setHeight(NORMAL_HEIGHT);
+                mSeriesOptions.setVisibility(View.GONE);
+            }
+
         }
 
         public void show() {
             mPopup.showAtLocation(mImage, Gravity.NO_GRAVITY, mTitle.getLeft(), mTitle.getTop() - 10);
             mOkButton.requestFocus();
 
+        }
+
+        public void dismiss() {
+            if (mPopup != null && mPopup.isShowing()) mPopup.dismiss();
         }
     }
 
@@ -474,6 +588,7 @@ public class LiveTvGuideActivity extends BaseActivity {
         TextView channel = new TextView(mActivity);
         channel.setText(program.getChannelName());
         channel.setTypeface(null, Typeface.BOLD);
+        channel.setTextColor(getResources().getColor(android.R.color.holo_blue_light));
         timelineRow.addView(channel);
         TextView datetime = new TextView(mActivity);
         datetime.setText(Utils.getFriendlyDate(local)+ " @ "+android.text.format.DateFormat.getTimeFormat(mActivity).format(local)+ " ("+ DateUtils.getRelativeTimeSpanString(local.getTime())+")");
