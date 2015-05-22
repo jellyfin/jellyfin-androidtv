69a6ee7147c0603680c7b098445403d87a65489c
Eric Reed <ebr@mediabrowser3.com>
2015-05-22 14:17:29 -0400

    Progress on new details

---
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/details/MyDetailsOverviewRow.java
@@ -0,0 +1,61 @@
+package tv.emby.embyatv.details;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v17.leanback.widget.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mediabrowser.model.dto.BaseItemDto;
+import tv.emby.embyatv.ui.ImageButton;
+
+/**
+ * Created by Eric on 5/22/2015.
+ */
+public class MyDetailsOverviewRow extends Row {
+    private BaseItemDto mItem;
+    private Drawable mImageDrawable;
+    private String mSummary;
+    private String mSummaryTitle;
+    private String mSummarySubTitle;
+
+    private List<ImageButton> mActions = new ArrayList<>();
+
+    public MyDetailsOverviewRow(BaseItemDto item) {
+        mItem = item;
+    }
+
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+
+    public String getSummaryTitle() {
+        return mSummaryTitle;
+    }
+
+    public void setSummaryTitle(String mSummaryTitle) {
+        this.mSummaryTitle = mSummaryTitle;
+    }
+
+    public String getSummarySubTitle() {
+        return mSummarySubTitle;
+    }
+
+    public void setSummarySubTitle(String mSummarySubTitle) {
+        this.mSummarySubTitle = mSummarySubTitle;
+    }
+
+    public BaseItemDto getItem() { return mItem; }
+    public Drawable getImageDrawable() { return mImageDrawable; }
+
+    public void setImageDrawable(Drawable drawable) { mImageDrawable = drawable; }
+    public void setImageBitmap(Context context, Bitmap bm) { mImageDrawable = new BitmapDrawable(context.getResources(), bm); }
+}
