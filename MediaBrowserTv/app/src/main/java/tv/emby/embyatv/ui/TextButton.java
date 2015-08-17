f78627c9b40f38b87ff87fd92bc6e955fb901701
Eric Reed <ebr@mediabrowser3.com>
2015-08-17 17:16:08 -0400

    Create jump list control

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/ui/TextButton.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/ui/TextButton.java
@@ -2,9 +2,12 @@ package tv.emby.embyatv.ui;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import tv.emby.embyatv.R;
+import tv.emby.embyatv.util.Utils;
 
 /**
  * Created by Eric on 4/30/2015.
@@ -13,6 +16,19 @@ public class TextButton extends Button {
     public TextButton(Context context) {
         super(context);
         setOnFocusChangeListener(focusChangeListener);
+    }
+
+    public TextButton(Context context, String text, int size, OnClickListener listener) {
+        super(context);
+        setOnFocusChangeListener(focusChangeListener);
+        setOnClickListener(listener);
+        setText(text);
+        setTextSize(size);
+        setBackgroundColor(0);
+        int trueSize = Utils.convertDpToPixel(context, size);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(trueSize*2 +20, trueSize + 50);
+
+        setLayoutParams(lp);
     }
 
     private OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
