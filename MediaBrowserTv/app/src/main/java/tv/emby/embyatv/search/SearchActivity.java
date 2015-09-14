5485ef0428db461715c159b2808bc6fe784d1a46
Eric Reed <ebr@mediabrowser3.com>
2015-09-14 13:51:31 -0400

    Proper search choice

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/search/SearchActivity.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/search/SearchActivity.java
@@ -3,8 +3,10 @@ package tv.emby.embyatv.search;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.SpeechRecognizer;
 
 import tv.emby.embyatv.R;
+import tv.emby.embyatv.TvApp;
 import tv.emby.embyatv.util.Utils;
 
 public class SearchActivity extends Activity {
@@ -12,7 +14,7 @@ public class SearchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!Utils.isFireTv()) {
+        if (SpeechRecognizer.isRecognitionAvailable(TvApp.getApplication())) {
             setContentView(R.layout.activity_search);
         } else {
             setContentView(R.layout.activity_search_no_speech);
