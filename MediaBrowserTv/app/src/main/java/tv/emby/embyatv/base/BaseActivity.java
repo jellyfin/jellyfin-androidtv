0a6f7f1067dac2df59ce80c50e982cc236c2fb04
Eric Reed <ebr@mediabrowser3.com>
2015-10-12 10:57:42 -0400

    Only available API 19+

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/base/BaseActivity.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/base/BaseActivity.java
@@ -38,7 +38,7 @@ public class BaseActivity extends Activity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
+        if (hasFocus && android.os.Build.VERSION.SDK_INT >= 19) {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
