9bd7cb7538c3ca604590f2edf6a910fcba87ed11
Eric Reed <ebr@mediabrowser3.com>
2015-12-06 12:07:48 -0500

    Favorite on now playing

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/BrowseViewFragment.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/browsing/BrowseViewFragment.java
@@ -139,7 +139,7 @@ public class BrowseViewFragment extends EnhancedBrowseFragment {
 
                 //Favorites
                 StdItemQuery favAlbums = new StdItemQuery();
-                favAlbums.setIncludeItemTypes(new String[]{"MusicAlbum", "MusicArtist"});
+                favAlbums.setIncludeItemTypes(new String[]{"MusicAlbum", "MusicArtist", "Audio"});
                 favAlbums.setRecursive(true);
                 favAlbums.setParentId(mFolder.getId());
                 favAlbums.setFilters(new ItemFilter[]{ItemFilter.IsFavorite});
