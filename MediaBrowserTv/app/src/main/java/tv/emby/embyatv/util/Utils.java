76fcfc76c1e5bf26961b9de9aedec1ced8e4d906
Eric Reed <spam@reedsplace.com>
2016-06-24 17:37:17 -0400

    Custom card colors

--- MediaBrowserTv/app/src/main/java/tv/emby/embyatv/util/Utils.java
+++ MediaBrowserTv/app/src/main/java/tv/emby/embyatv/util/Utils.java
@@ -1688,4 +1688,17 @@ public class Utils {
         return (isFireTv() && !is50()) || "1".equals(TvApp.getApplication().getPrefs().getString("pref_audio_option","0"));
     }
 
-}
+    /**
+     * Returns darker version of specified <code>color</code>.
+     */
+    public static int darker (int color, float factor) {
+        int a = Color.alpha( color );
+        int r = Color.red( color );
+        int g = Color.green( color );
+        int b = Color.blue( color );
+
+        return Color.argb( a,
+                Math.max( (int)(r * factor), 0 ),
+                Math.max( (int)(g * factor), 0 ),
+                Math.max( (int)(b * factor), 0 ) );
+    }}
