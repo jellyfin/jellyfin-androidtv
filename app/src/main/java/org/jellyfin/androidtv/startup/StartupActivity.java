7cb3a577e3ed2b44518bb9fd133a0fe504a7af5b
Bill Thornton <billt2006@gmail.com>
2019-02-15 19:57:22 -0500

    Remove Emby icon url

--- app/src/main/java/org/jellyfin/androidtv/startup/StartupActivity.java
+++ app/src/main/java/org/jellyfin/androidtv/startup/StartupActivity.java
@@ -141,7 +141,8 @@ public class StartupActivity extends Activity {
         capabilities.setDeviceProfile(new AndroidProfile(Utils.getProfileOptions()));
         capabilities.setSupportsMediaControl(true);
         capabilities.setSupportedCommands(supportedCommands);
-        capabilities.setIconUrl("https://raw.githubusercontent.com/MediaBrowser/MediaBrowser.Android/master/servericon.png");
+//        TODO: Add new icon url
+//        capabilities.setIconUrl("");
 
         IJsonSerializer jsonSerializer = new GsonJsonSerializer();
 
