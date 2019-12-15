cc767ef1cb2d60d0f61d86eff9216c5de2bb97d8
Niels van Velzen <git@ndat.nl>
2019-12-15 17:26:08 +0100

    Fix crash because the saved instance state can be null

--- app/src/main/java/org/jellyfin/androidtv/browsing/MainActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/browsing/MainActivity.kt
@@ -5,7 +5,7 @@ import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.base.BaseActivity
 
 class MainActivity : BaseActivity() {
-	override fun onCreate(savedInstanceState: Bundle) {
+	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_home)
 	}
