ec83261b4313de5486659860066e880fc86e9120
Niels van Velzen <git@ndat.nl>
2019-12-29 17:43:09 +0100

    Remove MySearchFragmentNoSpeech and custom speech permission logic (is now handled by androidx SearchSupportFragment)

--- app/src/main/java/org/jellyfin/androidtv/search/SearchActivity.java
+++ app/src/main/java/org/jellyfin/androidtv/search/SearchActivity.java
@@ -1,28 +1,23 @@
 package org.jellyfin.androidtv.search;
 
 import android.os.Bundle;
-import android.speech.SpeechRecognizer;
 
 import org.jellyfin.androidtv.R;
-import org.jellyfin.androidtv.TvApp;
 import org.jellyfin.androidtv.base.BaseActivity;
 
 public class SearchActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (TvApp.getApplication().isVoiceSearchAllowed() &&
-                SpeechRecognizer.isRecognitionAvailable(TvApp.getApplication())) {
-            setContentView(R.layout.activity_search);
-        } else {
-            setContentView(R.layout.activity_search_no_speech);
-        }
+
+        setContentView(R.layout.activity_search);
     }
 
     @Override
     public boolean onSearchRequested() {
-        //re-start us
-        this.recreate();
+        // Reset layout
+        recreate();
+
         return true;
     }
 }
