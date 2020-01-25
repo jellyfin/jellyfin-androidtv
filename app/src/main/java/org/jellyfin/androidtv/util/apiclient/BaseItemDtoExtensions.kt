61d65240e6f7b0cebcb1fc1bd04cf6c6694125a9
Niels van Velzen <git@ndat.nl>
2020-01-25 23:09:20 +0100

    Initial progress on movies

--- app/src/main/java/org/jellyfin/androidtv/util/apiclient/BaseItemDtoExtensions.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/apiclient/BaseItemDtoExtensions.kt
@@ -2,11 +2,16 @@ package org.jellyfin.androidtv.util.apiclient
 
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
 import org.jellyfin.androidtv.model.itemtypes.Episode
+import org.jellyfin.androidtv.model.itemtypes.Movie
 import org.jellyfin.apiclient.model.dto.BaseItemDto
 import org.jellyfin.apiclient.model.dto.BaseItemType
 
 fun BaseItemDto.liftToNewFormat() : BaseItem {
 	return when(baseItemType) {
+		// Movies
+		BaseItemType.Movie -> Movie(this)
+
+		// TV
 		BaseItemType.Episode -> Episode(this)
 		else -> TODO()
 	}
