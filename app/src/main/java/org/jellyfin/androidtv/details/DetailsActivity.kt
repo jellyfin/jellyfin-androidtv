b68f62708bd5c117da548b2f91a6442e2e826928
Andreas <andreas.ackermann@yahoo.de>
2020-01-25 01:45:38 +0100

    Introduce new Items Model

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsActivity.kt
@@ -3,19 +3,15 @@ package org.jellyfin.androidtv.details
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.FragmentActivity
-import androidx.leanback.app.BackgroundManager
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.TvApp
-import org.jellyfin.androidtv.util.apiclient.asEpisode
+import org.jellyfin.androidtv.model.itemtypes.Episode
+import org.jellyfin.androidtv.model.itemtypes.IBaseItemVisitor
 import org.jellyfin.androidtv.util.apiclient.getItem
-import org.jellyfin.apiclient.model.dto.BaseItemDto
-import org.jellyfin.apiclient.model.dto.BaseItemType
-import org.jellyfin.apiclient.model.dto.ImageOptions
+import org.jellyfin.androidtv.util.apiclient.liftToNewFormat
 
 private const val LOG_TAG = "DetailsActivity"
 
@@ -33,14 +29,13 @@ class DetailsActivity : FragmentActivity() {
 
 		GlobalScope.launch(Dispatchers.Main) {
 			val baseItem = getBaseItemDtoForID(id) ?: return@launch
+			val lifted = baseItem.liftToNewFormat()
 
-			fragment = when (baseItem.baseItemType!!) {
-				BaseItemType.Episode -> {
-					val episode = baseItem.asEpisode()
-					EpisodeDetailsFragment(episode)
+			lifted.acceptVisitor(object : IBaseItemVisitor {
+				override fun visit(item: Episode) {
+					fragment = EpisodeDetailsFragment(item)
 				}
-				else -> TODO()
-			}
+			})
 
 			supportFragmentManager.beginTransaction().add(android.R.id.content, fragment).commit()
 		}
