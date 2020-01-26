eb9616893c691453978ce53b866e9252ee08a800
Niels van Velzen <git@ndat.nl>
2020-01-26 12:28:17 +0100

    Added logo and refactor movie fragment

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
@@ -1,14 +1,20 @@
 package org.jellyfin.androidtv.details
 
 import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+import androidx.leanback.widget.Presenter
+import org.jellyfin.androidtv.model.itemtypes.BaseItem
 
-class DetailsDescriptionPresenter(val title: String, val subtitle: String, val body: String) : AbstractDetailsDescriptionPresenter() {
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+	override fun onBindViewHolder(viewHolder: Presenter.ViewHolder?, item: Any?, payloads: MutableList<Any>?) {
+		super.onBindViewHolder(viewHolder, item, payloads)
+	}
 	override fun onBindDescription(viewHolder: ViewHolder, itemData: Any) {
+		val item = itemData as BaseItem
 
 		viewHolder.also {
-			it.title.text = title
-			it.subtitle.text = subtitle
-			it.body.text = body
+			it.title.text = item.name
+//			it.subtitle.text = subtitle
+			it.body.text = item.description
 		}
 	}
 }
