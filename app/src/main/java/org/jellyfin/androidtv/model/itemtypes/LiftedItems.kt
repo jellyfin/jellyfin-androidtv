30cd536b57e3a003699cb4ebca1ba70e83f0b933
Niels van Velzen <git@ndat.nl>
2020-01-26 13:22:20 +0100

    Add mediainfo

--- app/src/main/java/org/jellyfin/androidtv/model/itemtypes/LiftedItems.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/itemtypes/LiftedItems.kt
@@ -2,17 +2,22 @@ package org.jellyfin.androidtv.model.itemtypes
 
 import org.jellyfin.apiclient.model.dto.BaseItemDto
 import org.jellyfin.apiclient.model.dto.BaseItemType
+import org.jellyfin.apiclient.model.dto.ChapterInfoDto
+import java.util.*
 
 sealed class BaseItem(original: BaseItemDto) {
 	val id: String = original.id
 	val name: String = original.name
-	val description: String = original.overview
+	val description: String? = original.overview
 	val images: ImageCollection = ImageCollection(original)
+	val added: Date = original.dateCreated
 }
 
 sealed class PlayableItem(original: BaseItemDto) : BaseItem(original) {
 	val canResume: Boolean = original.canResume
 	val playbackPositionTicks: Long = original.userData.playbackPositionTicks
+	val mediaInfo = MediaInfo(original.mediaSources, original.mediaStreams)
+	val chapters: List<ChapterInfoDto> = original.chapters
 }
 
 class Episode(original: BaseItemDto) : PlayableItem(original) {
