701a03f1ef347f8d1f1cdb0852d79efd5e6dde18
Niels van Velzen <git@ndat.nl>
2020-01-26 13:22:20 +0100

    Add mediainfo

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
@@ -1,13 +1,9 @@
 package org.jellyfin.androidtv.details
 
 import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
-import androidx.leanback.widget.Presenter
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
 
 class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
-	override fun onBindViewHolder(viewHolder: Presenter.ViewHolder?, item: Any?, payloads: MutableList<Any>?) {
-		super.onBindViewHolder(viewHolder, item, payloads)
-	}
 	override fun onBindDescription(viewHolder: ViewHolder, itemData: Any) {
 		val item = itemData as BaseItem
 
