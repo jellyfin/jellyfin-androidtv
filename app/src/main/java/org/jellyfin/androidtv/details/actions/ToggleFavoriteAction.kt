a289e38659caaf1715b78808950becfdb2259b84
Niels van Velzen <git@ndat.nl>
2020-02-03 12:17:05 +0100

    Add custom actionbar design

--- app/src/main/java/org/jellyfin/androidtv/details/actions/ToggleFavoriteAction.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/actions/ToggleFavoriteAction.kt
@@ -9,9 +9,10 @@ import org.jellyfin.androidtv.TvApp
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
 import org.jellyfin.androidtv.util.apiclient.updateFavoriteStatus
 
-class ToggleFavoriteAction(context: Context, val item: BaseItem) : BaseAction(ActionID.TOGGLE_FAVORITE.id, context) {
+class ToggleFavoriteAction(context: Context, val item: BaseItem) : ToggleAction(ActionID.TOGGLE_FAVORITE.id, context) {
 	init {
-		label1 = context.getString(if (item.favorite) R.string.lbl_remove_favorite else R.string.lbl_add_favorite)
+		active = item.favorite
+		label1 = context.getString(R.string.lbl_favorite)
 	}
 
 	override fun onClick() {
