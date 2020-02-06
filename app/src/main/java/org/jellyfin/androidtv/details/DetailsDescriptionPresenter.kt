f08f2b87396a02874c7f9f8d60926d782ba5ab70
Niels van Velzen <git@ndat.nl>
2020-02-06 16:59:52 +0100

    Start on detail layout

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsDescriptionPresenter.kt
@@ -1,16 +1,40 @@
 package org.jellyfin.androidtv.details
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+import androidx.leanback.widget.Presenter
+import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
 
-class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
-	override fun onBindDescription(viewHolder: ViewHolder, item: Any) {
-		if (item !is BaseItem) return
-
-		viewHolder.also {
-			it.title.text = item.name
-//			it.subtitle.text = subtitle
-			it.body.text = item.description
-		}
+class DetailsDescriptionPresenter : Presenter() {
+	/**
+	 * The ViewHolder for the [AbstractDetailsDescriptionPresenter].
+	 */
+	class ViewHolder(view: View) : Presenter.ViewHolder(view) {
+		val title: TextView = view.findViewById(R.id.details_description_title)
+		val subtitle: TextView = view.findViewById(R.id.details_description_subtitle)
+		val body: TextView = view.findViewById(R.id.details_description_body)
 	}
+
+	override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+		val view = LayoutInflater
+			.from(parent.context)
+			.inflate(R.layout.row_details_description, parent, false)
+
+		return ViewHolder(view)
+	}
+
+	override fun onBindViewHolder(viewHolder: Presenter.ViewHolder, item: Any) {
+		viewHolder as ViewHolder
+		item as BaseItem
+
+		viewHolder.title.text = item.name
+		viewHolder.subtitle.text = item.name
+		viewHolder.body.text = item.description
+	}
+
+	override fun onUnbindViewHolder(viewHolder: Presenter.ViewHolder?) {}
 }
