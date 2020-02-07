9d9c71b6dc0c9e3f0021827d7bb68c78ef3bf1b1
Andreas <andreas.ackermann@yahoo.de>
2020-02-07 14:48:43 +0100

    Item Click handling moved to Presenters, new Marquee ImageCardView Style, Refactoring

--- app/src/main/java/org/jellyfin/androidtv/details/MovieDetailsFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/MovieDetailsFragment.kt
@@ -1,22 +1,19 @@
 package org.jellyfin.androidtv.details
 
-import android.content.Intent
-import android.util.Log
 import androidx.leanback.widget.*
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.details.actions.*
 import org.jellyfin.androidtv.model.itemtypes.Movie
-import org.jellyfin.androidtv.model.itemtypes.Person
 import org.jellyfin.androidtv.presentation.InfoCardPresenter
 
 private const val LOG_TAG = "MovieDetailsFragment"
 
-class MovieDetailsFragment(item: Movie) : BaseDetailsFragment<Movie>(item), OnItemViewClickedListener {
+class MovieDetailsFragment(item: Movie) : BaseDetailsFragment<Movie>(item) {
 
 	private val detailsRow by lazy { DetailsOverviewRow(Unit).apply { actionsAdapter = ActionAdapter() } }
 //	private val chaptersRow by lazy { Row() }
 //	private val staffRow by lazy { Row() }
-	private val charactersRow by lazy { ListRow(HeaderItem("Cast/Crew"), ArrayObjectAdapter(PersonPresenter())) }
+	private val charactersRow by lazy { ListRow(HeaderItem("Cast/Crew"), ArrayObjectAdapter(PersonPresenter(this.activity!!))) }
 //	private val relatedRow by lazy { Row() }
 	private val mediaInfoRow by lazy { ListRow(HeaderItem("Media info"), ArrayObjectAdapter(InfoCardPresenter())) }
 
@@ -33,8 +30,6 @@ class MovieDetailsFragment(item: Movie) : BaseDetailsFragment<Movie>(item), OnIt
 		adapter.add(charactersRow)
 //		adapter.add(relatedRow)
 		adapter.add(mediaInfoRow)
-
-		onItemViewClickedListener = this
 	}
 
 	override suspend fun setItem(item: Movie) {
@@ -72,16 +67,6 @@ class MovieDetailsFragment(item: Movie) : BaseDetailsFragment<Movie>(item), OnIt
 			it as ArrayObjectAdapter
 			it.clear()
 			item.mediaInfo.streams.forEach(it::add)
-		}
-	}
-
-	override fun onItemClicked(itemViewHolder: Presenter.ViewHolder?, item: Any?, rowViewHolder: RowPresenter.ViewHolder?, row: Row?) {
-		if (row == charactersRow) {
-			val person = item as Person
-			val intent = Intent(activity, FullDetailsActivity::class.java)
-			intent.putExtra("ItemId", person.id)
-
-			activity!!.startActivity(intent)
 		}
 	}
 }
