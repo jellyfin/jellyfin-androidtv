798217db7b1f49d36df0fd6ff89532fbdfab6fac
Andreas <andreas.ackermann@yahoo.de>
2020-02-07 14:48:43 +0100

    Item Click handling moved to Presenters, new Marquee ImageCardView Style, Refactoring

---
+++ app/src/main/java/org/jellyfin/androidtv/model/itemtypes/BriefPersonData.kt
@@ -0,0 +1,13 @@
+package org.jellyfin.androidtv.model.itemtypes
+
+import org.jellyfin.apiclient.model.dto.BaseItemPerson
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.apiclient.model.entities.PersonType
+
+class BriefPersonData(original: BaseItemPerson) {
+	val id: String = original.id
+	val name: String = original.name
+	val role: String = original.role
+	val type: PersonType = original.personType
+	val primaryImage: ImageCollection.Image? = original.primaryImageTag?.let { ImageCollection.Image(original.id, ImageType.Primary, it) }
+}
