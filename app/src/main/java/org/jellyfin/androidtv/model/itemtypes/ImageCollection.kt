cf38b7fac812a5a8dee31771130fc03fd005c6a3
Andreas <andreas.ackermann@yahoo.de>
2020-02-08 20:24:02 +0100

    Refactor ImageCollection.Image

--- app/src/main/java/org/jellyfin/androidtv/model/itemtypes/ImageCollection.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/itemtypes/ImageCollection.kt
@@ -15,25 +15,14 @@ class ImageCollection(original: BaseItemDto) {
 	val logo = original.imageTags[ImageType.Logo]?.let { Image(original.id, ImageType.Logo, it) }
 	val backdrops = original.backdropImageTags.map { Image(original.id, ImageType.Backdrop, it) }.toList()
 
-	class Image(private val itemId: String, private val type: ImageType, private val tag: String) {
-		var index = 0
+	class Image(private val itemId: String, private val type: ImageType, private val tag: String, private val index: Int? = null) {
 
 		val url: String by lazy {
-			if (type == ImageType.Chapter)
-				TvApp.getApplication().apiClient.GetImageUrl(itemId, ImageOptions().also {
-					it.imageType = type
-					it.tag = tag
-					it.imageIndex = index
-				})
-			else
 			TvApp.getApplication().apiClient.GetImageUrl(itemId, ImageOptions().also {
 				it.imageType = type
 				it.tag = tag
+				if (index != null) it.imageIndex = index
 			})
-		}
-
-		constructor(itemId: String, type: ImageType, tag: String, index: Int) : this(itemId, type, tag) {
-			this.index = index
 		}
 
 		suspend fun getBitmap(context: Context) = withContext(Dispatchers.IO) {
