29b1fb7affe63bb34ea29c2fd7e273aef3e8a2aa
Niels van Velzen <git@ndat.nl>
2020-02-08 17:55:28 +0100

    Add new genre dto class (depends on jellyfin-apiclient-java#36)

--- app/src/main/java/org/jellyfin/androidtv/details/GenreAdapter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/GenreAdapter.kt
@@ -4,23 +4,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import org.jellyfin.androidtv.R
+import org.jellyfin.apiclient.model.dto.GenreDto
 
 class GenreAdapter : RecyclerView.Adapter<GenreAdapter.ViewHolder>() {
-	private var items = emptyList<String>()
+	private var items = emptyList<GenreDto>()
 
 	class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 		val button = view as Button
 	}
 
-	fun setItems(items: List<String>) {
+	fun setItems(items: List<GenreDto>) {
 		this.items = items
 		notifyDataSetChanged()
 	}
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-		val view = LayoutInflater.from(parent.context)
+		val view = LayoutInflater
+			.from(parent.context)
 			.inflate(R.layout.row_details_description_genre_button, parent, false)
 
 		return ViewHolder(view)
@@ -29,6 +32,9 @@ class GenreAdapter : RecyclerView.Adapter<GenreAdapter.ViewHolder>() {
 	override fun getItemCount() = items.size
 
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-		holder.button.text = items[position]
+		val genre = items[position]
+
+		holder.button.text = genre.name
+		holder.button.setOnClickListener { Toast.makeText(holder.button.context, "Test: ${genre.name} / ${genre.id}", Toast.LENGTH_LONG).show() }
 	}
 }
