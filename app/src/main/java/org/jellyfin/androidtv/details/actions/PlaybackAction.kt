79e9e5b0d58f97dd8345e8768c8e0c1a554b7358
Andreas <andreas.ackermann@yahoo.de>
2020-02-08 17:56:33 +0100

    Rough implementation of chapter selection cards

--- app/src/main/java/org/jellyfin/androidtv/details/actions/PlaybackAction.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/actions/PlaybackAction.kt
@@ -1,18 +1,13 @@
 package org.jellyfin.androidtv.details.actions
 
 import android.content.Context
-import android.content.Intent
 import android.util.Log
-import androidx.core.content.ContextCompat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jellyfin.androidtv.TvApp
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
-import org.jellyfin.androidtv.playback.MediaManager
-import org.jellyfin.androidtv.util.apiclient.PlaybackHelperKt
+import org.jellyfin.androidtv.util.PlaybackUtil
 import org.jellyfin.androidtv.util.apiclient.getItem
-import org.jellyfin.apiclient.model.dto.BaseItemDto
-import org.jellyfin.apiclient.model.dto.BaseItemType
 
 private const val LOG_TAG = "PlaybackAction"
 
@@ -24,21 +19,7 @@ abstract class PlaybackAction(id: Long, context: Context) : BaseAction(id, conte
 			Log.e(LOG_TAG, "Failed to get a base item for the given ID")
 			return@withContext
 		}
-		play(baseItem, pos, shuffle)
+		PlaybackUtil.play(context, baseItem, pos, shuffle)
 	}
 
-	private suspend fun play(item: BaseItemDto, pos: Long, shuffle: Boolean) {
-		val toPlay = PlaybackHelperKt.getItemsToPlayCoroutine(item, pos == 0L && item.baseItemType == BaseItemType.Movie, shuffle)
-
-		if (toPlay != null) {
-			if (item.baseItemType == BaseItemType.MusicArtist) {
-				MediaManager.playNow(toPlay)
-			} else {
-				val intent = Intent(context, TvApp.getApplication().getPlaybackActivityClass(item.baseItemType))
-				MediaManager.setCurrentVideoQueue(toPlay)
-				intent.putExtra("Position", pos.toInt())
-				ContextCompat.startActivity(context, intent, null)
-			}
-		}
-	}
 }
