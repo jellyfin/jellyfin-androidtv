a8c0b80bb81c57f473321df51a4da899a5fd1efc
Niels van Velzen <git@ndat.nl>
2020-02-09 20:52:38 +0100

    Organize dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,22 +4,23 @@ plugins {
 	id("kotlin-android-extensions")
 }
 
-val version = "0.11.0"
-
 android {
-	compileSdkVersion(28)
-	buildToolsVersion = "28.0.3"
-
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-		targetCompatibility = JavaVersion.VERSION_1_8
-	}
+	compileSdkVersion(29)
 
 	defaultConfig {
+		// Android version targets
 		minSdkVersion(21)
-		targetSdkVersion(28)
+		targetSdkVersion(29)
+
+		// Release version
 		versionCode = 905
-		versionName = version
+		versionName = "0.11.0"
+	}
+
+	compileOptions {
+		// Use Java 1.8 features
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
 	}
 
 	buildTypes {
@@ -29,40 +30,56 @@ android {
 		}
 
 		getByName("debug") {
+			// Use different application id to run release and debug at the same time
 			applicationIdSuffix = ".debug"
-			isDebuggable = true
 		}
 	}
 }
 
 dependencies {
-	// This is split like this because this is a modular gradle project on jitpack
-	// see https://github.com/jitpack/gradle-modular
-	// The split allows dependency substitution to work in the settings.gradle file,
-	// at least for the 'library' part. 'android' part is still broken somehow, but this
-	// is better than nothing.
-	implementation("com.github.jellyfin.jellyfin-apiclient-java:android:master-SNAPSHOT")
-	implementation("com.github.jellyfin.jellyfin-apiclient-java:library:master-SNAPSHOT")
-	implementation("com.amazon.android:exoplayer:2.10.6")
-	implementation("androidx.recyclerview:recyclerview:1.0.0")
+	// Jellyfin
+	val jellyfinApiclientVersion= "master-SNAPSHOT"
+	implementation("com.github.jellyfin.jellyfin-apiclient-java:android:$jellyfinApiclientVersion")
+	implementation("com.github.jellyfin.jellyfin-apiclient-java:library:$jellyfinApiclientVersion")
+
+	// Kotlin
+	implementation(kotlin("stdlib-jdk8"))
+	val kotlinCoroutinesVersion = "1.3.3"
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinCoroutinesVersion")
+
+	// Android(x)
+	implementation("androidx.core:core-ktx:1.1.0")
 	implementation("androidx.leanback:leanback:1.0.0")
 	implementation("androidx.appcompat:appcompat:1.0.2")
+	implementation("androidx.tvprovider:tvprovider:1.0.0")
 	implementation("androidx.palette:palette:1.0.0")
+	implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+	implementation("androidx.recyclerview:recyclerview:1.0.0")
+
+	// Media players
+	implementation("com.amazon.android:exoplayer:2.10.6")
+	implementation("org.videolan.android:libvlc-all:3.1.12")
+
+	// Image utility
 	implementation("com.squareup.picasso:picasso:2.3.2")
 	implementation("com.github.bumptech.glide:glide:3.7.0")
-	implementation("com.squareup.okhttp:okhttp:2.7.0")
-	implementation("com.squareup.okhttp:okhttp-urlconnection:2.7.0")
-	implementation("com.google.guava:guava:18.0")
 	implementation("com.flaviofaria:kenburnsview:1.0.6")
-	implementation("org.videolan.android:libvlc-all:3.1.12")
-	implementation("ch.acra:acra-http:5.4.0")
-	implementation("ch.acra:acra-dialog:5.4.0")
-	implementation("ch.acra:acra-limiter:5.4.0")
+
+	// HTTP utility
+	val okhttpVersion = "2.7.0"
+	implementation("com.squareup.okhttp:okhttp:$okhttpVersion")
+	implementation("com.squareup.okhttp:okhttp-urlconnection:$okhttpVersion")
+
+	// Crash Reporting
+	val acraVersion = "5.4.0"
+	implementation("ch.acra:acra-http:$acraVersion")
+	implementation("ch.acra:acra-dialog:$acraVersion")
+	implementation("ch.acra:acra-limiter:$acraVersion")
+
+	// Testing
 	testImplementation("junit:junit:4.12")
-	implementation("androidx.core:core-ktx:1.1.0")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
-	implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-	implementation("androidx.tvprovider:tvprovider:1.0.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2")
+
+	// Google core library
+	implementation("com.google.guava:guava:18.0")
 }
