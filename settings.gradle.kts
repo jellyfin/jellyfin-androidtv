01f9b7cf52387a0179e3107922604dbb53ece82b
Niels van Velzen <git@ndat.nl>
2020-02-10 09:50:14 +0100

    Make enableDependencySubstitution configurable (enabled by default)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
 include(":app")
 
-val enableDependencySubstitution = true
+// Load properties from local.properties
+val properties = java.util.Properties().apply {
+	val location = file("local.properties")
+	if (location.exists())
+		load(location.inputStream())
+}
 
+// Get value for dependency substitution
+val enableDependencySubstitution = properties.getProperty("enable.dependency.substitution", "true").equals("true", true)
+
+// Replace apiclient dependency with local version
 val apiclientLocation = "../jellyfin-apiclient-java"
 if (File(apiclientLocation).exists() && enableDependencySubstitution) {
 	includeBuild(apiclientLocation) {
