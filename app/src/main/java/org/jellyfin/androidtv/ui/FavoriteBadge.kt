3353beb16883b201790e89496901f01427dc5e5e
Andreas <andreas.ackermann@yahoo.de>
2020-02-13 20:23:31 +0100

    Implement Favorite Badge

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/FavoriteBadge.kt
@@ -0,0 +1,34 @@
+package org.jellyfin.androidtv.ui
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.model.itemtypes.BaseItem
+
+class FavoriteBadge(context: Context, val item: BaseItem? = null) : ImageView(context) {
+	private val changeListener: () -> Unit = {
+		item?.let(::setItemData)
+	}
+
+	init {
+		setImageDrawable(context.getDrawable(R.drawable.ic_heart_red))
+		item?.let(::setItemData)
+	}
+
+	constructor(context: Context) : this(context, null)
+
+	private fun setItemData(item: BaseItem) {
+		visibility = if (item.favorite) View.VISIBLE else View.GONE
+	}
+
+	override fun onAttachedToWindow() {
+		super.onAttachedToWindow()
+		item?.addChangeListener(changeListener)
+	}
+
+	override fun onDetachedFromWindow() {
+		super.onDetachedFromWindow()
+		item?.removeChangeListener(changeListener)
+	}
+}
