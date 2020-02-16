badbd2d532a182ee07ba90a1f403107572c63dba
Niels van Velzen <git@ndat.nl>
2020-02-16 13:08:59 +0100

    Remove debug code

--- app/src/main/java/org/jellyfin/androidtv/startup/StartupActivity.java
+++ app/src/main/java/org/jellyfin/androidtv/startup/StartupActivity.java
@@ -18,7 +18,6 @@ import org.jellyfin.androidtv.itemhandling.ItemLauncher;
 import org.jellyfin.androidtv.model.compat.AndroidProfile;
 import org.jellyfin.androidtv.playback.MediaManager;
 import org.jellyfin.androidtv.playback.PlaybackManager;
-import org.jellyfin.androidtv.playback.nextup.NextUpActivity;
 import org.jellyfin.androidtv.util.ProfileHelper;
 import org.jellyfin.androidtv.util.Utils;
 import org.jellyfin.androidtv.util.apiclient.AuthenticationHelper;
@@ -116,7 +115,7 @@ public class StartupActivity extends FragmentActivity {
             }
         } else {
             // go straight into last connection
-            Intent intent = new Intent(this, NextUpActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
 
             finish();
