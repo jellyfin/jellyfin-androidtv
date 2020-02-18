a7c15a0600a81d661544e5bb061b70a7efb24093
Andreas <andreas.ackermann@yahoo.de>
2020-02-18 13:49:08 +0100

    Add isVisible property to actions

--- app/src/main/java/org/jellyfin/androidtv/details/actions/SecondariesPopupAction.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/actions/SecondariesPopupAction.kt
@@ -9,26 +9,37 @@ class SecondariesPopupAction(context: Context) : BaseAction(ActionID.SECONDARIES
 	private val containedActions = mutableListOf<BaseAction>()
 	var anchor: View? = null
 
+	private fun recomputeVisibility() {
+		isVisible = containedActions.any { action -> action.isVisible }
+	}
+
 	init {
 		label1 = context.getString(R.string.lbl_more_actions)
 		icon = context.getDrawable(R.drawable.ic_more)
 	}
 
 	fun add(toAdd: BaseAction) {
+		toAdd.onVisibilityChanged = ::recomputeVisibility
 		containedActions.add(toAdd)
+		recomputeVisibility()
 	}
 
 	fun remove(toRemove: BaseAction) {
+		if (toRemove.onVisibilityChanged == ::recomputeVisibility) toRemove.onVisibilityChanged = null
 		containedActions.remove(toRemove)
+		recomputeVisibility()
 	}
 
 	override fun onClick() {
 		val menu = PopupMenu(context, anchor)
 		containedActions.forEach { action ->
-			val item = menu.menu.add(action.label1)
-			item.setOnMenuItemClickListener {
-				action.onClick()
-				true
+			if (action.isVisible) {
+				val item = menu.menu.add(action.label1)
+				item.icon = action.icon
+				item.setOnMenuItemClickListener {
+					action.onClick()
+					true
+				}
 			}
 		}
 
