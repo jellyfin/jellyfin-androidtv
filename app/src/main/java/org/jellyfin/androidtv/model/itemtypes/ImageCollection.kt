66e2a1f5a7bd5d4238434f402c14dfc3ada661e0
Niels van Velzen <git@ndat.nl>
2020-02-20 14:09:13 +0100

    Improve image loading

--- app/src/main/java/org/jellyfin/androidtv/model/itemtypes/ImageCollection.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/itemtypes/ImageCollection.kt
@@ -1,9 +1,12 @@
 package org.jellyfin.androidtv.model.itemtypes
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.util.Log
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jellyfin.androidtv.TvApp
 import org.jellyfin.apiclient.model.dto.BaseItemDto
@@ -15,19 +18,32 @@ class ImageCollection(original: BaseItemDto) {
 	val logo = original.imageTags[ImageType.Logo]?.let { Image(original.id, ImageType.Logo, it) }
 	val backdrops = original.backdropImageTags.map { Image(original.id, ImageType.Backdrop, it) }.toList()
 
-	class Image(private val itemId: String, private val type: ImageType, private val tag: String, private val index: Int? = null) {
-
+	class Image(
+		private val itemId: String,
+		private val type: ImageType,
+		private val tag: String,
+		private val index: Int? = null
+	) {
 		val url: String by lazy {
 			TvApp.getApplication().apiClient.GetImageUrl(itemId, ImageOptions().also {
 				it.imageType = type
 				it.tag = tag
 				it.imageIndex = index
+				it.enableImageEnhancers = false
 			})
 		}
 
 		suspend fun getBitmap(context: Context) = withContext(Dispatchers.IO) {
-			Log.i("Image URL", url)
+			Log.i("ImageCollection", "getBitmap() URL: $url")
 			Picasso.with(context).load(url).get()
+		}
+
+		fun load(context: Context, success: (bitmap: Bitmap) -> Unit) {
+			Log.i("ImageCollection", "load() URL: $url")
+			GlobalScope.launch(Dispatchers.IO) {
+				val bitmap = Picasso.with(context).load(url).get()
+				withContext(Dispatchers.Main) { success(bitmap) }
+			}
 		}
 	}
 }
