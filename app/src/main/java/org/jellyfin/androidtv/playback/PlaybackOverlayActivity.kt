00e9d4a6f914cfc724fd4d82446946d4e6742bbf
Niels van Velzen <git@ndat.nl>
2020-02-29 18:00:59 +0100

    Use the "colorMode" setting (since Android 9) to activate HDR in the player activity

--- app/src/main/java/org/jellyfin/androidtv/playback/PlaybackOverlayActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/playback/PlaybackOverlayActivity.kt
@@ -1,6 +1,5 @@
 package org.jellyfin.androidtv.playback
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
@@ -17,9 +16,6 @@ class PlaybackOverlayActivity : BaseActivity() {
 
 	public override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
-		// Workaround for Sony Bravia devices that show a "grey" background on HDR videos
-		window.decorView.setBackgroundColor(Color.TRANSPARENT)
 
 		setContentView(R.layout.activity_playback_overlay)
 
