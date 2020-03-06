e2753798d67b99c7a4d4e9ecfb6be129f7f194aa
Niels van Velzen <git@ndat.nl>
2020-03-06 19:12:06 +0100

    Upgrade Exoplayer to version 2.11.3

--- app/src/main/java/org/jellyfin/androidtv/playback/MediaManager.java
+++ app/src/main/java/org/jellyfin/androidtv/playback/MediaManager.java
@@ -13,7 +13,6 @@ import android.text.InputType;
 import android.widget.EditText;
 
 import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
@@ -202,7 +201,7 @@ public class MediaManager {
             // Create a new media player based on platform
             if (DeviceUtils.is60()) {
                 nativeMode = true;
-                mExoPlayer = ExoPlayerFactory.newSimpleInstance(TvApp.getApplication());
+                mExoPlayer = new SimpleExoPlayer.Builder(TvApp.getApplication()).build();
                 mExoPlayer.addListener(new Player.EventListener() {
                     @Override
                     public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
