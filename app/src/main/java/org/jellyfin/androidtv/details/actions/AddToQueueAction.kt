6cdc1691eef74164bbb351d8b3940b37405a573b
Niels van Velzen <git@ndat.nl>
2020-03-13 12:17:00 +0100

    Convert actions to LiveData

--- app/src/main/java/org/jellyfin/androidtv/details/actions/AddToQueueAction.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/actions/AddToQueueAction.kt
@@ -2,9 +2,8 @@ package org.jellyfin.androidtv.details.actions
 
 import android.content.Context
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.TvApp
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
@@ -13,21 +12,18 @@ import org.jellyfin.androidtv.util.apiclient.getItem
 
 private const val LOG_TAG = "AddToQueueAction"
 
-class AddToQueueAction(context: Context, private val item: BaseItem) : Action(ActionID.ADD_TO_QUEUE.id, context) {
-	override val visible = true
-	override val text = context.getString(R.string.lbl_add_to_queue)
-	override val icon = context.getDrawable(R.drawable.ic_add)!!
+class AddToQueueAction(context: Context, private val item: BaseItem) : Action() {
+	override val visible = MutableLiveData(true)
+	override val text = MutableLiveData(context.getString(R.string.lbl_add_to_queue))
+	override val icon = MutableLiveData(context.getDrawable(R.drawable.ic_add)!!)
 
-	override fun onClick() {
+	override suspend fun onClick(view: View) {
 		// TODO if audio is implemented, check here wheter we got an audio file and add it to audio queue
-		GlobalScope.launch(Dispatchers.Main) {
-			val retrieved = TvApp.getApplication().apiClient.getItem(item.id)
-			if (retrieved == null) {
-				Log.e(LOG_TAG, "Failed adding item %s to queue because retrieval failed!".format(item.id))
-			} else {
-				MediaManager.addToVideoQueue(retrieved)
-			}
+		val retrieved = TvApp.getApplication().apiClient.getItem(item.id)
+		if (retrieved == null) {
+			Log.e(LOG_TAG, "Failed adding item %s to queue because retrieval failed!".format(item.id))
+		} else {
+			MediaManager.addToVideoQueue(retrieved)
 		}
 	}
-
 }
