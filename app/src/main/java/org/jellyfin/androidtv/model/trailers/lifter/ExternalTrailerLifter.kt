4deb8742b0877b4ea435162ed0f16f70bde35419
Andreas <andreas.ackermann@yahoo.de>
2020-03-31 19:58:28 +0200

    Implemented lifting of YouTube Trailers

--- app/src/main/java/org/jellyfin/androidtv/model/trailers/lifter/ExternalTrailerLifter.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/trailers/lifter/ExternalTrailerLifter.kt
@@ -2,6 +2,7 @@ package org.jellyfin.androidtv.model.trailers.lifter
 
 import org.jellyfin.androidtv.model.trailers.external.ExternalTrailer
 import org.jellyfin.apiclient.model.entities.MediaUrl
+import java.net.MalformedURLException
 import java.net.URL
 
 abstract class ExternalTrailerLifter {
@@ -11,7 +12,17 @@ abstract class ExternalTrailerLifter {
 		return url.host?.let { host -> domainRegex.find(host)?.groups?.get(2)?.value }
 	}
 
+	protected fun mediaUrlToUrl(mediaUrl: MediaUrl) = try {
+		URL(mediaUrl.url)
+	} catch (ex: MalformedURLException) {
+		try {
+			URL("https://" + mediaUrl.url)
+		} catch (ex: Exception) {
+			null
+		}
+	}
 
-	abstract fun canLift(url: MediaUrl) : Boolean
-	abstract fun lift(url: MediaUrl) : ExternalTrailer
+
+	abstract fun canLift(url: MediaUrl): Boolean
+	abstract fun lift(url: MediaUrl): ExternalTrailer
 }
