4fd47b52862b1d25973eea8b54866320d2f68ae2
Andreas <andreas.ackermann@yahoo.de>
2020-04-04 15:03:02 +0200

    Implement generic external trailer handling

---
+++ app/src/main/java/org/jellyfin/androidtv/details/trailers/ExternalTrailerPresenter.kt
@@ -0,0 +1,55 @@
+package org.jellyfin.androidtv.details.trailers
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.leanback.widget.Presenter
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.base.IItemClickListener
+import org.jellyfin.androidtv.model.trailers.external.ExternalTrailer
+import org.jellyfin.androidtv.ui.MultiBadgeImageCardView
+import org.jellyfin.androidtv.util.ImageUtils
+
+open class ExternalTrailerPresenter(private val context: Context, private val imageHeight: Int) : Presenter(), IItemClickListener {
+	protected open val thumbnail
+		get() = context.getDrawable(R.drawable.tile_chapter)
+
+	protected open val description: String?
+		get() = null
+
+	override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
+		return ViewHolder(MultiBadgeImageCardView(ContextThemeWrapper(parent!!.context, R.style.MarqueeImageCardViewTheme)))
+	}
+
+	override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+		val trailer = item as ExternalTrailer
+
+		(viewHolder.view as MultiBadgeImageCardView).apply {
+			isFocusable = true
+			isFocusableInTouchMode = true
+			setMainImageDimensions((ImageUtils.ASPECT_RATIO_16_9 * imageHeight).toInt(), imageHeight)
+			mainImageDrawable = thumbnail
+			titleText = trailer.name
+			contentText = description
+		}
+	}
+
+	override fun onUnbindViewHolder(viewHolder: ViewHolder) {
+		// do nothing
+	}
+
+	override suspend fun onItemClicked(item: Any?) {
+		if (item !is ExternalTrailer)
+			throw IllegalArgumentException("Tried to pass ExternalTrailer.onItemClicked an item that is not a trailer")
+
+		val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.playbackURL))
+
+		if (intent.resolveActivity(context.packageManager) != null)
+			context.startActivity(intent)
+		else
+			Toast.makeText(context, R.string.toast_no_trailer_handler, Toast.LENGTH_LONG).show()
+	}
+}
--- app/src/main/java/org/jellyfin/androidtv/details/trailers/YouTubeTrailerPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/trailers/YouTubeTrailerPresenter.kt
@@ -1,14 +1,10 @@
 package org.jellyfin.androidtv.details.trailers
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.view.ContextThemeWrapper
-import androidx.leanback.widget.Presenter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.GlideDrawable
 import com.bumptech.glide.request.RequestListener
@@ -18,32 +14,30 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.R
-import org.jellyfin.androidtv.base.IItemClickListener
 import org.jellyfin.androidtv.model.trailers.external.YouTubeTrailer
 import org.jellyfin.androidtv.ui.MultiBadgeImageCardView
-import org.jellyfin.androidtv.util.ImageUtils
 
 class YouTubeTrailerPresenter(private val context: Context,
-							  private val imageHeight: Int,
+							  imageHeight: Int,
 							  private val safeBrandingCompliance: Boolean
-) : Presenter(), IItemClickListener {
+) : ExternalTrailerPresenter(context, imageHeight) {
 	private val LOG_TAG = "YouTubeTrailerPresenter"
+
+	override val thumbnail
+		get() = context.getDrawable(R.drawable.banner_youtube)
+
+	override val description
+		get() = context.getString(R.string.brand_youtube)
 
 	override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
 		return ViewHolder(MultiBadgeImageCardView(ContextThemeWrapper(parent!!.context, R.style.MarqueeImageCardViewTheme)))
 	}
 
 	override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
-		val trailer = item as YouTubeTrailer
+		super.onBindViewHolder(viewHolder, item)
 
-		val cardView = (viewHolder.view as MultiBadgeImageCardView).apply {
-			isFocusable = true
-			isFocusableInTouchMode = true
-			setMainImageDimensions((ImageUtils.ASPECT_RATIO_16_9 * imageHeight).toInt(), imageHeight)
-			mainImageDrawable = context.getDrawable(R.drawable.banner_youtube)
-			titleText = trailer.name
-			contentText = context.getString(R.string.brand_youtube)
-		}
+		val trailer = item as YouTubeTrailer
+		val cardView = (viewHolder.view as MultiBadgeImageCardView)
 
 		if (!safeBrandingCompliance) {
 			GlobalScope.launch(Dispatchers.IO) {
@@ -72,17 +66,5 @@ class YouTubeTrailerPresenter(private val context: Context,
 		(viewHolder.view as MultiBadgeImageCardView).apply {
 			setBadge(MultiBadgeImageCardView.BadgeLocation.BOTTOM_RIGHT, null)
 		}
-	}
-
-	override suspend fun onItemClicked(item: Any?) {
-		if (item !is YouTubeTrailer)
-			throw IllegalArgumentException("Tried to pass ExternalTrailerProvide.onClick a non-media-url")
-
-		val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.playbackURL))
-
-		if (intent.resolveActivity(context.packageManager) != null)
-			context.startActivity(intent)
-		else
-			Toast.makeText(context, R.string.toast_no_trailer_handler, Toast.LENGTH_LONG).show()
 	}
 }
