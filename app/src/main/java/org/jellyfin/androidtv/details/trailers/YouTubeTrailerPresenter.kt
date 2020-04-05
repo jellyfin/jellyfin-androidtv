5681c9b56abb3d939d5b609fee3606c88d648838
Andreas <andreas.ackermann@yahoo.de>
2020-04-05 17:12:57 +0200

    Branding compliance for external trailers

--- app/src/main/java/org/jellyfin/androidtv/details/trailers/YouTubeTrailerPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/trailers/YouTubeTrailerPresenter.kt
@@ -1,33 +1,20 @@
 package org.jellyfin.androidtv.details.trailers
 
 import android.content.Context
-import android.util.Log
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.appcompat.view.ContextThemeWrapper
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.GlideDrawable
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.multi_badge_image_card_view.view.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.model.trailers.external.YouTubeTrailer
 import org.jellyfin.androidtv.ui.MultiBadgeImageCardView
 
 class YouTubeTrailerPresenter(private val context: Context,
-							  imageHeight: Int,
-							  private val safeBrandingCompliance: Boolean
+							  imageHeight: Int
 ) : ExternalTrailerPresenter(context, imageHeight) {
-	private val LOG_TAG = "YouTubeTrailerPresenter"
-
-	override val thumbnail
-		get() = context.getDrawable(R.drawable.banner_youtube)
 
 	override val description
-		get() = context.getString(R.string.brand_youtube)
+		get() = context.getString(R.string.domain_youtube)
 
 	override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
 		return ViewHolder(MultiBadgeImageCardView(ContextThemeWrapper(parent!!.context, R.style.MarqueeImageCardViewTheme)))
@@ -39,27 +26,7 @@ class YouTubeTrailerPresenter(private val context: Context,
 		val trailer = item as YouTubeTrailer
 		val cardView = (viewHolder.view as MultiBadgeImageCardView)
 
-		if (!safeBrandingCompliance) {
-			GlobalScope.launch(Dispatchers.IO) {
-				Glide.with(context).load(trailer.thumbnailURL)
-			}
-
-			Glide.with(context).load(trailer.thumbnailURL).listener(object : RequestListener<String, GlideDrawable> {
-				override fun onException(e: Exception, model: String?, target: Target<GlideDrawable>?, isFirstResource: Boolean): Boolean {
-					Log.e(LOG_TAG, "Failed to load thumbnail: $e")
-					return false
-				}
-
-				override fun onResourceReady(resource: GlideDrawable?, model: String?, target: Target<GlideDrawable>?, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean {
-					val badge = ImageView(context)
-					badge.setImageDrawable(context.getDrawable(R.drawable.ic_youtube))
-
-					cardView.setBadge(MultiBadgeImageCardView.BadgeLocation.BOTTOM_RIGHT, badge)
-					return false
-				}
-
-			}).into(cardView.main_image)
-		}
+		Glide.with(context).load(trailer.thumbnailURL).into(cardView.main_image)
 	}
 
 	override fun onUnbindViewHolder(viewHolder: ViewHolder) {
