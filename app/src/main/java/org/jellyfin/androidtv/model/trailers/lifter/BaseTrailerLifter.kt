def07859f523f7279066741a850af319ceb3b6d7
Andreas <andreas.ackermann@yahoo.de>
2020-04-07 12:12:07 +0200

    Address code review

--- app/src/main/java/org/jellyfin/androidtv/model/trailers/lifter/BaseTrailerLifter.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/trailers/lifter/BaseTrailerLifter.kt
@@ -1,17 +1,24 @@
 package org.jellyfin.androidtv.model.trailers.lifter
 
+import android.util.Log
 import org.jellyfin.androidtv.model.trailers.external.ExternalTrailer
 import org.jellyfin.apiclient.model.entities.MediaUrl
 import java.net.MalformedURLException
 import java.net.URL
 
 abstract class BaseTrailerLifter {
+	private val LOG_TAG = "BaseTrailerLifter"
+
 	protected fun mediaUrlToUrl(mediaUrl: MediaUrl) = try {
 		URL(mediaUrl.url)
 	} catch (ex: MalformedURLException) {
+		// TODO: Add logging back once DI is available, as directly referencing the Android Logger
+		// TODO: causes the unit tests to fail as Log is not stubbed.
+//		Log.i(LOG_TAG, "Failed to convert ${mediaUrl.url} to Url, retrying with https:// prepended")
 		try {
 			URL("https://" + mediaUrl.url)
-		} catch (ex: Exception) {
+		} catch (ex: MalformedURLException) {
+//			Log.e(LOG_TAG, "Failed to convert ${mediaUrl.url} to Url")
 			null
 		}
 	}
