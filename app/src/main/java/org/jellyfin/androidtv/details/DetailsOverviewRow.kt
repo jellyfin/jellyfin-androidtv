2e17126b2795ca961b8ee78c070ff52986bf4909
Andreas <andreas.ackermann@yahoo.de>
2020-04-09 17:52:31 +0200

    Make DetailsOverviewRow primary image and backdrop configurable

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsOverviewRow.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsOverviewRow.kt
@@ -3,5 +3,9 @@ package org.jellyfin.androidtv.details
 import androidx.leanback.widget.Row
 import org.jellyfin.androidtv.details.actions.Action
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
+import org.jellyfin.androidtv.model.itemtypes.ImageCollection
 
-class DetailsOverviewRow(val item: BaseItem, val actions: List<Action>) : Row()
+class DetailsOverviewRow(val item: BaseItem,
+						 val actions: List<Action>,
+						 val primaryImage: ImageCollection.Image?,
+						 val backdrops: List<ImageCollection.Image>) : Row()
