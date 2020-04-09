d586b140257f93412038cc56b0e054e002f4d043
Bill Thornton <billt2006@gmail.com>
2020-04-08 22:11:16 -0400

    Fix user login buttons

--- app/src/main/java/org/jellyfin/androidtv/startup/SelectUserFragment.java
+++ app/src/main/java/org/jellyfin/androidtv/startup/SelectUserFragment.java
@@ -2,6 +2,8 @@ package org.jellyfin.androidtv.startup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.leanback.widget.ArrayObjectAdapter;
 import androidx.leanback.widget.HeaderItem;
 import androidx.leanback.widget.ListRow;
@@ -9,7 +11,6 @@ import androidx.leanback.widget.OnItemViewClickedListener;
 import androidx.leanback.widget.Presenter;
 import androidx.leanback.widget.Row;
 import androidx.leanback.widget.RowPresenter;
-import android.widget.Toast;
 
 import org.jellyfin.androidtv.R;
 import org.jellyfin.androidtv.TvApp;
@@ -20,13 +21,12 @@ import org.jellyfin.androidtv.presentation.CardPresenter;
 import org.jellyfin.androidtv.presentation.GridButtonPresenter;
 import org.jellyfin.androidtv.ui.GridButton;
 import org.jellyfin.androidtv.util.apiclient.AuthenticationHelper;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.jellyfin.apiclient.interaction.Response;
 import org.jellyfin.apiclient.model.apiclient.ServerInfo;
 import org.jellyfin.apiclient.model.serialization.GsonJsonSerializer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectUserFragment extends CustomBrowseFragment {
     private static final int ENTER_MANUALLY = 0;
@@ -40,7 +40,6 @@ public class SelectUserFragment extends CustomBrowseFragment {
         mServer = serializer.DeserializeFromString(getActivity().getIntent().getStringExtra("Server"), ServerInfo.class);
 
         super.onActivityCreated(savedInstanceState);
-
     }
 
     @Override
@@ -79,15 +78,15 @@ public class SelectUserFragment extends CustomBrowseFragment {
                         mApplication.getConnectionManager().GetAvailableServers(new Response<ArrayList<ServerInfo>>() {
                             @Override
                             public void onResponse(ArrayList<ServerInfo> serverResponse) {
-                                    Intent serverIntent = new Intent(getActivity(), SelectServerActivity.class);
-                                    GsonJsonSerializer serializer = TvApp.getApplication().getSerializer();
-                                    List<String> payload = new ArrayList<>();
-                                    for (ServerInfo server : serverResponse) {
-                                        payload.add(serializer.SerializeToString(server));
-                                    }
-                                    serverIntent.putExtra("Servers", payload.toArray(new String[] {}));
-                                    serverIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                                    requireActivity().startActivity(serverIntent);
+                                Intent serverIntent = new Intent(getActivity(), SelectServerActivity.class);
+                                GsonJsonSerializer serializer = TvApp.getApplication().getSerializer();
+                                List<String> payload = new ArrayList<>();
+                                for (ServerInfo server : serverResponse) {
+                                    payload.add(serializer.SerializeToString(server));
+                                }
+                                serverIntent.putExtra("Servers", payload.toArray(new String[]{}));
+                                serverIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                requireActivity().startActivity(serverIntent);
                             }
                         });
                         break;
@@ -100,14 +99,11 @@ public class SelectUserFragment extends CustomBrowseFragment {
                                 .show();
                         break;
                 }
-            }
-
-            if (item instanceof BaseRowItem) {
-                switch (((BaseRowItem) item).getGridButton().getId()) {
-                    case ENTER_MANUALLY:
-                        // Manual login
-                        AuthenticationHelper.enterManualUser(getActivity());
-                        break;
+            } else if (item instanceof BaseRowItem) {
+                if (((BaseRowItem) item).getGridButton() != null &&
+                        ((BaseRowItem) item).getGridButton().getId() == ENTER_MANUALLY) {
+                    // Manual login
+                    AuthenticationHelper.enterManualUser(getActivity());
                 }
             }
         }
