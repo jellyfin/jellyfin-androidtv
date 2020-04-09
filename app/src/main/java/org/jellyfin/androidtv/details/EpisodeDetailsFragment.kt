c1fb060601a738961763e0203875b37f7da02d62
Andreas <andreas.ackermann@yahoo.de>
2020-04-09 20:19:12 +0200

    Add Series and Season Actions as well as proper images to EpisodeDetailsFragment

--- app/src/main/java/org/jellyfin/androidtv/details/EpisodeDetailsFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/EpisodeDetailsFragment.kt
@@ -3,6 +3,7 @@ package org.jellyfin.androidtv.details
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.ClassPresenterSelector
 import androidx.lifecycle.MutableLiveData
+import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.details.actions.*
 import org.jellyfin.androidtv.model.itemtypes.Episode
 import org.jellyfin.androidtv.presentation.InfoCardPresenter
@@ -20,16 +21,20 @@ class EpisodeDetailsFragment(private val episode: Episode) : BaseDetailsFragment
 			ToggleFavoriteAction(context!!, item),
 
 			// "More" button
-			SecondariesPopupAction(context!!, listOf(
-				// TODO: Go to Series Button
-				// TODO: Go to Season Button
+			SecondariesPopupAction(context!!, listOfNotNull(
+				episode.seasonId?.let {GoToItemAction(context!!, context!!.getString(R.string.lbl_goto_season), it) },
+				episode.seriesId?.let {GoToItemAction(context!!, context!!.getString(R.string.lbl_goto_series), it) },
 				DeleteAction(context!!, item) { activity?.finish() }
 			))
 		)
 	}
 
 	// Row definitions
-	private val detailRow by lazy { DetailsOverviewRow(episode, actions, episode.images.logo, episode.images.backdrops) }
+	private val detailRow by lazy {
+		val primary = episode.images.parentPrimary ?: episode.seasonPrimaryImage ?: episode.seriesPrimaryImage ?: episode.images.primary
+		val backdrops = episode.images.parentBackdrops ?: episode.images.backdrops
+		DetailsOverviewRow(episode, actions, primary, backdrops)
+	}
 	// TODO: More from this season row
 	private val chaptersRow by lazy { createListRow("Chapters", episode.chapters, ChapterInfoPresenter(context!!)) }
 	private val streamInfoRow by lazy { createListRow("Media info", episode.mediaInfo.streams, InfoCardPresenter()) }
