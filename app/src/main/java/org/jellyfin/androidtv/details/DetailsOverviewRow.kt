52909b396d1c42126338b10b7fb80fca255dc9d3
Andreas <andreas.ackermann@yahoo.de>
2020-04-10 11:42:26 +0200

    Beautify DetailsOverviewRow

--- app/src/main/java/org/jellyfin/androidtv/details/DetailsOverviewRow.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/DetailsOverviewRow.kt
@@ -5,7 +5,9 @@ import org.jellyfin.androidtv.details.actions.Action
 import org.jellyfin.androidtv.model.itemtypes.BaseItem
 import org.jellyfin.androidtv.model.itemtypes.ImageCollection
 
-class DetailsOverviewRow(val item: BaseItem,
-						 val actions: List<Action>,
-						 val primaryImage: ImageCollection.Image?,
-						 val backdrops: List<ImageCollection.Image>) : Row()
+class DetailsOverviewRow(
+	val item: BaseItem,
+	val actions: List<Action>,
+	val primaryImage: ImageCollection.Image?,
+	val backdrops: List<ImageCollection.Image>
+) : Row()
