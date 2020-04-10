2ac3fef4d393a31b572b4699031856118ef1a87c
Andreas <andreas.ackermann@yahoo.de>
2020-04-10 02:25:48 +0200

    Add "More from this Season" + guest stars to EpisodeDetailsFragment

--- app/src/main/java/org/jellyfin/androidtv/details/EpisodeDetailsFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/details/EpisodeDetailsFragment.kt
@@ -3,11 +3,20 @@ package org.jellyfin.androidtv.details
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.ClassPresenterSelector
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.TvApp
 import org.jellyfin.androidtv.details.actions.*
 import org.jellyfin.androidtv.model.itemtypes.Episode
 import org.jellyfin.androidtv.presentation.InfoCardPresenter
+import org.jellyfin.androidtv.util.ImageUtils
 import org.jellyfin.androidtv.util.addIfNotEmpty
+import org.jellyfin.androidtv.util.apiclient.getEpisodesOfSeason
+import org.jellyfin.androidtv.util.dp
+import org.jellyfin.apiclient.model.entities.PersonType
 
 class EpisodeDetailsFragment(private val episode: Episode) : BaseDetailsFragment<Episode>(episode) {
 	// Action definitions
@@ -35,18 +44,35 @@ class EpisodeDetailsFragment(private val episode: Episode) : BaseDetailsFragment
 		val backdrops = episode.images.parentBackdrops ?: episode.images.backdrops
 		DetailsOverviewRow(episode, actions, primary, backdrops)
 	}
-	// TODO: More from this season row
+	private val moreFromThisSeason by lazy { createListRow("More from this Season", emptyList(), ItemPresenter(context!!, (ImageUtils.ASPECT_RATIO_16_9 * 140.dp).toInt(), 140.dp, true)) }
+	private val guestStars by lazy { createListRow("Guest Stars", episode.cast.filter { it.type == PersonType.GuestStar }, PersonPresenter(context!!)) }
 	private val chaptersRow by lazy { createListRow("Chapters", episode.chapters, ChapterInfoPresenter(context!!)) }
 	private val streamInfoRow by lazy { createListRow("Media info", episode.mediaInfo.streams, InfoCardPresenter()) }
 
 	override suspend fun onCreateAdapters(rowSelector: ClassPresenterSelector, rowAdapter: ArrayObjectAdapter) {
 		super.onCreateAdapters(rowSelector, rowAdapter)
 
+		loadAdditionalInformation()
+
 		// Add rows
 		rowAdapter.apply {
 			add(detailRow)
+			addIfNotEmpty(moreFromThisSeason)
+			addIfNotEmpty(guestStars)
 			addIfNotEmpty(chaptersRow)
 			addIfNotEmpty(streamInfoRow)
 		}
+	}
+
+	private suspend fun loadAdditionalInformation() = withContext(Dispatchers.IO) {
+		// Get additional information asynchronously
+		awaitAll(
+			async {
+				TvApp.getApplication().apiClient.getEpisodesOfSeason(episode)?.let { episodes ->
+					val adapter = (moreFromThisSeason.adapter as ArrayObjectAdapter)
+					adapter.apply { episodes.forEach(::add) }
+				}
+			}
+		)
 	}
 }
--- app/src/main/java/org/jellyfin/androidtv/model/itemtypes/BriefPersonData.kt
+++ app/src/main/java/org/jellyfin/androidtv/model/itemtypes/BriefPersonData.kt
@@ -7,7 +7,7 @@ import org.jellyfin.apiclient.model.entities.PersonType
 class BriefPersonData(original: BaseItemPerson) {
 	val id: String = original.id
 	val name: String = original.name
-	val role: String = original.role
+	val role: String? = original.role
 	val type: PersonType = original.personType
 	val primaryImage: ImageCollection.Image? = original.primaryImageTag?.let { ImageCollection.Image(original.id, ImageType.Primary, it) }
 }
