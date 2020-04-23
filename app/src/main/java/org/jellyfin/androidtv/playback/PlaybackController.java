57f8d9672be8eb3428e45f5bfa9be3bf02767498
Froghut <froghut@gmail.com>
2020-04-23 21:09:27 +0200

    stop progress report if playback was already stopped

--- app/src/main/java/org/jellyfin/androidtv/playback/PlaybackController.java
+++ app/src/main/java/org/jellyfin/androidtv/playback/PlaybackController.java
@@ -1005,6 +1005,11 @@ public class PlaybackController {
                     return;
                 }
 
+                if (mPlaybackState != PlaybackState.PLAYING) {
+                    // Playback was stopped, don't report progress anymore
+                    return;
+                }
+
                 long currentTime = isLiveTv ? getTimeShiftedProgress() : mVideoManager.getCurrentPosition();
                 if (isLiveTv && !directStreamLiveTv) {
                     mFragment.setSecondaryTime(getRealTimeProgress());
