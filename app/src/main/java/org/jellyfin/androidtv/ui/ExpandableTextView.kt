ba461b0165936fed8fd1179bfb49b405f6490c10
Niels van Velzen <git@ndat.nl>
2020-04-26 10:30:40 +0200

    Redo ExpandableTextView

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/ExpandableTextView.kt
@@ -0,0 +1,34 @@
+package org.jellyfin.androidtv.ui
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.app.ActivityOptionsCompat
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.TvApp
+import org.jellyfin.androidtv.details.ExpandedTextActivity
+import org.jellyfin.androidtv.util.getActivity
+
+class ExpandableTextView(context: Context, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
+	init {
+		background = context.getDrawable(R.drawable.expanded_text)
+		transitionName = ExpandedTextActivity.TRANSITION_NAME
+
+		setOnClickListener {
+			val activity = context.getActivity() ?: return@setOnClickListener
+
+			val intent = Intent(TvApp.getApplication(), ExpandedTextActivity::class.java).apply {
+				putExtra(ExpandedTextActivity.EXTRA_TEXT, text.toString())
+			}
+
+			val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+				activity,
+				this,
+				ExpandedTextActivity.TRANSITION_NAME
+			)
+
+			activity.startActivity(intent, options.toBundle())
+		}
+	}
+}
