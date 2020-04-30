d3aa064904b0a6006a62a736ed147eb0b23f927e
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2020-04-30 10:04:54 +0200

    Add the "v" prefix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
 	// Jellyfin
-	implementation("com.github.jellyfin.jellyfin-apiclient-java:android:0.6.0")
+	implementation("com.github.jellyfin.jellyfin-apiclient-java:android:v0.6.0")
 
 	// Kotlin
 	implementation(kotlin("stdlib-jdk8"))
