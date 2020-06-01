2af454fa08d9116123cf8b41c42518d12f995b83
Niels van Velzen <git@ndat.nl>
2020-06-01 16:41:21 +0200

    Add Detekt support

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,17 @@ allprojects {
 		maven("https://dl.bintray.com/videolan/Android")
 	}
 }
+
+plugins {
+	id("io.gitlab.arturbosch.detekt").version("1.9.1")
+}
+
+// Detekt configuration
+subprojects {
+	plugins.apply("io.gitlab.arturbosch.detekt")
+
+	detekt {
+		buildUponDefaultConfig = true
+		config = files("$rootDir/detekt.yml")
+	}
+}
