432defd721f19ddf21cf868f1052c0507f11cb30
Niels van Velzen <git@ndat.nl>
2020-06-09 15:22:19 +0200

    Add forced attribute to FullHeightView

--- app/src/main/java/org/jellyfin/androidtv/ui/FullHeightView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/FullHeightView.kt
@@ -4,16 +4,25 @@ import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import org.jellyfin.androidtv.R
 
 /**
  * A view that limits its height to the window
  */
 class FullHeightView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+	private val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.FullHeightView, 0, 0)
 	private val measureRect = Rect()
+	private val forced = attributes.getBoolean(R.styleable.FullHeightView_forced, false)
 
 	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
 		val maximumHeight = measureRect.also { getWindowVisibleDisplayFrame(it) }.height()
-		val newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(maximumHeight, MeasureSpec.AT_MOST)
-		super.onMeasure(widthMeasureSpec, newHeightMeasureSpec)
+		val mode = if (forced) MeasureSpec.EXACTLY else MeasureSpec.AT_MOST
+
+		if (maximumHeight > 0) {
+			val newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(maximumHeight, mode)
+			super.onMeasure(widthMeasureSpec, newHeightMeasureSpec)
+		} else {
+			super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+		}
 	}
 }
