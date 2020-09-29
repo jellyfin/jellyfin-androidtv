3048b9570460c62aaeadccd010fcd11777ec4a42
Bill Thornton <billt2006@gmail.com>
2020-09-29 16:11:20 -0400

    Remove unneeded null check
    (cherry picked from commit b9d521d57e6180b87bfc59711aaa6eb075156804)

--- app/src/main/java/org/jellyfin/androidtv/base/KeyboardFocusChangeListener.kt
+++ app/src/main/java/org/jellyfin/androidtv/base/KeyboardFocusChangeListener.kt
@@ -5,12 +5,10 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 class KeyboardFocusChangeListener : View.OnFocusChangeListener {
-	override fun onFocusChange(v : View?, hasFocus : Boolean) {
-		if (v != null) {
-			(v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).apply {
-				if (!hasFocus) hideSoftInputFromWindow(v.windowToken, 0)
-				else showSoftInput(v, 0)
-			}
+	override fun onFocusChange(v : View, hasFocus : Boolean) {
+		(v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+			if (!hasFocus) hideSoftInputFromWindow(v.windowToken, 0)
+			else showSoftInput(v, 0)
 		}
 	}
 }
