279e261d3b2f4557b331e6ed5db86320f1dec3f1
Bill Thornton <billt2006@gmail.com>
2020-10-06 15:31:01 -0400

    Use constants for SDK versions and remove unneeded check

--- app/src/main/java/org/jellyfin/androidtv/ui/shared/BaseActivity.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/shared/BaseActivity.java
@@ -25,7 +25,7 @@ public abstract class BaseActivity extends FragmentActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus && android.os.Build.VERSION.SDK_INT >= 19) {
+        if (hasFocus) {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
