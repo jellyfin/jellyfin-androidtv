b72993126d014f8d46f6a90033b2a06295f81f21
Bill Thornton <billt2006@gmail.com>
2020-11-12 13:15:09 -0500

    Fix failing tests

--- app/src/test/java/org/jellyfin/androidtv/util/apiclient/ModelExtensionsTest.kt
+++ app/src/test/java/org/jellyfin/androidtv/util/apiclient/ModelExtensionsTest.kt
@@ -1,5 +1,6 @@
 package org.jellyfin.androidtv.util.apiclient
 
+import org.jellyfin.androidtv.data.model.LegacyServer
 import org.jellyfin.androidtv.data.model.Server
 import org.jellyfin.androidtv.data.model.User
 import org.jellyfin.androidtv.data.model.UserConfiguration
@@ -25,7 +26,7 @@ class ModelExtensionsTest {
 		private const val USER_NAME = "User Name"
 		private const val ACCESS_TOKEN = "token"
 		private const val IMAGE_TAG = "tag"
-		private val EXCLUDE_ITEMS = arrayOf("exclude")
+		private val EXCLUDE_ITEMS = listOf("exclude")
 	}
 
 	@Test
@@ -69,6 +70,22 @@ class ModelExtensionsTest {
 	}
 
 	@Test
+	fun serverInfoToLegacyServer() {
+		val server = ServerInfo().apply {
+			id = SERVER_ID
+			address = SERVER_URL
+			name = SERVER_NAME
+			userId = USER_ID
+			accessToken = ACCESS_TOKEN
+		}.toLegacyServer()
+		assertEquals(SERVER_ID, server.id)
+		assertEquals(SERVER_URL, server.address)
+		assertEquals(SERVER_NAME, server.name)
+		assertEquals(USER_ID, server.userId)
+		assertEquals(ACCESS_TOKEN, server.accessToken)
+	}
+
+	@Test
 	fun serverToServerInfo() {
 		val instant = LocalDate.of(2020, 10, 1)
 			.atStartOfDay(ZoneOffset.UTC)
@@ -77,9 +94,25 @@ class ModelExtensionsTest {
 			SERVER_ID,
 			SERVER_NAME,
 			SERVER_URL,
-			USER_ID,
-			ACCESS_TOKEN,
 			Date.from(instant)
+		).toServerInfo()
+		assertEquals(SERVER_ID, serverInfo.id)
+		assertEquals(SERVER_NAME, serverInfo.name)
+		assertEquals(SERVER_URL, serverInfo.address)
+		assertEquals(instant, serverInfo.dateLastAccessed.toInstant())
+	}
+	@Test
+	fun legacyServerToServerInfo() {
+		val instant = LocalDate.of(2020, 10, 1)
+			.atStartOfDay(ZoneOffset.UTC)
+			.toInstant()
+		val serverInfo = LegacyServer(
+			SERVER_ID,
+			SERVER_NAME,
+			SERVER_URL,
+			Date.from(instant),
+			USER_ID,
+			ACCESS_TOKEN
 		).toServerInfo()
 		assertEquals(SERVER_ID, serverInfo.id)
 		assertEquals(SERVER_NAME, serverInfo.name)
@@ -104,7 +137,7 @@ class ModelExtensionsTest {
 				enableLiveTvManagement = false
 			}
 			configuration = org.jellyfin.apiclient.model.configuration.UserConfiguration().apply {
-				latestItemsExcludes = EXCLUDE_ITEMS
+				latestItemsExcludes = EXCLUDE_ITEMS.toTypedArray()
 			}
 		}.toUser()
 		assertEquals(USER_ID, user.id)
@@ -116,7 +149,7 @@ class ModelExtensionsTest {
 		assertFalse(user.hasConfiguredEasyPassword)
 		assertFalse(user.policy.enableLiveTvAccess)
 		assertFalse(user.policy.enableLiveTvManagement)
-		assertTrue(EXCLUDE_ITEMS.contentEquals(user.configuration.latestItemsExcludes))
+		assertTrue(EXCLUDE_ITEMS == user.configuration.latestItemsExcludes)
 	}
 
 	@Test
@@ -144,7 +177,7 @@ class ModelExtensionsTest {
 		assertTrue(userDto.hasPassword)
 		assertTrue(userDto.hasConfiguredPassword)
 		assertFalse(userDto.hasConfiguredEasyPassword)
-		assertTrue(EXCLUDE_ITEMS.contentEquals(userDto.configuration.latestItemsExcludes))
+		assertTrue(EXCLUDE_ITEMS == userDto.configuration.latestItemsExcludes.asList())
 		assertFalse(userDto.policy.enableLiveTvAccess)
 		assertFalse(userDto.policy.enableLiveTvManagement)
 	}
