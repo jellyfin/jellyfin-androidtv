c705c32f9da3f01904cc7e33178ea8e580953ee0
Niels van Velzen <git@ndat.nl>
2021-01-08 23:21:08 +0100

    Add developer preferences screen

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/preference/screen/DeveloperPreferencesScreen.kt
@@ -0,0 +1,36 @@
+package org.jellyfin.androidtv.ui.preference.screen
+
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.preference.UserPreferences
+import org.jellyfin.androidtv.preference.constant.GridDirection
+import org.jellyfin.androidtv.ui.preference.dsl.OptionsFragment
+import org.jellyfin.androidtv.ui.preference.dsl.checkbox
+import org.jellyfin.androidtv.ui.preference.dsl.enum
+import org.jellyfin.androidtv.ui.preference.dsl.optionsScreen
+import org.koin.android.ext.android.inject
+
+class DeveloperPreferencesScreen : OptionsFragment() {
+	private val userPreferences: UserPreferences by inject()
+
+	override val screen by optionsScreen {
+		setTitle(R.string.pref_developer_link)
+
+		category {
+			// Legacy debug flag
+			// Not in use by much components anymore
+			checkbox {
+				setTitle(R.string.lbl_enable_debug)
+				setContent(R.string.desc_debug)
+				bind(userPreferences, UserPreferences.debuggingEnabled)
+			}
+
+			// Changing the grid direction is experimental as the vertical
+			// direction doesn't calculate image sizes properly resulting
+			// in a weird layout in some cases.
+			enum<GridDirection> {
+				setTitle(R.string.grid_direction)
+				bind(userPreferences, UserPreferences.gridDirection)
+			}
+		}
+	}
+}
