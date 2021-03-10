e74cd2422e0e8f54178271fd2883fc135890fe42
Bill Thornton <billt2006@gmail.com>
2021-03-10 12:40:30 -0500

    Fix nullability of ViewHolder

--- app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
@@ -3,7 +3,7 @@ package org.jellyfin.androidtv.ui.presentation
 import androidx.leanback.widget.RowHeaderPresenter
 
 class CustomRowHeaderPresenter : RowHeaderPresenter() {
-	override fun onSelectLevelChanged(holder: ViewHolder?) {
+	override fun onSelectLevelChanged(holder: ViewHolder) {
 		// Do nothing - this keeps headers opaque
 	}
 }
