c4632f7a31955fee87114d319db8781985ec5b93
Bill Thornton <billt2006@gmail.com>
2021-03-10 16:59:05 -0500

    Add landscape folder drawable for libraries

--- app/src/main/java/org/jellyfin/androidtv/ui/presentation/CardPresenter.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/presentation/CardPresenter.java
@@ -151,7 +151,7 @@ public class CardPresenter extends Presenter {
                             // Force the aspect ratio to 16x9 because the server is returning the wrong value of 1
                             // When this is fixed we should still force 16x9 if an image is not set to be consistent
                             aspect = ImageUtils.ASPECT_RATIO_16_9;
-                            mDefaultCardImage = ContextCompat.getDrawable(mCardView.getContext(), R.drawable.tile_port_folder);
+                            mDefaultCardImage = ContextCompat.getDrawable(mCardView.getContext(), R.drawable.tile_land_folder);
                             break;
                         case Folder:
                         case MovieGenreFolder:
