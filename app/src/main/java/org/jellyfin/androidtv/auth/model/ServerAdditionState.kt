307d0f29b9bb5a4dbb8d457ac7412b28f60d41e3
Niels van Velzen <git@ndat.nl>
2021-03-27 22:16:32 +0100

    Use the AddressCandidateHelper of the Kotlin SDK to normalize the inputted server address

--- app/src/main/java/org/jellyfin/androidtv/auth/model/ServerAdditionState.kt
+++ app/src/main/java/org/jellyfin/androidtv/auth/model/ServerAdditionState.kt
@@ -3,6 +3,6 @@ package org.jellyfin.androidtv.auth.model
 import org.jellyfin.apiclient.model.system.PublicSystemInfo
 
 sealed class ServerAdditionState
-object ConnectingState : ServerAdditionState()
+data class ConnectingState(val address: String) : ServerAdditionState()
 data class UnableToConnectState(val error: Exception) : ServerAdditionState()
 data class ConnectedState(val publicInfo: PublicSystemInfo) : ServerAdditionState()
