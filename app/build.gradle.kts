b363590220a2090dc49ae33709eb0feb8c346223
Niels van Velzen <git@ndat.nl>
2021-03-27 21:37:35 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,18 +71,15 @@ dependencies {
 	implementation("org.jellyfin.apiclient:android:0.7.9")
 
 	// Kotlin
-	implementation(kotlin("stdlib"))
-
-	val kotlinxCoroutinesVersion = "1.3.3"
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+	val kotlinxCoroutinesVersion = "1.4.3"
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
 
-	val kotlinxSerializationVersion = "1.0.1"
+	val kotlinxSerializationVersion = "1.1.0"
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
 
 	// Android(x)
 	implementation("androidx.core:core-ktx:1.3.2")
-	implementation("androidx.fragment:fragment-ktx:1.2.5")
+	implementation("androidx.fragment:fragment-ktx:1.3.2")
 	val androidxLeanbackVersion = "1.1.0-beta01"
 	implementation("androidx.leanback:leanback:${androidxLeanbackVersion}")
 	implementation("androidx.leanback:leanback-preference:${androidxLeanbackVersion}")
@@ -90,15 +87,15 @@ dependencies {
 	implementation("androidx.preference:preference:$androidxPreferenceVersion")
 	implementation("androidx.preference:preference-ktx:$androidxPreferenceVersion")
 	implementation("androidx.appcompat:appcompat:1.2.0")
-	implementation("androidx.tvprovider:tvprovider:1.0.0")
+	implementation("androidx.tvprovider:tvprovider:1.1.0-alpha01")
 	implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 	implementation("androidx.recyclerview:recyclerview:1.1.0")
 	implementation("androidx.work:work-runtime-ktx:2.5.0")
 	implementation("com.google.android:flexbox:2.0.1")
-	val androidxLifecycleVersion = "2.2.0"
+	val androidxLifecycleVersion = "2.3.1"
 	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifecycleVersion")
 	implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidxLifecycleVersion")
-	implementation("androidx.window:window:1.0.0-alpha02")
+	implementation("androidx.window:window:1.0.0-alpha05")
 	implementation("androidx.viewpager:viewpager:1.0.0")
 
 	// Dependency Injection
@@ -113,11 +110,11 @@ dependencies {
 
 	// Media players
 	implementation("com.amazon.android:exoplayer:2.11.3")
-	implementation("org.videolan.android:libvlc-all:3.3.2")
+	implementation("org.videolan.android:libvlc-all:3.3.14")
 
 	// Image utility
 	implementation("com.github.bumptech.glide:glide:4.12.0")
-	implementation("com.flaviofaria:kenburnsview:1.0.6")
+	implementation("com.flaviofaria:kenburnsview:1.0.7")
 
 	// Crash Reporting
 	val acraVersion = "5.7.0"
