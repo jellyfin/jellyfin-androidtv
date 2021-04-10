ed358c37164e77b31d6a4bef94f3de7f516da8b4
Niels van Velzen <git@ndat.nl>
2021-04-10 14:58:22 +0200

    Rebuild AuthPreferencesScreen when server is removed

--- app/src/main/java/org/jellyfin/androidtv/ui/preference/screen/AuthPreferencesScreen.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/preference/screen/AuthPreferencesScreen.kt
@@ -6,14 +6,18 @@ import org.jellyfin.androidtv.preference.AuthenticationPreferences
 import org.jellyfin.androidtv.ui.preference.category.authenticationCategory
 import org.jellyfin.androidtv.ui.preference.category.manageServersCategory
 import org.jellyfin.androidtv.ui.preference.dsl.OptionsFragment
-import org.jellyfin.androidtv.ui.preference.dsl.lazyOptionsScreen
+import org.jellyfin.androidtv.ui.preference.dsl.optionsScreen
 import org.koin.android.ext.android.inject
 
 class AuthPreferencesScreen : OptionsFragment() {
 	private val authenticationRepository: AuthenticationRepository by inject()
 	private val authenticationPreferences: AuthenticationPreferences by inject()
 
-	override val screen by lazyOptionsScreen {
+	init {
+		rebuildOnResume = true
+	}
+
+	override val screen get() = optionsScreen {
 		setTitle(R.string.pref_authentication_cat)
 
 		authenticationCategory(authenticationRepository, authenticationPreferences)
