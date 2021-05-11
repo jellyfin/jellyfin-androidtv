d61b118ca8a36d5dcde98a12a185ecc736405324
Bill Thornton <billt2006@gmail.com>
2021-05-11 00:57:17 -0400

    Disable automatically opening the keyboard on Fire OS

--- app/src/main/java/org/jellyfin/androidtv/ui/shared/KeyboardFocusChangeListener.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/shared/KeyboardFocusChangeListener.kt
@@ -3,9 +3,14 @@ package org.jellyfin.androidtv.ui.shared
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.getSystemService
+import org.jellyfin.androidtv.util.DeviceUtils
 
 class KeyboardFocusChangeListener : View.OnFocusChangeListener {
 	override fun onFocusChange(view: View, hasFocus: Boolean) {
+		// The Fire OS on-screen keyboard blocks virtually all content on screen,
+		// so require manually opening the keyboard there
+		if (DeviceUtils.isFireTv()) return
+
 		view.context.getSystemService<InputMethodManager>()?.apply {
 			if (!hasFocus) hideSoftInputFromWindow(view.windowToken, 0)
 			else showSoftInput(view, 0)
