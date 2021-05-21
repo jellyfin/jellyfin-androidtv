6b6a762c240c88c05712c8434a9ec774f2f3ac9c
Niels van Velzen <git@ndat.nl>
2021-05-21 22:26:47 +0200

    Add KtorClient instances for user and system and apply sessions in SessionRepository
    This allows the use of the new SDK while keeping the old apiclient working

--- app/src/main/java/org/jellyfin/androidtv/di/AuthModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/di/AuthModule.kt
@@ -11,7 +11,7 @@ val authModule = module {
 	single { AuthenticationStore(androidContext()) }
 	single { AccountManagerHelper(androidContext().getSystemService()!!) }
 	single<AuthenticationRepository> { AuthenticationRepositoryImpl(get(), get(), get(), get(), get()) }
-	single<SessionRepository> { SessionRepositoryImpl(get(), get(), get()) }
+	single<SessionRepository> { SessionRepositoryImpl(get(), get(), get(), get(), get(userApiClient), get(systemApiClient)) }
 	single { LegacyAccountMigration(androidContext(), get(), get()) }
 	single { ApiBinder(androidApplication() as JellyfinApplication, get(), get(), get()) }
 }
