4db644aaeba16f7366b37b17163740ee39c0132a
Niels van Velzen <git@ndat.nl>
2021-06-15 15:28:21 +0200

    Add help and settings buttons to startup toolbar

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
@@ -1,12 +1,31 @@
 package org.jellyfin.androidtv.ui.startup.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.jellyfin.androidtv.databinding.FragmentToolbarStartupBinding
+import org.jellyfin.androidtv.ui.preference.PreferencesActivity
+import org.jellyfin.androidtv.ui.preference.screen.AuthPreferencesScreen
 
 class StartupToolbarFragment : Fragment() {
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-		FragmentToolbarStartupBinding.inflate(inflater, container, false).root
+	private lateinit var binding: FragmentToolbarStartupBinding
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		binding = FragmentToolbarStartupBinding.inflate(inflater, container, false)
+
+		binding.help.setOnClickListener {
+			// TODO: What do
+		}
+
+		binding.settings.setOnClickListener {
+			val intent = Intent(requireActivity(), PreferencesActivity::class.java)
+			intent.putExtra(PreferencesActivity.EXTRA_SCREEN, AuthPreferencesScreen::class.qualifiedName)
+			startActivity(intent)
+		}
+
+		return binding.root
+	}
 }
