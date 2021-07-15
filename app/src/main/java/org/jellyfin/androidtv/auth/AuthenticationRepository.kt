b328f4126ea5e275cb3272e01ef9a5b8eaeda98d
Bill Thornton <billt2006@gmail.com>
2021-07-15 16:43:55 -0400

    Migrate AuthenticationRepository to sdk

--- app/src/main/java/org/jellyfin/androidtv/auth/AuthenticationRepository.kt
+++ app/src/main/java/org/jellyfin/androidtv/auth/AuthenticationRepository.kt
@@ -6,13 +6,13 @@ import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import org.jellyfin.androidtv.auth.model.*
 import org.jellyfin.androidtv.util.ImageUtils
-import org.jellyfin.androidtv.util.apiclient.callApi
-import org.jellyfin.apiclient.Jellyfin
 import org.jellyfin.apiclient.interaction.device.IDevice
-import org.jellyfin.apiclient.model.dto.ImageOptions
-import org.jellyfin.apiclient.model.entities.ImageType
-import org.jellyfin.apiclient.model.users.AuthenticationResult
-import org.jellyfin.sdk.model.serializer.toUUID
+import org.jellyfin.sdk.Jellyfin
+import org.jellyfin.sdk.api.client.exception.ApiClientException
+import org.jellyfin.sdk.api.client.extensions.authenticateUserByName;
+import org.jellyfin.sdk.api.operations.ImageApi
+import org.jellyfin.sdk.api.operations.UserApi
+import org.jellyfin.sdk.model.api.ImageType
 import timber.log.Timber
 import java.util.*
 
@@ -165,30 +165,30 @@ class AuthenticationRepositoryImpl(
 			return@flow
 		}
 
+		val api = jellyfin.createApi(server.address, deviceInfo = AuthenticationDevice(device, username).toDeviceInfo())
+		val userApi = UserApi(api)
 		val result = try {
-			callApi<AuthenticationResult> { callback ->
-				val api = jellyfin.createApi(server.address, device = AuthenticationDevice(device, username))
-				api.AuthenticateUserAsync(username, password, callback)
-			}
-
-			// Supress because com.android.volley.AuthFailureError is not exposed by the apiclient
-		} catch (@Suppress("TooGenericExceptionCaught") err: Exception) {
+			val response = userApi.authenticateUserByName(username, password)
+			response.content
+		} catch (err: ApiClientException) {
 			Timber.e(err, "Unable to sign in as $username")
 			emit(RequireSignInState)
 			return@flow
 		}
 
-		val userId = result.user.id.toUUID()
+		val userInfo = result.user ?: return@flow emit(RequireSignInState)
+
+		val userId = userInfo.id
 		val currentUser = authenticationStore.getUser(server.id, userId)
 		val updatedUser = currentUser?.copy(
-			name = result.user.name,
+			name = userInfo.name!!,
 			lastUsed = Date().time,
-			requirePassword = result.user.hasPassword,
-			imageTag = result.user.primaryImageTag
+			requirePassword = userInfo.hasPassword,
+			imageTag = userInfo.primaryImageTag
 		) ?: AuthenticationStoreUser(
-			name = result.user.name,
-			requirePassword = result.user.hasPassword,
-			imageTag = result.user.primaryImageTag
+			name = userInfo.name!!,
+			requirePassword = userInfo.hasPassword,
+			imageTag = userInfo.primaryImageTag
 		)
 
 		authenticationStore.putUser(server.id, userId, updatedUser)
@@ -199,8 +199,8 @@ class AuthenticationRepositoryImpl(
 			serverId = server.id,
 			name = updatedUser.name,
 			accessToken = result.accessToken,
-			requirePassword = result.user.hasPassword,
-			imageTag = result.user.primaryImageTag,
+			requirePassword = userInfo.hasPassword,
+			imageTag = userInfo.primaryImageTag,
 			lastUsed = Date().time,
 		)
 		setActiveSession(user, server)
@@ -217,11 +217,14 @@ class AuthenticationRepositoryImpl(
 	}
 
 	override fun getUserImageUrl(server: Server, user: User): String? {
-		val apiClient = jellyfin.createApi(serverAddress = server.address, device = device)
-		return apiClient.GetUserImageUrl(user.id.toString(), ImageOptions().apply {
-			tag = user.imageTag
-			imageType = ImageType.Primary
+		val api = jellyfin.createApi(baseUrl = server.address)
+		val imageApi = ImageApi(api)
+
+		return imageApi.getUserImageUrl(
+			userId = user.id,
+			tag = user.imageTag,
+			imageType = ImageType.PRIMARY,
 			maxHeight = ImageUtils.MAX_PRIMARY_IMAGE_HEIGHT
-		})
+		)
 	}
 }
