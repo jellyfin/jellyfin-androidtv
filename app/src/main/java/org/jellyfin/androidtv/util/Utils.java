e44a7490a71f4f426197924c5f0ffce831c050ad
Niels van Velzen <git@ndat.nl>
2021-07-29 14:26:27 +0200

    Remove utility functions only used by tests

--- app/src/main/java/org/jellyfin/androidtv/util/Utils.java
+++ app/src/main/java/org/jellyfin/androidtv/util/Utils.java
@@ -111,33 +111,6 @@ public class Utils {
         return join(separator, Arrays.asList(items));
     }
 
-    public static boolean versionGreaterThanOrEqual(String firstVersion, String secondVersion) {
-        try {
-            String[] firstVersionComponents = firstVersion.split("[.]");
-            String[] secondVersionComponents = secondVersion.split("[.]");
-            int firstLength = firstVersionComponents.length;
-            int secondLength = secondVersionComponents.length;
-            int firstMajor = firstLength > 0 ? Integer.parseInt(firstVersionComponents[0]) : 0;
-            int secondMajor = secondLength > 0 ? Integer.parseInt(secondVersionComponents[0]) : 0;
-            int firstMinor = firstLength > 1 ? Integer.parseInt(firstVersionComponents[1]) : 0;
-            int secondMinor = secondLength > 1 ? Integer.parseInt(secondVersionComponents[1]) : 0;
-            int firstBuild = firstLength > 2 ? Integer.parseInt(firstVersionComponents[2]) : 0;
-            int secondBuild = secondLength > 0 ? Integer.parseInt(secondVersionComponents[2]) : 0;
-            int firstRelease = firstLength > 3 ? Integer.parseInt(firstVersionComponents[3]) : 0;
-            int secondRelease = secondLength > 3 ? Integer.parseInt(secondVersionComponents[3]) : 0;
-
-            if (firstMajor < secondMajor) return false;
-            if (firstMajor == secondMajor && firstMinor < secondMinor) return false;
-            if (firstMajor == secondMajor && firstMinor == secondMinor && firstBuild < secondBuild) return false;
-            if (firstMajor == secondMajor && firstMinor == secondMinor && firstBuild == secondBuild && firstRelease < secondRelease) return false;
-
-            return true;
-        } catch (Exception e) {
-            Timber.e(e);
-            return false;
-        }
-    }
-
     public static int getMaxBitrate() {
         String maxRate = get(UserPreferences.class).get(UserPreferences.Companion.getMaxBitrate());
         Long autoRate = get(AutoBitrate.class).getBitrate();
--- app/src/test/java/org/jellyfin/androidtv/util/TimeUtilsTest.java
+++ app/src/test/java/org/jellyfin/androidtv/util/TimeUtilsTest.java
@@ -1,8 +1,8 @@
 package org.jellyfin.androidtv.util;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class TimeUtilsTest {
 
@@ -11,20 +11,6 @@ public class TimeUtilsTest {
         assertEquals(0, TimeUtils.secondsToMillis(0));
         assertEquals(1000, TimeUtils.secondsToMillis(1));
         assertEquals(1250, TimeUtils.secondsToMillis(1.25));
-    }
-
-    @Test
-    public void minutesToMillis() {
-        assertEquals(0, TimeUtils.minutesToMillis(0));
-        assertEquals(60000, TimeUtils.minutesToMillis(1));
-        assertEquals(75000, TimeUtils.minutesToMillis(1.25));
-    }
-
-    @Test
-    public void hoursToMillis() {
-        assertEquals(0, TimeUtils.hoursToMillis(0));
-        assertEquals(3600000, TimeUtils.hoursToMillis(1));
-        assertEquals(4500000, TimeUtils.hoursToMillis(1.25));
     }
 
     @Test
