f7d3c7154d42c292c431ff9f220178c78b315e8e
Niels van Velzen <git@ndat.nl>
2021-08-22 16:49:49 +0200

    Fix VLC error reporting

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/VlcEventHandler.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/VlcEventHandler.java
@@ -9,10 +9,21 @@ public class VlcEventHandler implements org.videolan.libvlc.MediaPlayer.EventLis
     private PlaybackListener onPreparedListener;
     private PlaybackListener onProgressListener;
 
-    public void setOnCompletionListener(PlaybackListener listener) { onCompletionListener = listener; }
-    public void setOnErrorListener(PlaybackListener listener) { onErrorListener = listener; }
-    public void setOnPreparedListener(PlaybackListener listener) { onPreparedListener = listener; }
-    public void setOnProgressListener(PlaybackListener listener) { onProgressListener = listener; }
+    public void setOnCompletionListener(PlaybackListener listener) {
+        onCompletionListener = listener;
+    }
+
+    public void setOnErrorListener(PlaybackListener listener) {
+        onErrorListener = listener;
+    }
+
+    public void setOnPreparedListener(PlaybackListener listener) {
+        onPreparedListener = listener;
+    }
+
+    public void setOnProgressListener(PlaybackListener listener) {
+        onProgressListener = listener;
+    }
 
     @Override
     public void onEvent(Event event) {
@@ -26,6 +37,8 @@ public class VlcEventHandler implements org.videolan.libvlc.MediaPlayer.EventLis
             case Event.PositionChanged:
                 if (onProgressListener != null) onProgressListener.onEvent();
                 break;
+            case Event.EncounteredError:
+                if (onErrorListener != null) onErrorListener.onEvent();
             case Event.Paused:
             case Event.Stopped:
             default:
