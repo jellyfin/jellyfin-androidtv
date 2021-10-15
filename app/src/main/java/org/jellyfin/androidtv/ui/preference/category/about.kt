460e794215c79931610da74fbf8ee1b2dd7e7260
Niels van Velzen <git@ndat.nl>
2021-10-15 19:23:00 +0200

    Use link in about section

--- app/src/main/java/org/jellyfin/androidtv/ui/preference/category/about.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/preference/category/about.kt
@@ -3,7 +3,6 @@ package org.jellyfin.androidtv.ui.preference.category
 import android.os.Build
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.preference.dsl.OptionsScreen
-import org.jellyfin.androidtv.ui.preference.dsl.info
 import org.jellyfin.androidtv.ui.preference.dsl.link
 import org.jellyfin.androidtv.ui.preference.screen.LicensesScreen
 import org.jellyfin.androidtv.util.Utils
@@ -11,12 +10,12 @@ import org.jellyfin.androidtv.util.Utils
 fun OptionsScreen.aboutCategory() = category {
 	setTitle(R.string.pref_about_title)
 
-	info {
+	link {
 		setTitle(R.string.lbl_version)
 		content = Utils.getVersionString(context)
 	}
 
-	info {
+	link {
 		setTitle(R.string.pref_device_model)
 		content = "${Build.MANUFACTURER} ${Build.MODEL}"
 	}
