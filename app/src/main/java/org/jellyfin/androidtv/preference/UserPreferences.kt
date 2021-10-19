4ebc3e1f50df3c55092153e81d4ef625c7c3d55a
Niels van Velzen <git@ndat.nl>
2021-10-19 22:27:31 +0200

    Change default video player to ExoPlayer instead of auto detect
    (cherry picked from commit 65f4b75673bb51cd87a7afbf5782996c0bb9fe0a)

--- app/src/main/java/org/jellyfin/androidtv/preference/UserPreferences.kt
+++ app/src/main/java/org/jellyfin/androidtv/preference/UserPreferences.kt
@@ -4,14 +4,7 @@ import android.content.Context
 import android.view.KeyEvent
 import androidx.preference.PreferenceManager
 import org.acra.ACRA
-import org.jellyfin.androidtv.preference.constant.AppTheme
-import org.jellyfin.androidtv.preference.constant.AudioBehavior
-import org.jellyfin.androidtv.preference.constant.ClockBehavior
-import org.jellyfin.androidtv.preference.constant.NextUpBehavior
-import org.jellyfin.androidtv.preference.constant.PreferredVideoPlayer
-import org.jellyfin.androidtv.preference.constant.RatingType
-import org.jellyfin.androidtv.preference.constant.WatchedIndicatorBehavior
-import org.jellyfin.androidtv.preference.constant.defaultAudioBehavior
+import org.jellyfin.androidtv.preference.constant.*
 import org.jellyfin.androidtv.util.DeviceUtils
 
 /**
@@ -87,7 +80,7 @@ class UserPreferences(context: Context) : SharedPreferenceStore(
 		/**
 		 * Preferred video player.
 		 */
-		var videoPlayer = Preference.enum("video_player", PreferredVideoPlayer.AUTO)
+		var videoPlayer = Preference.enum("video_player", PreferredVideoPlayer.EXOPLAYER)
 
 		/**
 		 * Enable refresh rate switching when device supports it
