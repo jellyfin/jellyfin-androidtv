d639226b6ce1136f569a313c6d616ed264ac4ee3
mueslimaker <cameronlambert98@gmail.com>
2021-10-22 01:42:04 -0700

    dpad no longer calls skip. seek updates current pos
    the new player scrubs seekbar when these dpad buttons are called. Continuing to call seek appears to cause issues with the timestamps
    
    update playback controller position with seek pos

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
@@ -869,6 +869,10 @@ public class PlaybackController {
         if (mPlaybackMethod == PlayMethod.Transcode && ContainerTypes.MKV.equals(mCurrentStreamInfo.getContainer())) {
             //mkv transcodes require re-start of stream for seek
             mVideoManager.stopPlayback();
+            // tell the server that the current position is the seeked position. I don't know if this line is needed in addition to the line that updates mCurrentPosition
+            ReportingHelper.reportProgress(this, getCurrentlyPlayingItem(), getCurrentStreamInfo(), pos * 10000, false);
+            // update mCurrentPosition because when play() is called after seeking it uses its value
+            mCurrentPosition = pos;
             playbackManager.getValue().changeVideoStream(mCurrentStreamInfo, apiClient.getValue().getServerInfo().getId(), mCurrentOptions, pos * 10000, apiClient.getValue(), new Response<StreamInfo>() {
                 @Override
                 public void onResponse(StreamInfo response) {
