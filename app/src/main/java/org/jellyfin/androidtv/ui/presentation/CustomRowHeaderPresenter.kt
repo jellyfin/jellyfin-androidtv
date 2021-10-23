6065230f45551305183bbbd5e2cea6f961541ced
linetrimmer <46704654+linetrimmer@users.noreply.github.com>
2021-10-22 22:06:00 -0400

    Set default value for argument in CustomRowHeaderPresenter

--- app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
@@ -2,12 +2,10 @@ package org.jellyfin.androidtv.ui.presentation
 
 import androidx.leanback.widget.RowHeaderPresenter
 
-class CustomRowHeaderPresenter(private val homeHeaderEnabled: Boolean) : RowHeaderPresenter() {
-	constructor() : this(false)
-
+class CustomRowHeaderPresenter(private val homeHeaderEnabled: Boolean = false) : RowHeaderPresenter() {
 	override fun onSelectLevelChanged(holder: ViewHolder) {
 		// Do nothing - this keeps headers opaque
-		// Unless on home screen - add headers back manually
+		// Unless on home screen and the item preview is enabled - add headers back manually
 		if (homeHeaderEnabled) holder.view.alpha = 0f
 	}
 }
