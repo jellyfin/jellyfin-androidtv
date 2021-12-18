3095d7536c1556366e384e24c80c20ba081c644b
Niels van Velzen <git@ndat.nl>
2021-12-18 23:46:58 +0100

    Move Gradle plugins to plugin catalog

--- playback/build.gradle.kts
+++ playback/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-	compileSdkVersion(30)
+	compileSdk = 30
 
 	defaultConfig {
-		minSdkVersion(21)
-		targetSdkVersion(30)
+		minSdk = 21
+		targetSdk = 30
 	}
 
 	buildFeatures {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,39 @@ include(":app")
 
 // Modules
 include(":playback")
+
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		mavenCentral()
+		google()
+	}
+}
+
+dependencyResolutionManagement {
+	repositories {
+		mavenCentral()
+		google()
+
+		// Jellyfin SDK
+		mavenLocal {
+			content {
+				includeVersionByRegex("org.jellyfin.sdk", ".*", "latest-SNAPSHOT")
+			}
+		}
+		maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+			content {
+				includeVersionByRegex("org.jellyfin.sdk", ".*", "master-SNAPSHOT")
+				includeVersionByRegex("org.jellyfin.sdk", ".*", "openapi-unstable-SNAPSHOT")
+			}
+		}
+
+		// Jellyfin apiclient
+		maven("https://jitpack.io") {
+			content {
+				// Only allow legacy apiclient
+				includeVersionByRegex("com.github.jellyfin.jellyfin-sdk-kotlin", ".*", "v0.7.10")
+			}
+		}
+	}
+}
