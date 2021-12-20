5788167dfe6660f3c231ae3b1b30acb0f625d63e
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-20 15:14:42 +0000

    Switch to using Enums to representing speed
    This makes it much easier to compare for equality and has built in
    indexing making the menu much easier to work with.
    
    In comparison using doubles directly would have required us to add a
    comparison lambda in the view to correctly detect what was selected /
    determine what is selected.

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/VideoSpeedController.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/VideoSpeedController.kt
@@ -2,18 +2,37 @@ package org.jellyfin.androidtv.ui.playback
 
 class VideoSpeedController(playbackController: PlaybackController) {
 	companion object {
-		val speedSteps = doubleArrayOf(0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)
-		var mostRecentSpeed = 1.0
+		enum class SpeedSteps(val value: Double) {
+			SPEED_0_25(0.25),
+			SPEED_0_50(0.5),
+			SPEED_0_75(0.75),
+			SPEED_1_00(1.0),
+			SPEED_1_25(1.25),
+			SPEED_1_50(1.50),
+			SPEED_1_75(1.75),
+			SPEED_2_00(2.0),
+		}
+		private var previousSpeedSelection = SpeedSteps.SPEED_1_00
+		fun resetPreviousSpeedToDefault(){
+			previousSpeedSelection = SpeedSteps.SPEED_1_00
+		}
 	}
 
 	private val parentController = playbackController
-
 	init {
-		parentController.setPlaybackSpeed(mostRecentSpeed)
+		// Carry forward the user's recent speed selection onto the next video(s)
+		setNewSpeed(previousSpeedSelection)
 	}
 
-	fun setNewSpeed(speed: Double) {
-		mostRecentSpeed = speed
-		parentController.setPlaybackSpeed(speed)
+	fun getCurrentSpeed(): SpeedSteps{
+		// Currently getCurrentSpeed uses previousSpeedSelection (from the companion)
+		// but this is an implementation detail I'd rather not leak in-case we ever need
+		// to separate out the two details. So implement a custom named getter...
+		return previousSpeedSelection
+	}
+
+	fun setNewSpeed(selectedSpeed: SpeedSteps) {
+		previousSpeedSelection = selectedSpeed
+		parentController.setPlaybackSpeed(selectedSpeed.value)
 	}
 }
--- app/src/test/java/org/jellyfin/androidtv/ui/playback/VideoSpeedControllerTest.kt
+++ app/src/test/java/org/jellyfin/androidtv/ui/playback/VideoSpeedControllerTest.kt
@@ -4,21 +4,22 @@ import io.mockk.*
 import org.junit.After
 import org.junit.Test
 import org.junit.Assert.*
+import kotlin.math.exp
 
 class VideoSpeedControllerTest {
 	@After
 	fun tearDown() {
 		// Always reset the "user selected" speed back to default
-		VideoSpeedController.mostRecentSpeed = 1.0
+		VideoSpeedController.resetPreviousSpeedToDefault()
 	}
 
 	@Test
 	fun testSpeedSteps() {
-		val speedSteps = VideoSpeedController.speedSteps
+		val speedSteps = VideoSpeedController.Companion.SpeedSteps.values()
 		val expectedStep = 0.25
 		var i = 1
 		speedSteps.forEach { v ->
-			assertEquals(i * expectedStep, v, 0.001)
+			assertEquals(i * expectedStep, v.value, 0.001)
 			i += 1
 		}
 	}
@@ -36,24 +37,49 @@ class VideoSpeedControllerTest {
 	}
 
 	@Test
-	fun testSetNewSpeed() {
+	fun testSetNewSpeed(){
+		val mockController = mockk<PlaybackController>(relaxed = true)
+		val controller = VideoSpeedController(mockController)
+		val expected = VideoSpeedController.Companion.SpeedSteps.SPEED_1_25
+		controller.setNewSpeed(expected)
+		assertEquals(expected, controller.getCurrentSpeed())
+	}
+
+	@Test
+	fun testSetNewSpeedSetsOnManager() {
 		val mockController = mockk<PlaybackController>()
 		val slot = slot<Double>()
 		justRun { mockController.setPlaybackSpeed(capture(slot)) }
 
 		val controller = VideoSpeedController(mockController)
-		val expected = 0.567
+		val expected = VideoSpeedController.Companion.SpeedSteps.SPEED_1_75
 		controller.setNewSpeed(expected)
 
 		verify { mockController.setPlaybackSpeed(any()) }
-		assertEquals(expected, slot.captured, 0.0001)
+		assertEquals(expected.value, slot.captured, 0.0001)
 	}
+
+	@Test
+	fun testResetPreviousSpeedToDefault() {
+		val mockController = mockk<PlaybackController>(relaxed = true)
+		VideoSpeedController(mockController).setNewSpeed(VideoSpeedController.Companion.SpeedSteps.SPEED_2_00)
+		VideoSpeedController.resetPreviousSpeedToDefault()
+
+		val slot = slot<Double>()
+		justRun { mockController.setPlaybackSpeed(capture(slot)) }
+		VideoSpeedController(mockController)
+
+		verify { mockController.setPlaybackSpeed(any()) }
+		assertEquals(VideoSpeedController.Companion.SpeedSteps.SPEED_1_00.value, slot.captured, 0.0001)
+	}
+
 
 	@Test
 	fun testControllerPreservesMostRecentlySelectedSpeedConstructingNew() {
 		var lastSetSpeed = 1.0
+		val speeds = VideoSpeedController.Companion.SpeedSteps.values()
 
-		for (i in 1..5) {
+		speeds.forEach { newSpeed ->
 			val mockController = mockk<PlaybackController>()
 			val slot = slot<Double>()
 			justRun { mockController.setPlaybackSpeed(capture(slot)) }
@@ -63,9 +89,8 @@ class VideoSpeedControllerTest {
 			verify { mockController.setPlaybackSpeed(any()) }
 			assertEquals(lastSetSpeed, slot.captured, 0.001)
 
-			val newSpeed = i.toDouble() / 10
 			controller.setNewSpeed(newSpeed)
-			lastSetSpeed = newSpeed
+			lastSetSpeed = newSpeed.value
 		}
 	}
 
