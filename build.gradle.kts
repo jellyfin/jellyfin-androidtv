480a71d1a6e4a18106be7fa33b600c1bc4b617f3
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-23 13:27:20 +0000

    Emit JUnit output whilst running gradle through CLI
    Tweaks the gradle script to forward on test failures in a way that the
    stack trace is preserved.
    
    Previously, the stack pointed to a line after the test suite, e.g. line
    101 on an 80 line file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,20 @@ subprojects {
 		}
 	}
 }
+
+tasks.withType<Test> {
+	// Ensure Junit emits the full stack trace when a unit test fails through gradle
+	useJUnit()
+
+	testLogging {
+		events(
+			org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+			org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
+			org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+		)
+		exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+		showExceptions = true
+		showCauses = true
+		showStackTraces = true
+	}
+}
