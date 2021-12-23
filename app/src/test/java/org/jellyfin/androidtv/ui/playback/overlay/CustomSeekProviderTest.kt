8ef549d2ad7341c8dd990b2fb48201e67d53310c
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-23 14:25:56 +0000

    Convert Mockito to Mockk
    Converts the single usage of Mockito to Mockk, so we can use the same
    mocking framework throughout the codebase.

--- app/src/test/java/org/jellyfin/androidtv/ui/playback/overlay/CustomSeekProviderTest.kt
+++ app/src/test/java/org/jellyfin/androidtv/ui/playback/overlay/CustomSeekProviderTest.kt
@@ -1,17 +1,16 @@
 package org.jellyfin.androidtv.ui.playback.overlay
 
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert
 import org.junit.Test
-import org.mockito.Mockito
 
 class CustomSeekProviderTest {
 	@Test
 	fun testGetSeekPositions_withSimpleDuration() {
-		val videoPlayerAdapter = Mockito.mock(
-			VideoPlayerAdapter::class.java
-		)
-		Mockito.`when`(videoPlayerAdapter.canSeek()).thenReturn(true)
-		Mockito.`when`(videoPlayerAdapter.duration).thenReturn(90000L)
+		val videoPlayerAdapter = mockk<VideoPlayerAdapter>()
+		every { videoPlayerAdapter.canSeek() } returns true
+		every { videoPlayerAdapter.duration } returns 90000L
 		val customSeekProvider = CustomSeekProvider(videoPlayerAdapter)
 		val expected = longArrayOf(0L, 30000L, 60000L, 90000L)
 		val actual = customSeekProvider.seekPositions
@@ -20,11 +19,9 @@ class CustomSeekProviderTest {
 
 	@Test
 	fun testGetSeekPositions_withOddDuration() {
-		val videoPlayerAdapter = Mockito.mock(
-			VideoPlayerAdapter::class.java
-		)
-		Mockito.`when`(videoPlayerAdapter.canSeek()).thenReturn(true)
-		Mockito.`when`(videoPlayerAdapter.duration).thenReturn(130000L)
+		val videoPlayerAdapter = mockk<VideoPlayerAdapter>()
+		every { videoPlayerAdapter.canSeek() } returns true
+		every { videoPlayerAdapter.duration } returns 130000L
 		val customSeekProvider = CustomSeekProvider(videoPlayerAdapter)
 		val expected = longArrayOf(0L, 30000, 60000, 90000, 120000, 130000)
 		val actual = customSeekProvider.seekPositions
@@ -33,10 +30,8 @@ class CustomSeekProviderTest {
 
 	@Test
 	fun testGetSeekPositions_withSeekDisabled() {
-		val videoPlayerAdapter = Mockito.mock(
-			VideoPlayerAdapter::class.java
-		)
-		Mockito.`when`(videoPlayerAdapter.canSeek()).thenReturn(false)
+		val videoPlayerAdapter = mockk<VideoPlayerAdapter>()
+		every { videoPlayerAdapter.canSeek() } returns false
 		val customSeekProvider = CustomSeekProvider(videoPlayerAdapter)
 		val actual = customSeekProvider.seekPositions
 		Assert.assertEquals(0, actual.size.toLong())
