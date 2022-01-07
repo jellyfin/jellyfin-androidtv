7bd92d6b19bb6e847c6527c8a69a99bfcbd26edf
Niels van Velzen <git@ndat.nl>
2022-01-07 17:15:47 +0100

    Collect server issues in ServerRepository.add

--- app/src/main/java/org/jellyfin/androidtv/auth/model/ServerAdditionState.kt
+++ app/src/main/java/org/jellyfin/androidtv/auth/model/ServerAdditionState.kt
@@ -1,9 +1,10 @@
 package org.jellyfin.androidtv.auth.model
 
+import org.jellyfin.sdk.discovery.RecommendedServerIssue
 import org.jellyfin.sdk.model.api.PublicSystemInfo
 import java.util.*
 
 sealed class ServerAdditionState
 data class ConnectingState(val address: String) : ServerAdditionState()
-data class UnableToConnectState(val addressCandidates: List<String>) : ServerAdditionState()
+data class UnableToConnectState(val addressCandidates: Map<String, Collection<RecommendedServerIssue>>) : ServerAdditionState()
 data class ConnectedState(val id: UUID, val publicInfo: PublicSystemInfo) : ServerAdditionState()
