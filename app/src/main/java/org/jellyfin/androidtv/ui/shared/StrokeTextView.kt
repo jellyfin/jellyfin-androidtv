7d342dd388302c057cf7b9b595ae039d91292fd7
siankatabg <siankata91@gmail.com>
2022-01-15 20:13:55 +0200

    Code fixes

--- app/src/main/java/org/jellyfin/androidtv/ui/shared/StrokeTextView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/shared/StrokeTextView.kt
@@ -8,60 +8,39 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import org.jellyfin.androidtv.R
 
-class StrokeTextView : AppCompatTextView {
-	private var strokeWidth = 0.0f
+class StrokeTextView @JvmOverloads constructor(
+	context: Context,
+	attrs: AttributeSet? = null,
+) : AppCompatTextView(context, attrs) {
+	var strokeWidth = 0.0f
 	private var isDrawing: Boolean = false
 
-	constructor(
-		context: Context,
-		attrs: AttributeSet?,
-		defStyle: Int
-	) : super(context, attrs, defStyle) {
-		obtainStyledAttrs(context, attrs)
-	}
-
-	constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-		obtainStyledAttrs(context, attrs)
-	}
-
-	constructor(context: Context) : super(context) {
-		obtainStyledAttrs(context, attrs = null)
-	}
-
-	private fun obtainStyledAttrs(context: Context, attrs: AttributeSet?) {
+	init {
 		val styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.StrokeTextView)
-		strokeWidth = styledAttrs.getFloat(
-			R.styleable.StrokeTextView_stroke_width, 0.0f
-		)
-		styledAttrs.recycle()
+		strokeWidth = styledAttrs.getFloat(R.styleable.StrokeTextView_stroke_width, 0.0f)
 	}
 
-	// To prevent infinite call of onDraw because setTextColor calls invalidate()
 	override fun invalidate() {
+		// To prevent infinite call of onDraw because setTextColor calls invalidate()
 		if (isDrawing) return
 		super.invalidate()
 	}
 
-	fun setStrokeWidth(strokeWidth: Float){
-		this.strokeWidth = strokeWidth
-	}
-
 	override fun onDraw(canvas: Canvas?) {
-		if (strokeWidth > 0) {
-			isDrawing = true
-			val initialColor = textColors
-
-			paint.style = Paint.Style.STROKE
-			paint.strokeWidth = strokeWidth
-			setTextColor(ContextCompat.getColor(context, R.color.black))
-			super.onDraw(canvas)
-
-			paint.style = Paint.Style.FILL
-			setTextColor(initialColor)
-			super.onDraw(canvas)
-			isDrawing = false
-		} else {
-			super.onDraw(canvas)
+		if (strokeWidth <= 0) {
+			return super.onDraw(canvas)
 		}
+		isDrawing = true
+		val initialColor = textColors
+
+		paint.style = Paint.Style.STROKE
+		paint.strokeWidth = strokeWidth
+		setTextColor(ContextCompat.getColor(context, R.color.black))
+		super.onDraw(canvas)
+
+		paint.style = Paint.Style.FILL
+		setTextColor(initialColor)
+		super.onDraw(canvas)
+		isDrawing = false
 	}
 }
