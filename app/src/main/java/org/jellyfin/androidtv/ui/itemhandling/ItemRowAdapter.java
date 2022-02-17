9863412d03bd23b8601b470ab38e26536f637e09
Niels van Velzen <git@ndat.nl>
2022-02-17 19:35:52 +0100

    Remove some dead code in error handler from ItemRowAdapter

--- app/src/main/java/org/jellyfin/androidtv/ui/itemhandling/ItemRowAdapter.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/itemhandling/ItemRowAdapter.java
@@ -2,8 +2,6 @@ package org.jellyfin.androidtv.ui.itemhandling;
 
 import static org.koin.java.KoinJavaComponent.inject;
 
-import android.os.Handler;
-
 import androidx.annotation.Nullable;
 import androidx.leanback.widget.ArrayObjectAdapter;
 import androidx.leanback.widget.HeaderItem;
@@ -45,7 +43,6 @@ import org.jellyfin.apiclient.model.livetv.RecordingGroupQuery;
 import org.jellyfin.apiclient.model.livetv.RecordingQuery;
 import org.jellyfin.apiclient.model.livetv.SeriesTimerInfoDto;
 import org.jellyfin.apiclient.model.livetv.SeriesTimerQuery;
-import org.jellyfin.apiclient.model.net.HttpException;
 import org.jellyfin.apiclient.model.querying.ArtistsQuery;
 import org.jellyfin.apiclient.model.querying.ItemFields;
 import org.jellyfin.apiclient.model.querying.ItemQuery;
@@ -897,28 +894,10 @@ public class ItemRowAdapter extends ArrayObjectAdapter {
             @Override
             public void onError(Exception exception) {
                 Timber.e(exception, "Error retrieving items");
-                if (exception instanceof HttpException) {
-                    HttpException httpException = (HttpException) exception;
-                    if (httpException.getStatusCode() != null && httpException.getStatusCode() == 401 && "ParentalControl".equals(httpException.getHeaders().get("X-Application-Error-Code"))) {
-                        Utils.showToast(TvApp.getApplication(), TvApp.getApplication().getString(R.string.msg_access_restricted));
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                System.exit(1);
-                            }
-                        }, 3000);
-                    } else {
-                        removeRow();
-                        Utils.showToast(TvApp.getApplication(), exception.getLocalizedMessage());
-                    }
-                } else {
-                    removeRow();
-                    Utils.showToast(TvApp.getApplication(), exception.getLocalizedMessage());
-
-                }
+                removeRow();
+                Utils.showToast(TvApp.getApplication(), exception.getLocalizedMessage());
                 notifyRetrieveFinished();
             }
-
         });
     }
 
