546baa4304046c2de3527a0d56eb8a3834e8d20c
Niels van Velzen <git@ndat.nl>
2022-02-19 11:22:00 +0100

    Remove currentActivity from TvApp

--- app/src/main/java/org/jellyfin/androidtv/di/ActivityLifecycleCallbacksModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/di/ActivityLifecycleCallbacksModule.kt
@@ -3,12 +3,10 @@ package org.jellyfin.androidtv.di
 import android.app.Application
 import org.jellyfin.androidtv.ui.shared.AppThemeCallbacks
 import org.jellyfin.androidtv.ui.shared.AuthenticatedUserCallbacks
-import org.jellyfin.androidtv.ui.shared.CurrentActivityCallbacks
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val activityLifecycleCallbacksModule = module {
 	single { AuthenticatedUserCallbacks(get()) } bind Application.ActivityLifecycleCallbacks::class
 	single { AppThemeCallbacks(get()) } bind Application.ActivityLifecycleCallbacks::class
-	single { CurrentActivityCallbacks() } bind Application.ActivityLifecycleCallbacks::class
 }
