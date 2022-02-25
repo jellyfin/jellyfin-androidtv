dc5966da1ab11d521228620def9c78c0c2556f06
Niels van Velzen <git@ndat.nl>
2022-02-25 21:41:07 +0100

    Migrate TextUnderButton to view binding
    Also removed a lot of unused stuff from it

--- app/src/main/java/org/jellyfin/androidtv/ui/TextUnderButton.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/TextUnderButton.java
@@ -3,29 +3,19 @@ package org.jellyfin.androidtv.ui;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import org.jellyfin.androidtv.R;
 import org.jellyfin.androidtv.data.model.GotFocusEvent;
+import org.jellyfin.androidtv.databinding.TextUnderButtonBinding;
 import org.jellyfin.androidtv.util.Utils;
 
 public class TextUnderButton extends RelativeLayout {
-
-    public static int STATE_PRIMARY = 0;
-    public static int STATE_SECONDARY = 1;
-
     private TextView mLabel;
-    private android.widget.ImageButton mButton;
-    private int mPrimaryImage;
-    private int mSecondaryImage;
-    private int mState;
+    private ImageButton mButton;
 
     private GotFocusEvent mGotFocusListener;
-
-    public TextUnderButton(Context context, int imageResource, int size, final OnClickListener clicked) {
-        this(context, imageResource, size, null, null, clicked);
-    }
 
     public TextUnderButton(Context context, int imageResource, int size, String label, final OnClickListener clicked) {
         this(context, imageResource, size, null, label, clicked);
@@ -35,17 +25,16 @@ public class TextUnderButton extends RelativeLayout {
         super(context);
 
         LayoutInflater inflater = LayoutInflater.from(context);
-        View v = inflater.inflate(R.layout.text_under_button, this, false);
-        this.addView(v);
+        TextUnderButtonBinding binding = TextUnderButtonBinding.inflate(inflater, this, true);
 
-        mLabel = (TextView) v.findViewById(R.id.label);
+        mLabel = binding.label;
         if (label == null) {
             mLabel.setVisibility(GONE);
         } else {
             mLabel.setText(label);
         }
 
-        mButton = (android.widget.ImageButton) v.findViewById(R.id.imageButton);
+        mButton = binding.imageButton;
 
         if (label != null) mButton.setContentDescription(label);
 
@@ -62,15 +51,6 @@ public class TextUnderButton extends RelativeLayout {
 
     public void setText(String text) { mLabel.setText(text); }
 
-    public void setState(int state) {
-        mState = state;
-        if (mSecondaryImage > 0) mButton.setImageResource(mState == STATE_SECONDARY ? mSecondaryImage : mPrimaryImage);
-    }
-
-    public void toggleState() {
-        setState(mState == STATE_PRIMARY ? STATE_SECONDARY : STATE_PRIMARY);
-    }
-
     protected OnFocusChangeListener focusChange = new OnFocusChangeListener() {
         @Override
         public void onFocusChange(View v, boolean hasFocus) {
@@ -83,14 +63,6 @@ public class TextUnderButton extends RelativeLayout {
 
     public void setImageResource(int resource) {
         mButton.setImageResource(resource);
-    }
-
-    public void setPrimaryImage(int mPrimaryImage) {
-        this.mPrimaryImage = mPrimaryImage;
-    }
-
-    public void setSecondaryImage(int mSecondaryImage) {
-        this.mSecondaryImage = mSecondaryImage;
     }
 
     @Override
