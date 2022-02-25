1360632e3515b359e2cc42b1c9170a6939432c80
Niels van Velzen <git@ndat.nl>
2022-02-25 21:37:36 +0100

    Migrate NumberSpinner to view binding

--- app/src/main/java/org/jellyfin/androidtv/ui/NumberSpinner.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/NumberSpinner.java
@@ -7,20 +7,13 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import org.jellyfin.androidtv.R;
+import org.jellyfin.androidtv.databinding.NumberSpinnerBinding;
 
 public class NumberSpinner extends FrameLayout {
     long mValue = 0;
     long mIncrement = 10;
     TextView mTextValue;
     ValueChangedListener<Long> mValueChangedListener;
-
-
-    public NumberSpinner(Context context, ValueChangedListener<Long> listener) {
-        super(context);
-        mValueChangedListener = listener;
-        init(context);
-    }
 
     public NumberSpinner(Context context) {
         super(context);
@@ -34,16 +27,16 @@ public class NumberSpinner extends FrameLayout {
 
     private void init(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View v = inflater.inflate(R.layout.number_spinner, this, true);
+        NumberSpinnerBinding binding = NumberSpinnerBinding.inflate(inflater, this, true);
         if (!isInEditMode()) {
-            mTextValue = (TextView) v.findViewById(R.id.txtValue);
-            (v.findViewById(R.id.btnIncrease)).setOnClickListener(new OnClickListener() {
+            mTextValue = binding.txtValue;
+            binding.btnIncrease.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     setValue(mValue + mIncrement);
                 }
             });
-            (v.findViewById(R.id.btnDecrease)).setOnClickListener(new OnClickListener() {
+            binding.btnDecrease.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     setValue(mValue-mIncrement);
@@ -66,9 +59,5 @@ public class NumberSpinner extends FrameLayout {
 
     public long getValue() {
         return mValue;
-    }
-
-    public void setIncrement(long value) {
-        mIncrement = value;
     }
 }
