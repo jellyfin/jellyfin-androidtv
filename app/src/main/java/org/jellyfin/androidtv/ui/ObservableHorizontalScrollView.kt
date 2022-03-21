3c97879b0cf858ac7d47b90aa48f6b166376fdb4
Niels van Velzen <git@ndat.nl>
2022-03-21 21:21:50 +0100

    Migrate ObservableHorizontalScrollView to Kotlin

--- app/src/main/java/org/jellyfin/androidtv/ui/ObservableHorizontalScrollView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/ObservableHorizontalScrollView.kt
@@ -1,33 +1,19 @@
-package org.jellyfin.androidtv.ui;
+package org.jellyfin.androidtv.ui
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.HorizontalScrollView;
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.HorizontalScrollView
 
-public class ObservableHorizontalScrollView extends HorizontalScrollView {
-    private HorizontalScrollViewListener scrollViewListener = null;
+class ObservableHorizontalScrollView @JvmOverloads constructor(
+	context: Context,
+	attrs: AttributeSet? = null,
+	defStyleAttr: Int = android.R.attr.horizontalScrollViewStyle,
+	defStyleRes: Int = 0,
+) : HorizontalScrollView(context, attrs, defStyleAttr, defStyleRes) {
+	var scrollViewListener: HorizontalScrollViewListener? = null
 
-    public ObservableHorizontalScrollView(Context context) {
-        super(context);
-    }
-
-    public ObservableHorizontalScrollView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    public ObservableHorizontalScrollView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public void setScrollViewListener(HorizontalScrollViewListener scrollViewListener) {
-        this.scrollViewListener = scrollViewListener;
-    }
-
-    @Override
-    protected void onScrollChanged(int x, int y, int oldx, int oldy) {
-        super.onScrollChanged(x, y, oldx, oldy);
-        if(scrollViewListener != null) {
-            scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
-        }
-    }
+	override fun onScrollChanged(x: Int, y: Int, oldx: Int, oldy: Int) {
+		super.onScrollChanged(x, y, oldx, oldy)
+		scrollViewListener?.onScrollChanged(this, x, y, oldx, oldy)
+	}
 }
