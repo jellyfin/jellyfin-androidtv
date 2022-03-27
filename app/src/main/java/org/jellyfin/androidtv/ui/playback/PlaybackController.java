431265c81e0940ad0ee2e623960e6eca1f54c542
David Fairbrother <DavidFair@users.noreply.github.com>
2022-03-27 15:33:01 +0100

    Extract methods to populate video options for VLC and ExoPlayer
    Since we will be touching this code in a later commit it makes sense to
    extract these into private methods as-is.
    This commit only extracts the options and does not perform any
    additional refactoring steps

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
@@ -507,43 +507,8 @@ public class PlaybackController {
                 if (isLiveTv) mSeekPosition = -1;
 
                 //Build options for each player
-                VideoOptions vlcOptions = new VideoOptions();
-                vlcOptions.setItemId(item.getId());
-                vlcOptions.setMediaSources(item.getMediaSources());
-                vlcOptions.setMaxBitrate(Utils.getMaxBitrate());
-                if (vlcErrorEncountered) {
-                    Timber.i("*** Disabling direct play/stream due to previous error");
-                    vlcOptions.setEnableDirectStream(false);
-                    vlcOptions.setEnableDirectPlay(false);
-                }
-                vlcOptions.setSubtitleStreamIndex(forcedSubtitleIndex);
-                vlcOptions.setMediaSourceId(forcedSubtitleIndex != null ? getCurrentMediaSource().getId() : null);
-                DeviceProfile vlcProfile = new LibVlcProfile(mFragment.getContext(), isLiveTv);
-                vlcOptions.setProfile(vlcProfile);
-
-                VideoOptions internalOptions = new VideoOptions();
-                internalOptions.setItemId(item.getId());
-                internalOptions.setMediaSources(item.getMediaSources());
-                internalOptions.setMaxBitrate(Utils.getMaxBitrate());
-                if (exoErrorEncountered || (isLiveTv && !directStreamLiveTv))
-                    internalOptions.setEnableDirectStream(false);
-                internalOptions.setMaxAudioChannels(Utils.downMixAudio(mFragment.getContext()) ? 2 : null); //have to downmix at server
-                internalOptions.setSubtitleStreamIndex(forcedSubtitleIndex);
-                internalOptions.setMediaSourceId(forcedSubtitleIndex != null ? getCurrentMediaSource().getId() : null);
-                DeviceProfile internalProfile = new BaseProfile();
-                if (DeviceUtils.is60() || userPreferences.getValue().get(UserPreferences.Companion.getAc3Enabled())) {
-                    internalProfile = new ExoPlayerProfile(
-                            mFragment.getContext(),
-                            isLiveTv,
-                            userPreferences.getValue().get(UserPreferences.Companion.getLiveTvDirectPlayEnabled()),
-                            userPreferences.getValue().get(UserPreferences.Companion.getAc3Enabled())
-                    );
-                    Timber.i("*** Using extended Exoplayer profile options");
-                } else {
-                    Timber.i("*** Using default android profile");
-                }
-                internalOptions.setProfile(internalProfile);
-
+                VideoOptions vlcOptions = buildVLCOptions(forcedSubtitleIndex, item);
+                VideoOptions internalOptions = buildExoPlayerOptions(forcedSubtitleIndex, item);
                 Timber.d("Max bitrate is: %d", Utils.getMaxBitrate());
 
                 playInternal(getCurrentlyPlayingItem(), position, vlcOptions, internalOptions);
@@ -559,6 +524,51 @@ public class PlaybackController {
 
                 break;
         }
+    }
+
+    @NonNull
+    private VideoOptions buildExoPlayerOptions(@Nullable Integer forcedSubtitleIndex, BaseItemDto item) {
+        VideoOptions internalOptions = new VideoOptions();
+        internalOptions.setItemId(item.getId());
+        internalOptions.setMediaSources(item.getMediaSources());
+        internalOptions.setMaxBitrate(Utils.getMaxBitrate());
+        if (exoErrorEncountered || (isLiveTv && !directStreamLiveTv))
+            internalOptions.setEnableDirectStream(false);
+        internalOptions.setMaxAudioChannels(Utils.downMixAudio(mFragment.getContext()) ? 2 : null); //have to downmix at server
+        internalOptions.setSubtitleStreamIndex(forcedSubtitleIndex);
+        internalOptions.setMediaSourceId(forcedSubtitleIndex != null ? getCurrentMediaSource().getId() : null);
+        DeviceProfile internalProfile = new BaseProfile();
+        if (DeviceUtils.is60() || userPreferences.getValue().get(UserPreferences.Companion.getAc3Enabled())) {
+            internalProfile = new ExoPlayerProfile(
+                    mFragment.getContext(),
+                    isLiveTv,
+                    userPreferences.getValue().get(UserPreferences.Companion.getLiveTvDirectPlayEnabled()),
+                    userPreferences.getValue().get(UserPreferences.Companion.getAc3Enabled())
+            );
+            Timber.i("*** Using extended Exoplayer profile options");
+        } else {
+            Timber.i("*** Using default android profile");
+        }
+        internalOptions.setProfile(internalProfile);
+        return internalOptions;
+    }
+
+    @NonNull
+    private VideoOptions buildVLCOptions(@Nullable Integer forcedSubtitleIndex, BaseItemDto item) {
+        VideoOptions vlcOptions = new VideoOptions();
+        vlcOptions.setItemId(item.getId());
+        vlcOptions.setMediaSources(item.getMediaSources());
+        vlcOptions.setMaxBitrate(Utils.getMaxBitrate());
+        if (vlcErrorEncountered) {
+            Timber.i("*** Disabling direct play/stream due to previous error");
+            vlcOptions.setEnableDirectStream(false);
+            vlcOptions.setEnableDirectPlay(false);
+        }
+        vlcOptions.setSubtitleStreamIndex(forcedSubtitleIndex);
+        vlcOptions.setMediaSourceId(forcedSubtitleIndex != null ? getCurrentMediaSource().getId() : null);
+        DeviceProfile vlcProfile = new LibVlcProfile(mFragment.getContext(), isLiveTv);
+        vlcOptions.setProfile(vlcProfile);
+        return vlcOptions;
     }
 
     public int getBufferAmount() {
