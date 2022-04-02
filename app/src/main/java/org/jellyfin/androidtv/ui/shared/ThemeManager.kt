d1dde2c0bc6f65a9b6e6823ad5e803aaa17e9bd8
Niels van Velzen <git@ndat.nl>
2022-04-02 13:14:54 +0200

    Remove hotdog theme

--- app/src/main/java/org/jellyfin/androidtv/ui/shared/ThemeManager.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/shared/ThemeManager.kt
@@ -2,37 +2,20 @@ package org.jellyfin.androidtv.ui.shared
 
 import android.app.Activity
 import org.jellyfin.androidtv.R
-import org.jellyfin.androidtv.preference.UserPreferences
 import org.jellyfin.androidtv.preference.constant.AppTheme
 import org.jellyfin.androidtv.ui.preference.PreferencesActivity
-import org.koin.android.ext.android.get
-import java.util.Calendar
-import java.util.GregorianCalendar
 
 object ThemeManager {
-	private fun showAprilFools(userPreferences: UserPreferences): Boolean {
-		val enableGreetings = userPreferences[UserPreferences.seasonalGreetingsEnabled]
-		if (!enableGreetings) return false
-
-		val today = GregorianCalendar()
-		return today[Calendar.MONTH] == Calendar.APRIL && today[Calendar.DAY_OF_MONTH] == 1
-	}
-
 	fun getTheme(activity: Activity, appTheme: AppTheme): Int {
 		// Settings fragment use a special theme with a transparent background
 		if (activity is PreferencesActivity) {
 			return R.style.Theme_Jellyfin_Preferences
 		}
 
-		if (showAprilFools(activity.get())) {
-			return R.style.Theme_Jellyfin_HotDogStand
-		}
-
 		return when (appTheme) {
 			AppTheme.DARK -> R.style.Theme_Jellyfin
 			AppTheme.EMERALD -> R.style.Theme_Jellyfin_Emerald
 			AppTheme.MUTED_PURPLE -> R.style.Theme_Jellyfin_MutedPurple
-			AppTheme.HOT_DOG_STAND -> R.style.Theme_Jellyfin_HotDogStand
 		}
 	}
 }
