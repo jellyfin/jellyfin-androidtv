748dd56decf774c4ffb1d3c11ea3cd1f364630b5
Niels van Velzen <git@ndat.nl>
2022-04-16 12:26:27 +0200

    Update playback rewrite build files

--- playback/build.gradle.kts
+++ playback/build.gradle.kts
@@ -22,8 +22,39 @@ android {
 		lintConfig = file("$rootDir/android-lint.xml")
 		abortOnError = false
 	}
+
+	testOptions.unitTests.all {
+		it.useJUnitPlatform()
+	}
 }
 
 dependencies {
+	// Jellyfin
+	compileOnly(libs.jellyfin.sdk)
 
+	// Kotlin
+	implementation(libs.kotlinx.coroutines)
+	implementation(libs.kotlinx.coroutines.guava)
+	implementation(libs.kotlinx.serialization.json)
+
+	// Android(x)
+	implementation(libs.androidx.core)
+	implementation(libs.androidx.appcompat)
+	implementation(libs.androidx.constraintlayout)
+	implementation(libs.bundles.androidx.lifecycle)
+
+	// Dependency Injection
+	implementation(libs.bundles.koin)
+
+	// Media
+	implementation(libs.exoplayer)
+	implementation(libs.androidx.media2.session)
+
+	// Logging
+	implementation(libs.timber)
+
+	// Testing
+	testImplementation(libs.kotest.runner.junit5)
+	testImplementation(libs.kotest.assertions)
+	testImplementation(libs.mockk)
 }
