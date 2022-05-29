c64d43090cf33e9404b92fd877ee22b8e629794b
Niels van Velzen <git@ndat.nl>
2022-05-29 22:07:58 +0200

    Check for RECORD_AUDIO permission in SearchActivity

--- app/src/main/java/org/jellyfin/androidtv/ui/search/SearchActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/search/SearchActivity.kt
@@ -1,12 +1,16 @@
 package org.jellyfin.androidtv.ui.search
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.speech.SpeechRecognizer
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 
 class SearchActivity : FragmentActivity() {
 	private val isSpeechEnabled by lazy {
 		SpeechRecognizer.isRecognitionAvailable(this)
+			&& ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_DENIED
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
