0ede654513c89317f8df7e112e3830afc53b829b
Niels van Velzen <git@ndat.nl>
2022-06-07 13:23:53 +0200

    Merge ConnectHelpAlertFragment and AlertFragment

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
@@ -4,22 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.jellyfin.androidtv.R
+import androidx.fragment.app.Fragment
 import org.jellyfin.androidtv.databinding.FragmentAlertConnectHelpBinding
-import org.jellyfin.androidtv.ui.shared.AlertFragment
 
-class ConnectHelpAlertFragment : AlertFragment() {
+class ConnectHelpAlertFragment : Fragment() {
 	private lateinit var binding: FragmentAlertConnectHelpBinding
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-
-		title = R.string.login_help_title
-		cancelable = false
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+		binding = FragmentAlertConnectHelpBinding.inflate(inflater, container, false)
+		return binding.root
 	}
 
-	override fun onCreateChildView(inflater: LayoutInflater, contentContainer: ViewGroup): View? {
-		binding = FragmentAlertConnectHelpBinding.inflate(inflater, contentContainer, false)
-		return binding.root
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		with(binding.confirm) {
+			requestFocus()
+			setOnClickListener { parentFragmentManager.popBackStack() }
+		}
 	}
 }
