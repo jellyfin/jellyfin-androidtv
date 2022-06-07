d852365092a50067006ff96640b5738740569621
Niels van Velzen <git@ndat.nl>
2022-06-07 15:33:13 +0200

    Add UI mode notification

--- app/src/main/java/org/jellyfin/androidtv/data/repository/NotificationsRepository.kt
+++ app/src/main/java/org/jellyfin/androidtv/data/repository/NotificationsRepository.kt
@@ -1,19 +1,42 @@
 package org.jellyfin.androidtv.data.repository
 
+import android.app.UiModeManager
+import android.content.Context
+import android.content.res.Configuration
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.data.model.AppNotification
 
 interface NotificationsRepository {
 	val notifications: StateFlow<List<AppNotification>>
 
 	fun dismissNotification(item: AppNotification)
+	fun addDefaultNotifications()
 }
 
-class NotificationsRepositoryImpl : NotificationsRepository {
+class NotificationsRepositoryImpl(
+	private val context: Context,
+	private val uiModeManager: UiModeManager,
+) : NotificationsRepository {
 	override val notifications = MutableStateFlow(emptyList<AppNotification>())
 
 	override fun dismissNotification(item: AppNotification) {
 		notifications.value = notifications.value.filter { it != item }
+	}
+
+	override fun addDefaultNotifications() {
+		addUiModeNotification()
+	}
+
+	private fun addNotification(message: String) {
+		notifications.value = notifications.value + AppNotification(message)
+	}
+
+	private fun addUiModeNotification() {
+		val supportedUiModes = setOf(Configuration.UI_MODE_TYPE_TELEVISION, Configuration.UI_MODE_TYPE_UNDEFINED)
+		val invalidUiMode = !supportedUiModes.contains(uiModeManager.currentModeType)
+
+		if (invalidUiMode) addNotification(context.getString(R.string.app_notification_uimode_invalid))
 	}
 }
--- app/src/main/java/org/jellyfin/androidtv/di/AndroidModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/di/AndroidModule.kt
@@ -1,6 +1,7 @@
 package org.jellyfin.androidtv.di
 
 import android.accounts.AccountManager
+import android.app.UiModeManager
 import androidx.core.content.getSystemService
 import androidx.work.WorkManager
 import org.koin.android.ext.koin.androidApplication
@@ -11,5 +12,6 @@ import org.koin.dsl.module
  */
 val androidModule = module {
 	factory { androidApplication().getSystemService<AccountManager>()!! }
+	factory { androidApplication().getSystemService<UiModeManager>()!! }
 	factory { WorkManager.getInstance(get()) }
 }
