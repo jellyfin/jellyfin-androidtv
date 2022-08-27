8da831679208966c697113fcd63a74fba0ada35b
Niels van Velzen <git@ndat.nl>
2022-08-27 22:15:37 +0200

    Migrate favorite and played toggles in KeyProcessor to SDK

---
+++ app/src/main/java/org/jellyfin/androidtv/data/repository/ItemMutationRepository.kt
@@ -0,0 +1,37 @@
+package org.jellyfin.androidtv.data.repository
+
+import org.jellyfin.androidtv.data.model.DataRefreshService
+import org.jellyfin.sdk.api.client.ApiClient
+import org.jellyfin.sdk.api.client.extensions.playStateApi
+import org.jellyfin.sdk.api.client.extensions.userLibraryApi
+import org.jellyfin.sdk.model.UUID
+import org.jellyfin.sdk.model.api.UserItemDataDto
+
+interface ItemMutationRepository {
+	suspend fun setFavorite(item: UUID, favorite: Boolean): UserItemDataDto
+	suspend fun setPlayed(item: UUID, played: Boolean): UserItemDataDto
+}
+
+class ItemMutationRepositoryImpl(
+	private val api: ApiClient,
+	private val dataRefreshService: DataRefreshService,
+) : ItemMutationRepository {
+	override suspend fun setFavorite(item: UUID, favorite: Boolean): UserItemDataDto {
+		val response by when {
+			favorite -> api.userLibraryApi.markFavoriteItem(itemId = item)
+			else -> api.userLibraryApi.unmarkFavoriteItem(itemId = item)
+		}
+
+		dataRefreshService.lastFavoriteUpdate = System.currentTimeMillis()
+		return response
+	}
+
+	override suspend fun setPlayed(item: UUID, played: Boolean): UserItemDataDto {
+		val response by when {
+			played -> api.playStateApi.markPlayedItem(itemId = item)
+			else -> api.playStateApi.markUnplayedItem(itemId = item)
+		}
+
+		return response
+	}
+}
