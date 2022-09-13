4a77646141c4cf0457d8cad7c666271ea2d87f9f
Niels van Velzen <git@ndat.nl>
2022-09-13 13:03:54 +0200

    Rewrite DeviceUtils in Kotlin

--- app/src/test/kotlin/util/DeviceUtilsTests.kt
+++ app/src/test/kotlin/util/DeviceUtilsTests.kt
@@ -3,8 +3,8 @@ package org.jellyfin.androidtv.util
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
 
 class DeviceUtilsTests : FunSpec({
 	test("DeviceUtils.getBuildModel() is unknown") {
@@ -12,56 +12,56 @@ class DeviceUtilsTests : FunSpec({
 	}
 
 	test("DeviceUtils methods support unknown as model") {
-		DeviceUtils.isChromecastWithGoogleTV() shouldBe false
-		DeviceUtils.isFireTv() shouldBe false
-		DeviceUtils.isFireTvStickGen1() shouldBe false
-		DeviceUtils.isFireTvStick4k() shouldBe false
-		DeviceUtils.isShieldTv() shouldBe false
+		DeviceUtils.isChromecastWithGoogleTV shouldBe false
+		DeviceUtils.isFireTv shouldBe false
+		DeviceUtils.isFireTvStickGen1 shouldBe false
+		DeviceUtils.isFireTvStick4k shouldBe false
+		DeviceUtils.isShieldTv shouldBe false
 		DeviceUtils.has4kVideoSupport() shouldBe false
 	}
 
 	fun withBuildModel(buildModel: String, block: () -> Unit) {
-		mockkStatic(DeviceUtils::class)
+		mockkObject(DeviceUtils)
 		every { DeviceUtils.getBuildModel() } returns buildModel
 		block()
-		unmockkStatic(DeviceUtils::class)
+		unmockkObject(DeviceUtils)
 	}
 
 	test("DeviceUtils.isChromecastWithGoogleTV() works correctly") {
 		withBuildModel("Chromecast") {
-			DeviceUtils.isChromecastWithGoogleTV() shouldBe true
+			DeviceUtils.isChromecastWithGoogleTV shouldBe true
 		}
 	}
 
 	test("DeviceUtils.isFireTv() works correctly") {
 		arrayOf("AFT", "AFT_foo", "AFT ", "AFT2").forEach { input ->
 			withBuildModel(input) {
-				DeviceUtils.isFireTv() shouldBe true
+				DeviceUtils.isFireTv shouldBe true
 			}
 		}
 	}
 
 	test("DeviceUtils.isFireTvStickGen1() works correctly") {
 		withBuildModel("AFTM") {
-			DeviceUtils.isFireTv() shouldBe true
-			DeviceUtils.isFireTvStickGen1() shouldBe true
-			DeviceUtils.isFireTvStick4k() shouldBe false
+			DeviceUtils.isFireTv shouldBe true
+			DeviceUtils.isFireTvStickGen1 shouldBe true
+			DeviceUtils.isFireTvStick4k shouldBe false
 		}
 	}
 
 	test("DeviceUtils.isFireTvStick4k() works correctly") {
 		arrayOf("AFTMM", "AFTKA").forEach { input ->
 			withBuildModel(input) {
-				DeviceUtils.isFireTv() shouldBe true
-				DeviceUtils.isFireTvStick4k() shouldBe true
-				DeviceUtils.isFireTvStickGen1() shouldBe false
+				DeviceUtils.isFireTv shouldBe true
+				DeviceUtils.isFireTvStick4k shouldBe true
+				DeviceUtils.isFireTvStickGen1 shouldBe false
 			}
 		}
 	}
 
 	test("DeviceUtils.isShieldTv() works correctly") {
 		withBuildModel("SHIELD Android TV") {
-			DeviceUtils.isShieldTv() shouldBe true
+			DeviceUtils.isShieldTv shouldBe true
 		}
 	}
 
