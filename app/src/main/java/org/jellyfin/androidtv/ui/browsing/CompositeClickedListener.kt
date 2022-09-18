8c85101020149dd4c18eed21b95467c9816a44c3
Niels van Velzen <git@ndat.nl>
2022-09-18 21:37:43 +0200

    Rewrite CompositeListeners in Kotlin

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/CompositeClickedListener.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/CompositeClickedListener.kt
@@ -1,29 +1,23 @@
-package org.jellyfin.androidtv.ui.browsing;
+package org.jellyfin.androidtv.ui.browsing
 
-import androidx.leanback.widget.OnItemViewClickedListener;
-import androidx.leanback.widget.Presenter;
-import androidx.leanback.widget.Row;
-import androidx.leanback.widget.RowPresenter;
+import androidx.leanback.widget.OnItemViewClickedListener
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.Row
+import androidx.leanback.widget.RowPresenter
 
-import java.util.ArrayList;
-import java.util.List;
+class CompositeClickedListener : OnItemViewClickedListener {
+	private val listeners = mutableListOf<OnItemViewClickedListener>()
 
-public class CompositeClickedListener implements OnItemViewClickedListener {
-    private List<OnItemViewClickedListener> registeredListeners = new ArrayList<>();
+	fun registerListener(listener: OnItemViewClickedListener) = listeners.add(listener)
 
-    public void registerListener (OnItemViewClickedListener listener) {
-        registeredListeners.add(listener);
-    }
-
-    public void unRegisterListener (OnItemViewClickedListener listener) {
-        registeredListeners.remove(listener);
-    }
-
-    @Override
-    public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item, RowPresenter.ViewHolder rowViewHolder, Row row) {
-        for (OnItemViewClickedListener listener : registeredListeners) {
-            listener.onItemClicked(itemViewHolder, item, rowViewHolder, row);
-        }
-    }
+	override fun onItemClicked(
+		itemViewHolder: Presenter.ViewHolder?,
+		item: Any?,
+		rowViewHolder: RowPresenter.ViewHolder,
+		row: Row,
+	) {
+		for (listener in listeners) {
+			listener.onItemClicked(itemViewHolder, item, rowViewHolder, row)
+		}
+	}
 }
-
