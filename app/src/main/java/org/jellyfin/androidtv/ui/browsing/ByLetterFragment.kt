4225317172fbc1408655777fb1ed5a9454e6fd49
Niels van Velzen <git@ndat.nl>
2022-09-27 13:19:56 +0200

    Merge StdBrowseFragment and BrowseFolderFragment and rewrite in Kotlin

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/ByLetterFragment.kt
@@ -0,0 +1,39 @@
+package org.jellyfin.androidtv.ui.browsing
+
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.data.querying.StdItemQuery
+import org.jellyfin.sdk.model.constant.ItemSortBy
+
+class ByLetterFragment : BrowseFolderFragment() {
+	override suspend fun setupQueries(rowLoader: RowLoader) {
+		val childCount = folder?.childCount ?: 0
+		if (childCount <= 0) return
+
+		val letters = getString(R.string.byletter_letters)
+
+		// Add a '#' item
+		val numbersQuery = StdItemQuery().apply {
+			parentId = folder?.id?.toString()
+			sortBy = arrayOf(ItemSortBy.SortName)
+			nameLessThan = letters.substring(0, 1)
+			recursive = true
+		}
+
+		rows.add(BrowseRowDef("#", numbersQuery, 40))
+
+		// Add all the defined letters
+		for (letter in letters.toCharArray()) {
+			val letterQuery = StdItemQuery().apply {
+				parentId = folder?.id?.toString()
+				sortBy = arrayOf(ItemSortBy.SortName)
+				includeType?.let { includeItemTypes = arrayOf(it) }
+				nameStartsWith = letter.toString()
+				recursive = true
+			}
+
+			rows.add(BrowseRowDef(letter.toString(), letterQuery, 40))
+		}
+
+		rowLoader.loadRows(rows)
+	}
+}
