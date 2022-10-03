123d2850ce7f80713cdb51469e3b565d73ecc43f
Niels van Velzen <git@ndat.nl>
2022-10-03 21:37:25 +0200

    Add NavigationAction to control fragment manager

--- app/src/main/java/org/jellyfin/androidtv/ui/navigation/NavigationOptions.kt
+++
@@ -1,6 +0,0 @@
-package org.jellyfin.androidtv.ui.navigation
-
-data class NavigationOptions(
-	val clearHistory: Boolean = false,
-	val replaceCurrentDestination: Boolean = false,
-)
--- app/src/main/java/org/jellyfin/androidtv/ui/navigation/NavigationRepository.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/navigation/NavigationRepository.kt
@@ -1,42 +1,93 @@
 package org.jellyfin.androidtv.ui.navigation
 
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import timber.log.Timber
 import java.util.Stack
 
+/**
+ * Repository for app navigation. This manages the screens/pages for the app.
+ */
 interface NavigationRepository {
-	val currentDestination: StateFlow<Destination>
+	/**
+	 * The current action to act on.
+	 *
+	 * @see NavigationAction
+	 */
+	val currentAction: SharedFlow<NavigationAction>
 
-	fun navigate(destination: Destination) = navigate(destination, NavigationOptions())
-	fun navigate(destination: Destination, options: NavigationOptions)
+	/**
+	 * Navigate to [destination].
+	 *
+	 * @see Destinations
+	 */
+	fun navigate(destination: Destination)
 
+	/**
+	 * Whether the [goBack] function will succeed or not.
+	 *
+	 * @see [goBack]
+	 */
 	val canGoBack: Boolean
+
+	/**
+	 * Go back to the previous fragment. The back stack does not consider other destination types.
+	 *
+	 * @see [canGoBack]
+	 */
 	fun goBack(): Boolean
+
+	/**
+	 * Reset navigation to it's initial state and remvoe all history.
+	 */
+	fun reset()
 }
 
 class NavigationRepositoryImpl(
-	initialDestination: Destination,
+	private val initialDestination: Destination.Fragment,
 ) : NavigationRepository {
-	private val history = Stack<Destination>()
+	private val fragmentHistory = Stack<Destination.Fragment>()
 
-	private val _currentDestination = MutableStateFlow(initialDestination)
-	override val currentDestination = _currentDestination.asStateFlow()
+	private val _currentAction = MutableSharedFlow<NavigationAction>(1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+	override val currentAction = _currentAction.asSharedFlow()
 
-	override fun navigate(destination: Destination, options: NavigationOptions) {
-		if (options.clearHistory) history.clear()
-
-		if (!options.replaceCurrentDestination) history.push(_currentDestination.value)
-		_currentDestination.value = destination
+	init {
+		// Never add the initial destination to the history to prevent an empty screen when the user
+		// uses the "back" button to close the app
+		_currentAction.tryEmit(NavigationAction.NavigateFragment(initialDestination, false))
+		Timber.d("Navigating to $initialDestination")
 	}
 
-	override val canGoBack: Boolean get() = history.isNotEmpty()
+	override fun navigate(destination: Destination) {
+		Timber.d("Navigating to $destination")
+		val action = when (destination) {
+			is Destination.Fragment -> NavigationAction.NavigateFragment(destination, true)
+			is Destination.Activity -> NavigationAction.NavigateActivity(destination) {
+				Timber.d("Navigating to nothing")
+				_currentAction.tryEmit(NavigationAction.Nothing)
+			}
+		}
+		_currentAction.tryEmit(action)
+		if (destination is Destination.Fragment) fragmentHistory.push(destination)
+	}
+
+	override val canGoBack: Boolean get() = fragmentHistory.isNotEmpty()
 
 	override fun goBack(): Boolean {
-		if (history.empty()) return false
+		if (fragmentHistory.empty()) return false
 
-		_currentDestination.value = history.pop()
+		Timber.d("Navigating back")
+		fragmentHistory.pop()
+		_currentAction.tryEmit(NavigationAction.GoBack)
 		return true
+	}
+
+	override fun reset() {
+		fragmentHistory.clear()
+		_currentAction.tryEmit(NavigationAction.NavigateFragment(initialDestination, false))
+		Timber.d("Navigating to $initialDestination")
 	}
 }
 
