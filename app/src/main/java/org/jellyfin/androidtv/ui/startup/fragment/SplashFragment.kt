5d994132269efa6e81dce658d5eae5ca37c53eae
Niels van Velzen <git@ndat.nl>
2022-10-29 13:00:26 +0200

    Rewrite SplashFragment in Compose

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/SplashFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/SplashFragment.kt
@@ -3,10 +3,50 @@ package org.jellyfin.androidtv.ui.startup.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import org.jellyfin.androidtv.databinding.FragmentSplashBinding
+import org.jellyfin.androidtv.R
+
+@Composable
+fun SplashScreen() {
+	Surface(
+		color = colorResource(id = R.color.not_quite_black),
+	) {
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.Center,
+			modifier = Modifier.fillMaxSize(),
+		) {
+			Image(
+				painter = painterResource(R.drawable.app_logo),
+				contentDescription = stringResource(R.string.app_name),
+				modifier = Modifier.width(400.dp)
+			)
+		}
+	}
+}
 
 class SplashFragment : Fragment() {
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-		FragmentSplashBinding.inflate(inflater, container, false).root
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	) = ComposeView(requireContext()).apply {
+		setContent {
+			SplashScreen()
+		}
+	}
 }
