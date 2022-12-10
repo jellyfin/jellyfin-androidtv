f0178663d5ab77d8c477934fcbad0a149eaa4f42
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2022-12-10 15:44:57 +0100

    Revert "Remember button focus on summary page"
    This reverts commit 0c8fe45b0668b9f64b99e6c96d3306b499379898.

--- app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
@@ -13,18 +13,4 @@ class DetailRowView @JvmOverloads constructor(
 	defStyleRes: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
 	val binding = ViewRowDetailsBinding.inflate(LayoutInflater.from(context), this, true)
-
-	private var lastFocusedButton: TextUnderButton? = null
-
-	init {
-		// Keep track of the last selected button and reselect it when navigating back to the buttons row
-		binding.root.viewTreeObserver.addOnGlobalFocusChangeListener { oldFocus, newFocus ->
-			if (oldFocus is TextUnderButton && newFocus !is TextUnderButton) {
-				lastFocusedButton = oldFocus
-			} else if (newFocus is TextUnderButton && lastFocusedButton != null) {
-				lastFocusedButton?.requestFocus()
-				lastFocusedButton = null
-			}
-		}
-	}
 }
