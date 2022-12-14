d739aba19077997b1e0a8f002eb0cd848f306b84
Teo Baranga <teodor08@live.com>
2022-12-14 11:27:40 +0000

    Remember button focus on details screen

--- app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
@@ -3,6 +3,8 @@ package org.jellyfin.androidtv.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.widget.FrameLayout
 import org.jellyfin.androidtv.databinding.ViewRowDetailsBinding
 
@@ -13,4 +15,38 @@ class DetailRowView @JvmOverloads constructor(
 	defStyleRes: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
 	val binding = ViewRowDetailsBinding.inflate(LayoutInflater.from(context), this, true)
+
+	/**
+	 * Keeps track of the last selected button and reselect it when navigating back to the buttons row.
+	 */
+	private val buttonsHierarchyChangeListener = object : OnHierarchyChangeListener {
+
+		private var lastFocusedButton: View? = null
+
+		private val focusChangeListener = OnFocusChangeListener { view, hasFocus ->
+			// Restore last focused button when navigating back to button row
+			if (hasFocus && lastFocusedButton != null) {
+				lastFocusedButton?.requestFocus()
+				lastFocusedButton = null
+			}
+			view.post {
+				// Store last focused button when navigating away from button row
+				if (binding.fdButtonRow.focusedChild == null) {
+					lastFocusedButton = view
+				}
+			}
+		}
+
+		override fun onChildViewAdded(parent: View?, child: View?) {
+			child?.onFocusChangeListener = focusChangeListener
+		}
+
+		override fun onChildViewRemoved(parent: View?, child: View?) {
+			child?.onFocusChangeListener = null
+		}
+	}
+
+	init {
+		binding.fdButtonRow.setOnHierarchyChangeListener(buttonsHierarchyChangeListener)
+	}
 }
