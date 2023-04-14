f99451d22e74fcb87123b09a08ceffa5c7af7840
Niels van Velzen <git@ndat.nl>
2023-04-14 22:17:34 +0200

    Fix app crash when playback stop is requested without an active player

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
@@ -1135,7 +1135,7 @@ public class PlaybackController implements PlaybackControllerNotifiable {
     }
 
     public void endPlayback(Boolean closeActivity) {
-        if (closeActivity) mFragment.getActivity().finish();
+        if (closeActivity && mFragment != null) mFragment.getActivity().finish();
         stop();
         if (mVideoManager != null)
             mVideoManager.destroy();
