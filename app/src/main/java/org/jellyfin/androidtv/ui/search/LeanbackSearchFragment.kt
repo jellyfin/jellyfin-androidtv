49ae93585c83ff99592b88f2cfb4998fb2a2cfb0
polson <302882+polson@users.noreply.github.com>
2023-05-11 12:09:49 -0500

    Cancel search requests when search box input changes

--- app/src/main/java/org/jellyfin/androidtv/ui/search/LeanbackSearchFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/search/LeanbackSearchFragment.kt
@@ -1,35 +1,50 @@
 package org.jellyfin.androidtv.ui.search
 
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.leanback.app.SearchSupportFragment
-import androidx.leanback.widget.ListRow
-import org.jellyfin.androidtv.data.service.BackgroundService
-import org.jellyfin.androidtv.ui.itemhandling.BaseRowItem
-import org.jellyfin.androidtv.ui.itemhandling.ItemLauncher
-import org.jellyfin.androidtv.ui.itemhandling.ItemRowAdapter
-import org.koin.java.KoinJavaComponent.inject
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class LeanbackSearchFragment : SearchSupportFragment() {
-	private val backgroundService = inject<BackgroundService>(BackgroundService::class.java)
+class LeanbackSearchFragment :
+	SearchSupportFragment(),
+	SearchSupportFragment.SearchResultProvider {
+
+	private val viewModel by viewModels<SearchViewModel>()
+
+	private val searchFragmentDelegate = SearchFragmentDelegate(this)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
-		// Create provider
-		val searchProvider = SearchProvider(requireContext(), lifecycle)
-		setSearchResultProvider(searchProvider)
-
-		// Add event listeners
-		setOnItemViewClickedListener { _, item, _, row ->
-			if (item !is BaseRowItem) return@setOnItemViewClickedListener
-
-			val adapter = (row as ListRow).adapter as ItemRowAdapter
-			ItemLauncher.launch(item as BaseRowItem?, adapter, item.index, requireContext())
-		}
-
-		setOnItemViewSelectedListener { _, item, _, _ ->
-			if (item is BaseRowItem) backgroundService.value.setBackground(item.searchHint!!)
-			else backgroundService.value.clearBackgrounds()
-		}
+		setSearchResultProvider(this)
+		setOnItemViewClickedListener(
+			searchFragmentDelegate.onItemViewClickedListener
+		)
+		setOnItemViewSelectedListener(
+			searchFragmentDelegate.onItemViewSelectedListener
+		)
 	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		observeSearchResults()
+	}
+
+	private fun observeSearchResults() {
+		viewModel.searchResultsFlow
+			.onEach {
+				searchFragmentDelegate.showResults(it)
+			}.launchIn(lifecycleScope)
+	}
+
+	override fun getResultsAdapter() = searchFragmentDelegate.rowsAdapter
+
+	override fun onQueryTextChange(query: String): Boolean =
+		viewModel.searchDebounced(query)
+
+	override fun onQueryTextSubmit(query: String): Boolean =
+		viewModel.search(query)
+
 }
