de79f82bdedcf42dea8eb5adfbaceffecd9dbb8e
DoggoOfSpeed <doggoofspeed@gmail.com>
2023-08-25 21:39:50 +0300

    Watched indicator setting string clarity improvements

--- app/src/main/java/org/jellyfin/androidtv/preference/constant/WatchedIndicatorBehavior.kt
+++ app/src/main/java/org/jellyfin/androidtv/preference/constant/WatchedIndicatorBehavior.kt
@@ -19,7 +19,7 @@ enum class WatchedIndicatorBehavior(
 	/**
 	 * Hide unwatched count indicator, show watched check mark on individual episodes only.
 	 */
-	EPISODES_ONLY(R.string.lbl_episode_marks),
+	EPISODES_ONLY(R.string.lbl_hide_watched_checkmark),
 
 	/**
 	 * Never show watched indicators.
