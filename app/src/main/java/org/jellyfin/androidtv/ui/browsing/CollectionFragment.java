9934713d95a683b8e27016294956b362b8d85b9d
Niels van Velzen <git@ndat.nl>
2023-12-23 12:54:24 +0100

    Fix opening collections via search results showing empty screen

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/CollectionFragment.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/CollectionFragment.java
@@ -1,52 +1,36 @@
 package org.jellyfin.androidtv.ui.browsing;
 
-import android.os.Bundle;
-
 import org.jellyfin.androidtv.R;
 import org.jellyfin.androidtv.data.querying.StdItemQuery;
-import org.jellyfin.androidtv.util.Utils;
 import org.jellyfin.apiclient.model.querying.ItemFields;
+import org.jellyfin.sdk.model.api.BaseItemKind;
 
 public class CollectionFragment extends EnhancedBrowseFragment {
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-    }
-
     @Override
     protected void setupQueries(RowLoader rowLoader) {
-        if (Utils.getSafeValue(mFolder.getChildCount(), 0) > 0) {
-            StdItemQuery movies = new StdItemQuery(new ItemFields[]{
-                    ItemFields.PrimaryImageAspectRatio,
-                    ItemFields.Overview,
-                    ItemFields.ItemCounts,
-                    ItemFields.DisplayPreferencesId,
-                    ItemFields.ChildCount,
-                    ItemFields.MediaStreams,
-                    ItemFields.MediaSources
-            });
-            movies.setParentId(mFolder.getId().toString());
-            movies.setIncludeItemTypes(new String[]{"Movie"});
-            mRows.add(new BrowseRowDef(getString(R.string.lbl_movies), movies, 100));
+        StdItemQuery movies = new StdItemQuery(new ItemFields[]{
+                ItemFields.PrimaryImageAspectRatio,
+                ItemFields.Overview,
+                ItemFields.ItemCounts,
+                ItemFields.DisplayPreferencesId,
+                ItemFields.ChildCount,
+                ItemFields.MediaStreams,
+                ItemFields.MediaSources
+        });
+        movies.setParentId(mFolder.getId().toString());
+        movies.setIncludeItemTypes(new String[]{BaseItemKind.MOVIE.getSerialName()});
+        mRows.add(new BrowseRowDef(getString(R.string.lbl_movies), movies, 100));
 
-            StdItemQuery series = new StdItemQuery();
-            series.setParentId(mFolder.getId().toString());
-            series.setIncludeItemTypes(new String[]{"Series"});
-            mRows.add(new BrowseRowDef(getString(R.string.lbl_tv_series), series, 100));
+        StdItemQuery series = new StdItemQuery();
+        series.setParentId(mFolder.getId().toString());
+        series.setIncludeItemTypes(new String[]{BaseItemKind.SERIES.getSerialName()});
+        mRows.add(new BrowseRowDef(getString(R.string.lbl_tv_series), series, 100));
 
-            StdItemQuery others = new StdItemQuery();
-            others.setParentId(mFolder.getId().toString());
-            others.setExcludeItemTypes(new String[]{"Movie", "Series"});
-            mRows.add(new BrowseRowDef(getString(R.string.lbl_other), others, 100));
+        StdItemQuery others = new StdItemQuery();
+        others.setParentId(mFolder.getId().toString());
+        others.setExcludeItemTypes(new String[]{BaseItemKind.MOVIE.getSerialName(), BaseItemKind.SERIES.getSerialName()});
+        mRows.add(new BrowseRowDef(getString(R.string.lbl_other), others, 100));
 
-
-            rowLoader.loadRows(mRows);
-        }
-
-
+        rowLoader.loadRows(mRows);
     }
-
-
 }
