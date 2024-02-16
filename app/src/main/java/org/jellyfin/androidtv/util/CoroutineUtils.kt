bd704c4ac959603ac9c163697710c51308bf69dd
Niels van Velzen <git@ndat.nl>
2024-02-16 22:23:48 +0100

    Replace CoroutineUtils.runBlocking with new CoroutineUtils.runOnLifecycle
    This will prevent the main thread from blocking, which in extreme cases can kill the app

--- app/src/main/java/org/jellyfin/androidtv/util/CoroutineUtils.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/CoroutineUtils.kt
@@ -8,12 +8,14 @@ import androidx.lifecycle.flowWithLifecycle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.constant.CustomMessage
 import org.jellyfin.androidtv.data.repository.CustomMessageRepository
 
-fun <T : Any> runBlocking(block: suspend CoroutineScope.() -> T) = kotlinx.coroutines.runBlocking {
-	block()
-}
+fun <T : Any> runOnLifecycle(
+	lifecycle: Lifecycle,
+	block: suspend CoroutineScope.() -> T
+) = lifecycle.coroutineScope.launch { block() }
 
 fun readCustomMessagesOnLifecycle(
 	lifecycle: Lifecycle,
