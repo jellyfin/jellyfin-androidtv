3616a122e7c6a2f88569de6256d76202bde02d95
Niels van Velzen <git@ndat.nl>
2024-02-18 09:56:52 +0100

    Add "clear image cache" button to developer preferences

--- app/src/main/java/org/jellyfin/androidtv/ui/preference/screen/DeveloperPreferencesScreen.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/preference/screen/DeveloperPreferencesScreen.kt
@@ -1,10 +1,14 @@
 package org.jellyfin.androidtv.ui.preference.screen
 
+import android.text.format.Formatter
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
 import org.jellyfin.androidtv.BuildConfig
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.preference.SystemPreferences
 import org.jellyfin.androidtv.preference.UserPreferences
 import org.jellyfin.androidtv.ui.preference.dsl.OptionsFragment
+import org.jellyfin.androidtv.ui.preference.dsl.action
 import org.jellyfin.androidtv.ui.preference.dsl.checkbox
 import org.jellyfin.androidtv.ui.preference.dsl.optionsScreen
 import org.jellyfin.androidtv.util.isTvDevice
@@ -13,6 +17,7 @@ import org.koin.android.ext.android.inject
 class DeveloperPreferencesScreen : OptionsFragment() {
 	private val userPreferences: UserPreferences by inject()
 	private val systemPreferences: SystemPreferences by inject()
+	private val imageLoader: ImageLoader by inject()
 
 	override val screen by optionsScreen {
 		setTitle(R.string.pref_developer_link)
@@ -48,6 +53,17 @@ class DeveloperPreferencesScreen : OptionsFragment() {
 					setContent(R.string.enable_playback_module_description)
 
 					bind(userPreferences, UserPreferences.playbackRewriteVideoEnabled)
+				}
+			}
+
+			@OptIn(ExperimentalCoilApi::class)
+			action {
+				setTitle(R.string.clear_image_cache)
+				content = getString(R.string.clear_image_cache_content, Formatter.formatFileSize(context, imageLoader.diskCache?.size ?: 0))
+				onActivate = {
+					imageLoader.memoryCache?.clear()
+					imageLoader.diskCache?.clear()
+					rebuild()
 				}
 			}
 		}
