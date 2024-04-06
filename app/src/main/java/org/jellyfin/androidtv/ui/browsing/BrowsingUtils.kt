a00e6bb555aebbcbe81a1846c503c36965af8c60
Niels van Velzen <git@ndat.nl>
2024-04-06 16:49:11 +0200

    Add button to pick a random item to the smart screen
    Works for movies, tv series and music (random album)

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/BrowsingUtils.kt
@@ -0,0 +1,34 @@
+package org.jellyfin.androidtv.ui.browsing
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import org.jellyfin.sdk.api.client.ApiClient
+import org.jellyfin.sdk.api.client.exception.ApiClientException
+import org.jellyfin.sdk.api.client.extensions.itemsApi
+import org.jellyfin.sdk.model.api.BaseItemDto
+import org.jellyfin.sdk.model.api.BaseItemKind
+import org.jellyfin.sdk.model.constant.ItemSortBy
+import timber.log.Timber
+
+object BrowsingUtils {
+	@JvmStatic
+	fun getRandomItem(api: ApiClient, lifecycle: LifecycleOwner, library: BaseItemDto, type: BaseItemKind, callback: (item: BaseItemDto?) -> Unit) {
+		lifecycle.lifecycleScope.launch {
+			try {
+				val result by api.itemsApi.getItemsByUserId(
+					parentId = library.id,
+					includeItemTypes = setOf(type),
+					recursive = true,
+					sortBy = setOf(ItemSortBy.Random),
+					limit = 1,
+				)
+
+				callback(result.items?.firstOrNull())
+			} catch (error: ApiClientException) {
+				Timber.w(error, "Failed to retrieve random item")
+				callback(null)
+			}
+		}
+	}
+}
