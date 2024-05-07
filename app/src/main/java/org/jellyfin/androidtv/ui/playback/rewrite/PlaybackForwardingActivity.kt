84a16804fa3eb2b163caa0f110b2260cdb1be2cd
Niels van Velzen <git@ndat.nl>
2024-05-07 14:42:19 +0200

    Update PlaybackForwardingActivity to show video output

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackForwardingActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackForwardingActivity.kt
@@ -2,26 +2,33 @@ package org.jellyfin.androidtv.ui.playback.rewrite
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.ui.playback.VideoQueueManager
+import org.jellyfin.playback.core.PlaybackManager
+import org.jellyfin.playback.core.queue.Queue
+import org.jellyfin.playback.core.queue.QueueEntry
+import org.jellyfin.playback.core.ui.PlayerSurfaceView
+import org.jellyfin.playback.jellyfin.queue.createBaseItemQueueEntry
 import org.jellyfin.sdk.api.client.ApiClient
 import org.jellyfin.sdk.api.client.extensions.userLibraryApi
+import org.jellyfin.sdk.model.api.BaseItemDto
 import org.jellyfin.sdk.model.api.BaseItemKind
 import org.jellyfin.sdk.model.serializer.toUUIDOrNull
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 import java.util.UUID
 
-class PlaybackForwardingActivity : FragmentActivity() {
+class PlaybackForwardingActivity : ComponentActivity() {
 	companion object {
 		const val EXTRA_ITEM_ID: String = "item_id"
 	}
 
 	private val videoQueueManager by inject<VideoQueueManager>()
+	private val playbackManager by inject<PlaybackManager>()
 	private val api by inject<ApiClient>()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,17 +60,30 @@ class PlaybackForwardingActivity : FragmentActivity() {
 				).show()
 				Timber.i(item.toString())
 
-				// TODO: Create queue, send to new playback manager, start new player UI
-				finishAfterTransition()
+				// TODO: Dirty hack to create a single item queue
+				val queueEntry = createBaseItemQueueEntry(api, item)
+				playbackManager.state.queue.replaceQueue(object : Queue {
+					override val size: Int = 1
+
+					override suspend fun getItem(index: Int): QueueEntry? {
+						if (index == 0) return queueEntry
+						return null
+					}
+				})
 			}
 		}
+
+		// TODO: Dirty hack to display surface
+		val view = PlayerSurfaceView(this)
+		view.playbackManager = playbackManager
+		setContentView(view)
 	}
 
 	private fun findItemId(): UUID? {
 		val extra = intent.getStringExtra(EXTRA_ITEM_ID)?.toUUIDOrNull()
 
-		var first: org.jellyfin.sdk.model.api.BaseItemDto? = null
-		var best: org.jellyfin.sdk.model.api.BaseItemDto? = null
+		var first: BaseItemDto? = null
+		var best: BaseItemDto? = null
 
 		for (item in videoQueueManager.getCurrentVideoQueue()) {
 			if (first == null) first = item
