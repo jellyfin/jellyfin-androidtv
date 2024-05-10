79ce9e3ca4a2812305c341e1f055a9b57cded155
Niels van Velzen <git@ndat.nl>
2024-05-10 12:33:52 +0200

    Read items to play from legacy video queue

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackRewriteFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackRewriteFragment.kt
@@ -4,26 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.ui.playback.VideoQueueManager
 import org.jellyfin.playback.core.PlaybackManager
-import org.jellyfin.playback.core.queue.Queue
-import org.jellyfin.playback.core.queue.QueueEntry
 import org.jellyfin.playback.core.ui.PlayerSurfaceView
-import org.jellyfin.playback.jellyfin.queue.createBaseItemQueueEntry
 import org.jellyfin.sdk.api.client.ApiClient
-import org.jellyfin.sdk.api.client.extensions.userLibraryApi
-import org.jellyfin.sdk.model.api.BaseItemDto
-import org.jellyfin.sdk.model.api.BaseItemKind
-import org.jellyfin.sdk.model.serializer.toUUIDOrNull
 import org.koin.android.ext.android.inject
 import timber.log.Timber
-import java.util.UUID
 
 /**
  * Temporary fragment used for testing the playback rewrite. This will eventually be replaced with a
@@ -31,7 +20,7 @@ import java.util.UUID
  */
 class PlaybackRewriteFragment : Fragment() {
 	companion object {
-		const val EXTRA_ITEM_ID: String = "item_id"
+		const val EXTRA_POSITION: String = "position"
 	}
 
 	private val videoQueueManager by inject<VideoQueueManager>()
@@ -41,41 +30,18 @@ class PlaybackRewriteFragment : Fragment() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		// Try find the item id
-		val itemId = findItemId()
+		// Create a queue from the items added to the legacy video queue
+		val queue = RewriteMediaManager.BaseItemQueue(api)
+		queue.items.addAll(videoQueueManager.getCurrentVideoQueue())
+		Timber.i("Created a queue with ${queue.items.size} items")
+		playbackManager.state.queue.replaceQueue(queue)
 
-		if (itemId == null) {
-			Toast.makeText(
-				requireContext(),
-				"Could not find item to play (itemId=null)",
-				Toast.LENGTH_LONG
-			).show()
-			return
-		}
-
-		lifecycleScope.launch {
-			lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-				// Get fresh info from API in SDK format
-				val item by api.userLibraryApi.getItem(itemId = itemId)
-
-				// Log info
-				Toast.makeText(
-					requireContext(),
-					"Found item of type ${item.type} - ${item.name} (${item.id}",
-					Toast.LENGTH_LONG
-				).show()
-				Timber.i(item.toString())
-
-				// TODO: Dirty hack to create a single item queue
-				val queueEntry = createBaseItemQueueEntry(api, item)
-				playbackManager.state.queue.replaceQueue(object : Queue {
-					override val size: Int = 1
-
-					override suspend fun getItem(index: Int): QueueEntry? {
-						if (index == 0) return queueEntry
-						return null
-					}
-				})
+		// Set position
+		val position = arguments?.getInt(EXTRA_POSITION) ?: 0
+		if (position != 0) {
+			lifecycleScope.launch {
+				Timber.i("Skipping to queue item $position")
+				playbackManager.state.queue.setIndex(position, false)
 			}
 		}
 	}
@@ -84,21 +50,5 @@ class PlaybackRewriteFragment : Fragment() {
 		val view = PlayerSurfaceView(requireContext())
 		view.playbackManager = playbackManager
 		return view
-	}
-
-	private fun findItemId(): UUID? {
-		val extra = requireArguments().getString(EXTRA_ITEM_ID)?.toUUIDOrNull()
-
-		var first: BaseItemDto? = null
-		var best: BaseItemDto? = null
-
-		for (item in videoQueueManager.getCurrentVideoQueue()) {
-			if (first == null) first = item
-			if (item.type !== BaseItemKind.TRAILER) best = item
-
-			if (best != null) break
-		}
-
-		return extra ?: best?.id ?: first?.id
 	}
 }
