1d77f9fdc8a8b5b3b95dfe03bc296a40bf628e60
Niels van Velzen <git@ndat.nl>
2024-05-10 12:59:55 +0200

    Use screensaverLock in PlaybackRewriteFragment

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackRewriteFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/rewrite/PlaybackRewriteFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import org.jellyfin.androidtv.ui.ScreensaverViewModel
 import org.jellyfin.androidtv.ui.playback.VideoQueueManager
 import org.jellyfin.playback.core.PlaybackManager
+import org.jellyfin.playback.core.model.PlayState
 import org.jellyfin.playback.core.ui.PlayerSurfaceView
 import org.jellyfin.sdk.api.client.ApiClient
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import timber.log.Timber
 
 /**
@@ -26,6 +33,9 @@ class PlaybackRewriteFragment : Fragment() {
 	private val videoQueueManager by inject<VideoQueueManager>()
 	private val playbackManager by inject<PlaybackManager>()
 	private val api by inject<ApiClient>()
+
+	private val screensaverViewModel by activityViewModel<ScreensaverViewModel>()
+	private var screensaverLock: (() -> Unit)? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -47,6 +57,20 @@ class PlaybackRewriteFragment : Fragment() {
 
 		// Pause player until the initial resume
 		playbackManager.state.pause()
+
+		// Add lifecycle listeners
+		lifecycleScope.launch {
+			repeatOnLifecycle(Lifecycle.State.RESUMED) {
+				playbackManager.state.playState.onEach { playState ->
+					if (playState == PlayState.PLAYING && screensaverLock == null) {
+						screensaverLock = screensaverViewModel.addLifecycleLock(lifecycle)
+					} else if (playState != PlayState.PLAYING && screensaverLock != null) {
+						screensaverLock?.invoke()
+						screensaverLock = null
+					}
+				}.launchIn(this)
+			}
+		}
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
