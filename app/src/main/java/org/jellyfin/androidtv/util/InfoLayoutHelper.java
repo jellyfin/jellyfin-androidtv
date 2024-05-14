c798807e9f9c1b9f3cb7a26946187c573fd550f8
Niels van Velzen <git@ndat.nl>
2024-05-14 15:11:44 +0200

    Move addBlockText to ProgramGridCell

--- app/src/main/java/org/jellyfin/androidtv/util/InfoLayoutHelper.java
+++ app/src/main/java/org/jellyfin/androidtv/util/InfoLayoutHelper.java
@@ -3,7 +3,6 @@ package org.jellyfin.androidtv.util;
 import android.content.Context;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import org.jellyfin.androidtv.ui.browsing.composable.inforow.BaseItemInfoRowView;
 import org.jellyfin.sdk.model.api.BaseItemDto;
@@ -30,17 +29,5 @@ public class InfoLayoutHelper {
         // Update item info
         baseItemInfoRowView.setItem(item);
         baseItemInfoRowView.setIncludeRuntime(includeRuntime);
-    }
-
-    public static void addBlockText(Context context, LinearLayout layout, String text, int size, int textColor, int backgroundRes) {
-        TextView view = new TextView(context);
-        view.setTextSize(size);
-        view.setTextColor(textColor);
-        view.setText(" " + text + " ");
-        view.setBackgroundResource(backgroundRes);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        params.setMargins(0, Utils.convertDpToPixel(context, -2), 0, 0);
-        view.setLayoutParams(params);
-        layout.addView(view);
     }
 }
