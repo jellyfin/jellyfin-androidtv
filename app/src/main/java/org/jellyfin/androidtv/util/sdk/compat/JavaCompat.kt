88e8a28f9ebbfb05af8373c742843c6fa43baef8
Niels van Velzen <git@ndat.nl>
2024-05-19 14:13:41 +0200

    Use SDK types in FullDetailsFragment

--- app/src/main/java/org/jellyfin/androidtv/util/sdk/compat/JavaCompat.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/sdk/compat/JavaCompat.kt
@@ -4,6 +4,9 @@ package org.jellyfin.androidtv.util.sdk.compat
 
 import org.jellyfin.sdk.model.api.BaseItemDto
 import org.jellyfin.sdk.model.api.MediaSourceInfo
+import org.jellyfin.sdk.model.api.UserItemDataDto
+import java.time.LocalDateTime
+import java.util.UUID
 import org.jellyfin.apiclient.model.dto.BaseItemDto as LegacyBaseItemdto
 
 fun BaseItemDto.copyWithDisplayPreferencesId(
@@ -12,16 +15,49 @@ fun BaseItemDto.copyWithDisplayPreferencesId(
 	displayPreferencesId = displayPreferencesId,
 )
 
+fun BaseItemDto.copyWithDates(
+	premiereDate: LocalDateTime?,
+	endDate: LocalDateTime?,
+	officialRating: String?,
+	runTimeTicks: Long?,
+) = copy(
+	premiereDate = premiereDate,
+	endDate = endDate,
+	officialRating = officialRating,
+	runTimeTicks = runTimeTicks,
+)
+
 fun BaseItemDto.copyWithTimerId(
 	seriesTimerId: String?,
 ) = copy(
 	seriesTimerId = seriesTimerId,
 )
 
+fun BaseItemDto.copyWithOverview(
+	overview: String?,
+) = copy(
+	overview = overview,
+)
+
+fun BaseItemDto.copyWithParentId(
+	parentId: UUID?,
+) = copy(
+	parentId = parentId,
+)
+
+fun BaseItemDto.copyWithUserData(
+	userData: UserItemDataDto?,
+) = copy(
+	userData = userData,
+)
+
 fun BaseItemDto.getResumePositionTicks() = userData?.playbackPositionTicks ?: 0
 
 fun Collection<LegacyBaseItemdto>.mapBaseItemCollection(): List<BaseItemDto> = map { it.asSdk() }
+fun Array<LegacyBaseItemdto>.mapBaseItemArray(): List<BaseItemDto> = map { it.asSdk() }
 
 fun MediaSourceInfo.getVideoStream() = mediaStreams?.firstOrNull {
 	it.type == org.jellyfin.sdk.model.api.MediaStreamType.VIDEO
 }
+
+val BaseItemDto.canResume get() = (userData?.playbackPositionTicks ?: 0) > 0
