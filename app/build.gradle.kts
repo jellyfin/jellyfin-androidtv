c22379074e06ed7723d9337c72b5e37f56900ca3
Pascal Weidenhammer <pascal@paswei.com>
2024-05-21 00:16:27 +0200

    Add AndroidX TV Material implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,6 +132,7 @@ dependencies {
 	implementation(libs.androidx.cardview)
 	implementation(libs.androidx.startup)
 	implementation(libs.bundles.androidx.compose)
+	implementation(libs.androidx.tv.material)
 
 	// Dependency Injection
 	implementation(libs.bundles.koin)
