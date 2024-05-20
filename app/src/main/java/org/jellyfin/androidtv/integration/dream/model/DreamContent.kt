d13123c4e5a09a5be1ed87134a6d15c9cb5f5a73
Niels van Velzen <git@ndat.nl>
2024-05-20 12:49:05 +0200

    Implement lyrics

--- app/src/main/java/org/jellyfin/androidtv/integration/dream/model/DreamContent.kt
+++ app/src/main/java/org/jellyfin/androidtv/integration/dream/model/DreamContent.kt
@@ -2,9 +2,10 @@ package org.jellyfin.androidtv.integration.dream.model
 
 import android.graphics.Bitmap
 import org.jellyfin.sdk.model.api.BaseItemDto
+import org.jellyfin.sdk.model.api.LyricDto
 
 sealed interface DreamContent {
 	data object Logo : DreamContent
 	data class LibraryShowcase(val item: BaseItemDto, val backdrop: Bitmap, val logo: Bitmap?) : DreamContent
-	data class NowPlaying(val item: BaseItemDto?) : DreamContent
+	data class NowPlaying(val item: BaseItemDto, val lyrics: LyricDto?) : DreamContent
 }
