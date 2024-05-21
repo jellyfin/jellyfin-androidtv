e6c047c2e6cb9dcaabe451bb58aba470b7e1021d
Pascal Weidenhammer <pascal@paswei.com>
2024-05-21 14:41:14 +0200

    Add Material Theme and remove back button from ConnectHelpAlert
    There should be no onscreen back buttons, because the tv remote has a dedicated back button: https://developer.android.com/design/ui/tv/guides/foundations/navigation-on-tv#dont-display-a-back-button

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
@@ -4,41 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.compose.content
-import androidx.tv.material3.Button
-import androidx.tv.material3.ButtonDefaults
-import androidx.tv.material3.Icon
+import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
 import androidx.tv.material3.MaterialTheme
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.composable.overscan
+import org.jellyfin.androidtv.ui.shared.theme.JellyfinMaterialTheme
 
 class ConnectHelpAlertFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -49,61 +38,40 @@ class ConnectHelpAlertFragment : Fragment() {
 
     @Composable
     fun ConnectHelpAlert() {
-        val focusRequester = FocusRequester()
-        Surface(
-            color = colorResource(id = R.color.not_quite_black),
-            modifier = Modifier
-                .overscan()
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly
+        JellyfinMaterialTheme {
+            Surface(
             ) {
-                Column(
+                Row(
                     modifier = Modifier
-                        .width(400.dp)
-                        .align(Alignment.CenterVertically)
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .overscan(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Text(
-                        text = stringResource(id = R.string.login_help_title),
-                        style = MaterialTheme.typography.displayMedium,
-                        color = Color.White
-                    )
-                    Text(
-                        modifier = Modifier.padding(top = 16.dp, bottom = 32.dp),
-                        text = stringResource(id = R.string.login_help_description),
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = Color.White
-                    )
-                    Button(
+                    Column(
                         modifier = Modifier
-                            .focusRequester(focusRequester)
-                            .focusable(),
-                        onClick = {
-                            parentFragmentManager.popBackStack()
-                        }
+                            .width(400.dp)
+                            .align(Alignment.CenterVertically)
                     ) {
-                        Icon(
-                            imageVector = Icons.Default.Done,
-                            contentDescription = null,
-                            modifier = Modifier.size(ButtonDefaults.IconSize)
+                        Text(
+                            text = stringResource(R.string.login_help_title),
+                            style = MaterialTheme.typography.displayMedium
                         )
-                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                        Text(stringResource(id = R.string.lbl_ok))
+                        Text(
+                            modifier = Modifier.padding(top = 16.dp),
+                            text = stringResource(R.string.login_help_description),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
                     }
+                    Image(
+                        painter = painterResource(R.drawable.qr_jellyfin_docs),
+                        contentDescription = stringResource(R.string.app_name),
+                        modifier = Modifier
+                            .width(200.dp)
+                            .align(Alignment.CenterVertically)
+                    )
                 }
-                Image(
-                    painter = painterResource(R.drawable.qr_jellyfin_docs),
-                    contentDescription = stringResource(R.string.app_name),
-                    modifier = Modifier
-                        .width(200.dp)
-                        .align(Alignment.CenterVertically)
-                )
             }
-        }
-        LaunchedEffect(Unit) {
-            focusRequester.requestFocus()
         }
     }
 }
