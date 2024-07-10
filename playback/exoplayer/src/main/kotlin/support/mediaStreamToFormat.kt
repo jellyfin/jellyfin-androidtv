76f8608f4fe8439e8438b3ee683777318ace8253
Niels van Velzen <git@ndat.nl>
2024-07-10 17:34:47 +0200

    Add direct play support for WAV audio

--- playback/exoplayer/src/main/kotlin/support/mediaStreamToFormat.kt
+++ playback/exoplayer/src/main/kotlin/support/mediaStreamToFormat.kt
@@ -1,7 +1,9 @@
 package org.jellyfin.playback.exoplayer.support
 
 import androidx.annotation.OptIn
+import androidx.media3.common.C
 import androidx.media3.common.Format
+import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
 import org.jellyfin.playback.core.mediastream.MediaStream
 import org.jellyfin.playback.core.mediastream.MediaStreamAudioTrack
@@ -11,12 +13,31 @@ import org.jellyfin.playback.exoplayer.mapping.getFfmpegContainerMimeType
 import org.jellyfin.playback.exoplayer.mapping.getFfmpegVideoMimeType
 
 @OptIn(UnstableApi::class)
+private val PCM_CODECS = mapOf(
+	"pcm_s8" to C.ENCODING_PCM_8BIT,
+	"pcm_s16le" to C.ENCODING_PCM_16BIT,
+	"pcm_s16be" to C.ENCODING_PCM_16BIT_BIG_ENDIAN,
+	"pcm_s24le" to C.ENCODING_PCM_24BIT,
+	"pcm_s24be" to C.ENCODING_PCM_24BIT_BIG_ENDIAN,
+	"pcm_s32le" to C.ENCODING_PCM_32BIT,
+	"pcm_s32be" to C.ENCODING_PCM_32BIT_BIG_ENDIAN,
+	"pcm_f32le" to C.ENCODING_PCM_FLOAT,
+)
+
+@OptIn(UnstableApi::class)
 fun toFormat(stream: MediaStream, track: MediaStreamAudioTrack) = Format.Builder().also { f ->
 	f.setId(stream.identifier)
 	f.setContainerMimeType(getFfmpegContainerMimeType(stream.container.format))
 
-	f.setCodecs(track.codec)
-	f.setSampleMimeType(getFfmpegAudioMimeType(track.codec))
+	val pcmEncoding = PCM_CODECS[track.codec]
+	if (pcmEncoding != null) {
+		f.setSampleMimeType(MimeTypes.AUDIO_RAW)
+		f.setPcmEncoding(pcmEncoding)
+	} else {
+		f.setCodecs(track.codec)
+		f.setSampleMimeType(getFfmpegAudioMimeType(track.codec))
+	}
+
 	f.setChannelCount(track.channels)
 	f.setAverageBitrate(track.bitrate)
 	f.setSampleRate(track.sampleRate)
