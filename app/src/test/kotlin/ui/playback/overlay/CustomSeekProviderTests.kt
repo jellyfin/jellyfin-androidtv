c03bec878b39e9a228588860f343071090788a87
Eric Leblanc <ericleb010@gmail.com>
2024-08-23 22:05:39 -0400

    more granular seek setting

--- app/src/test/kotlin/ui/playback/overlay/CustomSeekProviderTests.kt
+++ app/src/test/kotlin/ui/playback/overlay/CustomSeekProviderTests.kt
@@ -9,21 +9,21 @@ class CustomSeekProviderTests : FunSpec({
 	test("CustomSeekProvider.seekPositions with simple duration") {
 		val videoPlayerAdapter = mockk<VideoPlayerAdapter> {
 			every { canSeek() } returns true
-			every { duration } returns 90000L
+			every { duration } returns 30000L
 		}
 		val customSeekProvider = CustomSeekProvider(videoPlayerAdapter)
 
-		customSeekProvider.seekPositions shouldBe arrayOf(0L, 30000L, 60000L, 90000L)
+		customSeekProvider.seekPositions shouldBe arrayOf(0L, 10000L, 20000L, 30000L)
 	}
 
 	test("CustomSeekProvider.seekPositions with odd duration") {
 		val videoPlayerAdapter = mockk<VideoPlayerAdapter> {
 			every { canSeek() } returns true
-			every { duration } returns 130000L
+			every { duration } returns 45000L
 		}
 		val customSeekProvider = CustomSeekProvider(videoPlayerAdapter)
 
-		customSeekProvider.seekPositions shouldBe arrayOf(0L, 30000, 60000, 90000, 120000, 130000)
+		customSeekProvider.seekPositions shouldBe arrayOf(0L, 10000, 20000, 30000, 40000, 45000)
 	}
 
 	test("CustomSeekProvider.seekPositions with seek disabled") {
