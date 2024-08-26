203388bed299ef3e24e16ff5796529b8befc0597
MichaelRUSF <78215956+MichaelRUSF@users.noreply.github.com>
2024-08-26 12:58:57 -0400

    Add HDMI ARC mode

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackController.java
@@ -514,7 +514,8 @@ public class PlaybackController implements PlaybackControllerNotifiable {
         DeviceProfile internalProfile = new ExoPlayerProfile(
                 isLiveTv && !userPreferences.getValue().get(UserPreferences.Companion.getLiveTvDirectPlayEnabled()),
                 userPreferences.getValue().get(UserPreferences.Companion.getAc3Enabled()),
-                userPreferences.getValue().get(UserPreferences.Companion.getAudioBehaviour()) == AudioBehavior.DOWNMIX_TO_STEREO
+                userPreferences.getValue().get(UserPreferences.Companion.getAudioBehaviour()) == AudioBehavior.DOWNMIX_TO_STEREO,
+                userPreferences.getValue().get(UserPreferences.Companion.getAudioBehaviour()) == AudioBehavior.HDMI_ARC_OUTPUT
         );
         internalOptions.setProfile(internalProfile);
         return internalOptions;
