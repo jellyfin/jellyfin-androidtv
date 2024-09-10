b9d34f7fe763f2a8430e124d5f525c2b65cddd03
Niels van Velzen <git@ndat.nl>
2024-09-10 14:56:43 +0200

    Refactor ConnectHelpAlertFragment

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/ConnectHelpAlertFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -38,85 +38,90 @@ import androidx.tv.material3.Text
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.composable.overscan
 
-class ConnectHelpAlertFragment : Fragment() {
+@Composable
+private fun ConnectHelpAlert(
+	onClose: () -> Unit,
+) {
+	val focusRequester = remember { FocusRequester() }
 
+	Surface(
+		colors = SurfaceDefaults.colors(
+			containerColor = colorResource(R.color.not_quite_black),
+			contentColor = colorResource(R.color.white),
+		)
+	) {
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.fillMaxHeight()
+				.overscan(),
+			horizontalArrangement = Arrangement.SpaceEvenly,
+		) {
+			Column(
+				modifier = Modifier
+					.width(400.dp)
+					.align(Alignment.CenterVertically),
+			) {
+				Text(
+					text = stringResource(R.string.login_help_title),
+					style = MaterialTheme.typography.displayMedium,
+				)
+				Text(
+					modifier = Modifier.padding(top = 16.dp),
+					text = stringResource(R.string.login_help_description),
+					style = MaterialTheme.typography.bodyLarge,
+				)
+				Button(
+					modifier = Modifier
+						.padding(top = 24.dp)
+						.align(Alignment.Start)
+						.focusRequester(focusRequester),
+					onClick = onClose,
+					colors = ButtonDefaults.colors(
+						containerColor = colorResource(R.color.button_default_normal_background),
+						contentColor = colorResource(R.color.button_default_normal_text),
+						focusedContainerColor = colorResource(R.color.button_default_highlight_background),
+						focusedContentColor = colorResource(R.color.button_default_highlight_text),
+						disabledContainerColor = colorResource(R.color.button_default_disabled_background),
+						disabledContentColor = colorResource(R.color.button_default_disabled_text),
+					),
+				) {
+					Icon(
+						imageVector = Icons.Default.Done,
+						contentDescription = null,
+						modifier = Modifier.size(ButtonDefaults.IconSize),
+					)
+					Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+					Text(
+						text = stringResource(id = R.string.btn_got_it),
+						style = MaterialTheme.typography.bodyLarge,
+					)
+				}
+			}
+
+			Image(
+				painter = painterResource(R.drawable.qr_jellyfin_docs),
+				contentDescription = stringResource(R.string.app_name),
+				modifier = Modifier
+					.width(200.dp)
+					.align(Alignment.CenterVertically),
+			)
+		}
+	}
+
+	LaunchedEffect(focusRequester) {
+		focusRequester.requestFocus()
+	}
+}
+
+class ConnectHelpAlertFragment : Fragment() {
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	) = content {
-		ConnectHelpAlert()
-	}
-
-	@Composable
-	fun ConnectHelpAlert() {
-		val focusRequester = FocusRequester()
-
-		Surface(
-			colors = SurfaceDefaults.colors(
-				containerColor = colorResource(R.color.not_quite_black),
-				contentColor = colorResource(R.color.white)
-			)
-		) {
-			Row(
-				modifier = Modifier
-					.fillMaxWidth()
-					.fillMaxHeight()
-					.overscan(),
-				horizontalArrangement = Arrangement.SpaceEvenly
-			) {
-				Column(
-					modifier = Modifier
-						.width(400.dp)
-						.align(Alignment.CenterVertically)
-				) {
-					Text(
-						text = stringResource(R.string.login_help_title),
-						style = MaterialTheme.typography.displayMedium
-					)
-					Text(
-						modifier = Modifier.padding(top = 16.dp),
-						text = stringResource(R.string.login_help_description),
-						style = MaterialTheme.typography.bodyLarge
-					)
-					Button(
-						modifier = Modifier
-							.padding(top = 24.dp)
-							.align(Alignment.Start)
-							.focusRequester(focusRequester).focusable(),
-						onClick = { parentFragmentManager.popBackStack() },
-						colors = ButtonDefaults.colors(
-							containerColor = colorResource(R.color.button_default_normal_background),
-							contentColor = colorResource(R.color.button_default_normal_text),
-							focusedContainerColor = colorResource(R.color.button_default_highlight_background),
-							focusedContentColor = colorResource(R.color.button_default_highlight_text),
-							disabledContainerColor = colorResource(R.color.button_default_disabled_background),
-							disabledContentColor = colorResource(R.color.button_default_disabled_text)
-						)
-					) {
-						Icon(
-							imageVector = Icons.Default.Done,
-							contentDescription = null,
-							modifier = Modifier.size(ButtonDefaults.IconSize)
-						)
-						Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-						Text(
-							text = stringResource(id = R.string.btn_got_it),
-							style = MaterialTheme.typography.bodyLarge
-						)
-					}
-				}
-				Image(
-					painter = painterResource(R.drawable.qr_jellyfin_docs),
-					contentDescription = stringResource(R.string.app_name),
-					modifier = Modifier
-						.width(200.dp)
-						.align(Alignment.CenterVertically)
-				)
-			}
-		}
-		LaunchedEffect(Unit) {
-			focusRequester.requestFocus()
-		}
+		ConnectHelpAlert(
+			onClose = { parentFragmentManager.popBackStack() },
+		)
 	}
 }
