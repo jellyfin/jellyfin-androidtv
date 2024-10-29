62d9b1a622046969857ff4cf0c11aefb695b4f26
Niels van Velzen <git@ndat.nl>
2024-10-29 17:38:26 +0100

    Rewrite PlaybackManager with SDK

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/PlaybackManager.kt
@@ -0,0 +1,115 @@
+package org.jellyfin.androidtv.ui.playback
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import org.jellyfin.androidtv.data.compat.PlaybackException
+import org.jellyfin.androidtv.data.compat.StreamInfo
+import org.jellyfin.androidtv.data.compat.VideoOptions
+import org.jellyfin.apiclient.interaction.Response
+import org.jellyfin.sdk.api.client.ApiClient
+import org.jellyfin.sdk.api.client.extensions.hlsSegmentApi
+import org.jellyfin.sdk.api.client.extensions.mediaInfoApi
+import org.jellyfin.sdk.api.client.extensions.videosApi
+import org.jellyfin.sdk.model.api.PlayMethod
+import org.jellyfin.sdk.model.api.PlaybackInfoDto
+import org.jellyfin.sdk.model.api.PlaybackInfoResponse
+
+private fun createStreamInfo(
+	api: ApiClient,
+	options: VideoOptions,
+	response: PlaybackInfoResponse,
+): StreamInfo = StreamInfo().apply {
+	val source = response.mediaSources.firstOrNull {
+		options.mediaSourceId != null && it.id == options.mediaSourceId
+	} ?: response.mediaSources.firstOrNull()
+
+	itemId = options.itemId
+	mediaSource = source
+	runTimeTicks = source?.runTimeTicks
+	playSessionId = response.playSessionId
+
+	if (source == null) return@apply
+
+	if (options.enableDirectPlay && source.supportsDirectPlay) {
+		playMethod = PlayMethod.DIRECT_PLAY
+		container = source.container
+		mediaUrl = api.videosApi.getVideoStreamUrl(
+			itemId = itemId,
+			mediaSourceId = source.id,
+			static = true,
+			tag = source.eTag,
+		)
+	} else if (options.enableDirectStream && source.supportsDirectStream) {
+		playMethod = PlayMethod.DIRECT_STREAM
+		container = source.transcodingContainer
+		mediaUrl = api.createUrl(requireNotNull(source.transcodingUrl), ignorePathParameters = true)
+	} else if (source.supportsTranscoding) {
+		playMethod = PlayMethod.TRANSCODE
+		container = source.transcodingContainer
+		mediaUrl = api.createUrl(requireNotNull(source.transcodingUrl), ignorePathParameters = true)
+	}
+}
+
+class PlaybackManager(
+	private val api: ApiClient
+) {
+	fun getVideoStreamInfo(
+		lifecycleOwner: LifecycleOwner,
+		options: VideoOptions,
+		startTimeTicks: Long,
+		callback: Response<StreamInfo>,
+	) = lifecycleOwner.lifecycleScope.launch {
+		getVideoStreamInfoInternal(options, startTimeTicks).fold(
+			onSuccess = { callback.onResponse(it) },
+			onFailure = { callback.onError(Exception(it)) },
+		)
+	}
+
+	fun changeVideoStream(
+		lifecycleOwner: LifecycleOwner,
+		stream: StreamInfo,
+		options: VideoOptions,
+		startTimeTicks: Long,
+		callback: Response<StreamInfo>
+	) = lifecycleOwner.lifecycleScope.launch {
+		if (stream.playSessionId != null && stream.playMethod != PlayMethod.DIRECT_PLAY) {
+			api.hlsSegmentApi.stopEncodingProcess(api.deviceInfo.id, stream.playSessionId)
+		}
+
+		getVideoStreamInfoInternal(options, startTimeTicks).fold(
+			onSuccess = { callback.onResponse(it) },
+			onFailure = { callback.onError(Exception(it)) },
+		)
+	}
+
+	private suspend fun getVideoStreamInfoInternal(
+		options: VideoOptions,
+		startTimeTicks: Long
+	) = runCatching {
+		val response by api.mediaInfoApi.getPostedPlaybackInfo(
+			itemId = requireNotNull(options.itemId) { "Item id cannot be null" },
+			data = PlaybackInfoDto(
+				mediaSourceId = options.mediaSourceId,
+				startTimeTicks = startTimeTicks,
+				deviceProfile = options.profile,
+				enableDirectStream = options.enableDirectStream,
+				enableDirectPlay = options.enableDirectPlay,
+				maxAudioChannels = options.maxAudioChannels,
+				audioStreamIndex = options.audioStreamIndex.takeIf { it != null && it >= 0 },
+				subtitleStreamIndex = options.subtitleStreamIndex,
+				allowVideoStreamCopy = true,
+				allowAudioStreamCopy = true,
+				autoOpenLiveStream = true,
+			)
+		)
+
+		if (response.errorCode != null) {
+			throw PlaybackException().apply {
+				errorCode = response.errorCode!!
+			}
+		}
+
+		createStreamInfo(api, options, response)
+	}
+}
