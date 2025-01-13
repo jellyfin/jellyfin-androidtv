40f5d6e4c7abad2d452ba2f8b6a3178e208b6130
Benedikt Gilbert <gilbert.benedikt@gmail.com>
2025-01-13 15:19:32 +0100

    Refined play icons

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/composable/inforow/InfoRowItem.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/composable/inforow/InfoRowItem.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
@@ -23,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.tv.material3.ProvideTextStyle
 import androidx.compose.ui.text.font.FontFamily
+import org.jellyfin.androidtv.R
 
 /**
  * A single item in the [BaseItemInfoRow].
