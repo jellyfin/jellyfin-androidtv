8f4309360b299e41258e0d11e3bc249d7a32294d
Niels van Velzen <git@ndat.nl>
2025-05-30 20:28:49 +0200

    Display cover art of currently playing music via compose

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/AudioNowPlayingFragmentHelper.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/AudioNowPlayingFragmentHelper.kt
@@ -1,42 +1,85 @@
 package org.jellyfin.androidtv.ui.playback
 
+import android.widget.ImageView
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jellyfin.androidtv.ui.AsyncImageView
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.ui.base.Icon
+import org.jellyfin.androidtv.ui.composable.AsyncImage
 import org.jellyfin.androidtv.ui.composable.LyricsDtoBox
 import org.jellyfin.androidtv.ui.composable.modifier.fadingEdges
 import org.jellyfin.androidtv.ui.composable.rememberPlayerProgress
 import org.jellyfin.androidtv.ui.composable.rememberQueueEntry
+import org.jellyfin.androidtv.util.apiclient.albumPrimaryImage
+import org.jellyfin.androidtv.util.apiclient.getUrl
+import org.jellyfin.androidtv.util.apiclient.itemImages
+import org.jellyfin.androidtv.util.apiclient.parentImages
 import org.jellyfin.playback.core.PlaybackManager
 import org.jellyfin.playback.core.model.PlayState
 import org.jellyfin.playback.jellyfin.lyrics
 import org.jellyfin.playback.jellyfin.lyricsFlow
+import org.jellyfin.playback.jellyfin.queue.baseItem
+import org.jellyfin.playback.jellyfin.queue.baseItemFlow
+import org.jellyfin.sdk.api.client.ApiClient
+import org.jellyfin.sdk.model.api.ImageType
+import org.koin.compose.koinInject
 
-fun initializeLyricsView(
-	coverView: AsyncImageView,
+fun initializePreviewView(
 	lyricsView: ComposeView,
 	playbackManager: PlaybackManager,
 ) {
 	lyricsView.setContent {
+		val api = koinInject<ApiClient>()
 		val entry by rememberQueueEntry(playbackManager)
+		val baseItem = entry?.run { baseItemFlow.collectAsState(baseItem).value }
 		val lyrics = entry?.run { lyricsFlow.collectAsState(lyrics) }?.value
+		val cover = baseItem?.itemImages[ImageType.PRIMARY] ?: baseItem?.albumPrimaryImage ?: baseItem?.parentImages[ImageType.PRIMARY]
 
-		// Animate cover view alpha
+		// Show track/album art when available and fade it out when lyrics are displayed on top
 		val coverViewAlpha by animateFloatAsState(
-			label = "CoverViewAlpha",
+			label = "coverViewAlpha",
 			targetValue = if (lyrics == null) 1f else 0.2f,
 		)
-		LaunchedEffect(coverViewAlpha) { coverView.alpha = coverViewAlpha }
+
+		AnimatedContent(cover) { cover ->
+			if (cover != null) {
+				Box(
+					modifier = Modifier
+						.wrapContentSize()
+						.clip(RoundedCornerShape(4.dp))
+						.background(Color.Black)
+				) {
+					AsyncImage(
+						url = cover.getUrl(api),
+						blurHash = cover.blurHash,
+						aspectRatio = cover.aspectRatio?.toFloat() ?: 1f,
+						scaleType = ImageView.ScaleType.CENTER_INSIDE,
+						modifier = Modifier
+							.alpha(coverViewAlpha)
+					)
+				}
+			} else if (lyrics == null) {
+				// "placeholder" image
+				Icon(painterResource(R.drawable.ic_album), contentDescription = null, tint = Color.White.copy(alpha = 0.4f))
+			}
+		}
 
 		// Display lyrics overlay
 		if (lyrics != null) {
