4fb9b7ef28c47bef923f0025ee3805621e79f06e
Niels van Velzen <git@ndat.nl>
2025-06-06 20:50:02 +0200

    Fix DestinationFragmentView not replacing existing fragment on resume

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/DestinationFragmentView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/DestinationFragmentView.kt
@@ -152,7 +152,7 @@ class DestinationFragmentView @JvmOverloads constructor(
 
 			// Attach or add next fragment
 			if (fragment.isDetached) attach(fragment)
-			else add(container.id, fragment, FRAGMENT_TAG_CONTENT)
+			else replace(container.id, fragment, FRAGMENT_TAG_CONTENT)
 		}
 
 		if (fragmentManager.isDestroyed) {
