52b15b2572dc7ce1d41008e8e991d555b91ce657
Niels van Velzen <git@ndat.nl>
2025-07-17 22:07:28 +0200

    Prefer imageVector painting for icon drawables

--- app/src/main/java/org/jellyfin/androidtv/ui/shared/toolbar/StartupToolbar.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/shared/toolbar/StartupToolbar.kt
@@ -4,8 +4,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.base.Icon
@@ -21,14 +22,14 @@ fun StartupToolbar(
 			ToolbarButtons {
 				IconButton(onClick = openHelp) {
 					Icon(
-						painter = painterResource(R.drawable.ic_help),
+						imageVector = ImageVector.vectorResource(R.drawable.ic_help),
 						contentDescription = stringResource(R.string.help),
 					)
 				}
 
 				IconButton(onClick = openSettings) {
 					Icon(
-						painter = painterResource(R.drawable.ic_settings),
+						imageVector = ImageVector.vectorResource(R.drawable.ic_settings),
 						contentDescription = stringResource(R.string.lbl_settings),
 					)
 				}
