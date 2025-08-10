47c3ab693512adaf3875302a8e7341b3636884a9
Niels van Velzen <git@ndat.nl>
2025-08-10 14:44:23 +0200

    Use toolbar clock in screensaver

--- app/src/main/java/org/jellyfin/androidtv/integration/dream/composable/DreamHeader.kt
+++ app/src/main/java/org/jellyfin/androidtv/integration/dream/composable/DreamHeader.kt
@@ -6,15 +6,10 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
-import org.jellyfin.androidtv.ui.base.Text
 import org.jellyfin.androidtv.ui.composable.modifier.overscan
-import org.jellyfin.androidtv.ui.composable.rememberCurrentTime
+import org.jellyfin.androidtv.ui.shared.toolbar.ToolbarClock
 
 @Composable
 fun DreamHeader(
@@ -32,13 +27,6 @@ fun DreamHeader(
 		modifier = Modifier
 			.align(Alignment.TopEnd),
 	) {
-		val currentTime by rememberCurrentTime()
-		Text(
-			text = currentTime,
-			style = TextStyle(
-				color = Color.White,
-				fontSize = 20.sp
-			),
-		)
+		ToolbarClock()
 	}
 }
