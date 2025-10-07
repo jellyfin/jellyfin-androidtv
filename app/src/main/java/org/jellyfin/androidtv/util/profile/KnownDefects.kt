ab473d127d72a36d6ccf1e2a47361db5b408f1ef
Niels van Velzen <git@ndat.nl>
2025-10-07 21:23:16 +0200

    Revert "Add Google TV Streamer to modelsWithDoViHdr10PlusBug"
    This reverts commit e340b7267abb0875d54c3fa6c743d4a7bb5361ef.

--- app/src/main/java/org/jellyfin/androidtv/util/profile/KnownDefects.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/profile/KnownDefects.kt
@@ -9,7 +9,6 @@ private val modelsWithDoViHdr10PlusBug = listOf(
 	"AFTKRT", // Amazon Fire TV 4K Max (2nd Gen)
 	"AFTKA", // Amazon Fire TV 4K Max (1st Gen)
 	"AFTKM", // Amazon Fire TV 4K (2nd Gen)
-	"GTVS4K",  // Google TV Streamer (4K)
 )
 
 object KnownDefects {
