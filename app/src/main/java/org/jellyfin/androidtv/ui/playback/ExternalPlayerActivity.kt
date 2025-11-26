7e95582f89c2be028d5785748ad315295b91238f
Douglas Newman <35926121+dnewman1@users.noreply.github.com>
2025-11-26 15:08:44 -0600

    Fix Vimu Player Integration (#5187)
    * fix activity result status code handling for vimu
    
    * Fixup
    
    ---------
    
    Co-authored-by: Niels van Velzen <git@ndat.nl>

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/ExternalPlayerActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/ExternalPlayerActivity.kt
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.net.toUri
 import androidx.fragment.app.FragmentActivity
@@ -62,6 +63,8 @@ class ExternalPlayerActivity : FragmentActivity() {
 		private const val API_VIMU_TITLE = "forcename"
 		private const val API_VIMU_SEEK_POSITION = "startfrom"
 		private const val API_VIMU_RESUME = "forceresume"
+		private const val API_VIMU_RESULT_ID = "net.gtvbox.videoplayer.result"
+		private const val API_VIMU_RESULT_ERROR = 4
 
 		// The extra keys used by various video players to read the end position
 		private val resultPositionExtras = arrayOf(API_MX_RESULT_POSITION, API_VLC_RESULT_POSITION)
@@ -75,12 +78,17 @@ class ExternalPlayerActivity : FragmentActivity() {
 		Timber.i("Playback finished with result code ${result.resultCode}")
 		videoQueueManager.setCurrentMediaPosition(videoQueueManager.getCurrentMediaPosition() + 1)
 
-		if (result.resultCode != RESULT_OK) {
+		if (result.isError) {
 			Toast.makeText(this, R.string.video_error_unknown_error, Toast.LENGTH_LONG).show()
 			finish()
 		} else {
 			onItemFinished(result.data)
 		}
+	}
+
+	private val ActivityResult.isError get() = when (data?.action) {
+		API_VIMU_RESULT_ID -> resultCode == API_VIMU_RESULT_ERROR
+		else -> resultCode != RESULT_OK
 	}
 
 	private var currentItem: Pair<BaseItemDto, MediaSourceInfo>? = null
