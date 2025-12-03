d69d6a29bb1d6928e2bc60ee86e236c985147d40
Niels van Velzen <git@ndat.nl>
2025-12-03 22:46:23 +0100

    Migrate telemetry settings to compose

--- app/src/main/java/org/jellyfin/androidtv/ui/settings/routes.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/settings/routes.kt
@@ -2,11 +2,14 @@ package org.jellyfin.androidtv.ui.settings
 
 import androidx.compose.runtime.Composable
 import org.jellyfin.androidtv.ui.settings.screen.SettingsMainScreen
+import org.jellyfin.androidtv.ui.settings.screen.SettingsTelemetryScreen
 
 object Routes {
 	const val MAIN = "/"
+	const val TELEMETRY = "/telemetry"
 }
 
 val routes = mapOf<String, @Composable () -> Unit>(
 	Routes.MAIN to ::SettingsMainScreen,
+	Routes.TELEMETRY to ::SettingsTelemetryScreen
 )
