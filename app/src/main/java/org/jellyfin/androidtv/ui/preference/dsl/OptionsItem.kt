b7ec938ca376213930b32db952e43af03a111e6c
Niels van Velzen <git@ndat.nl>
2026-01-03 11:34:52 +0100

    Remove old preference UI

--- app/src/main/java/org/jellyfin/androidtv/ui/preference/dsl/OptionsItem.kt
+++
@@ -1,7 +0,0 @@
-package org.jellyfin.androidtv.ui.preference.dsl
-
-import androidx.preference.PreferenceCategory
-
-interface OptionsItem {
-	fun build(category: PreferenceCategory, container: OptionsUpdateFunContainer)
-}
