87fe49ddb827660479edd30f1bd75e3ded185367
Niels van Velzen <git@ndat.nl>
2026-02-06 20:35:17 +0100

    Check for playAccess in BaseItemDto.canPlay extension
    (cherry picked from commit a3683822620a06dacfd24d5c09453d431d46ea13)

--- app/src/main/java/org/jellyfin/androidtv/util/sdk/BaseItemExtensions.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/sdk/BaseItemExtensions.kt
@@ -13,6 +13,7 @@ import org.jellyfin.sdk.model.api.BaseItemKind
 import org.jellyfin.sdk.model.api.ImageType
 import org.jellyfin.sdk.model.api.LocationType
 import org.jellyfin.sdk.model.api.PersonKind
+import org.jellyfin.sdk.model.api.PlayAccess
 import java.time.LocalDateTime
 
 fun BaseItemDto.getSeasonEpisodeName(context: Context): String {
@@ -50,6 +51,7 @@ fun BaseItemDto.getDisplayName(context: Context): String {
 
 
 fun BaseItemDto?.canPlay() = this != null
+	&& playAccess != PlayAccess.NONE
 	&& isPlaceHolder != true
 	&& (type != BaseItemKind.EPISODE || locationType != LocationType.VIRTUAL)
 	&& type != BaseItemKind.PERSON
