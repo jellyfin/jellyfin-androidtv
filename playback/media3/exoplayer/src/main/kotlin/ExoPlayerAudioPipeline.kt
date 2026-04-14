45879928709c31492a3bee5794553671bdec3abc
Niels van Velzen <git@ndat.nl>
2026-04-14 13:58:26 +0200

    Catch errors when applying gain in ExoPlayerAudioPipeline

--- playback/media3/exoplayer/src/main/kotlin/ExoPlayerAudioPipeline.kt
+++ playback/media3/exoplayer/src/main/kotlin/ExoPlayerAudioPipeline.kt
@@ -36,9 +36,17 @@ class ExoPlayerAudioPipeline {
 			?.times(100f)
 			// Round to integer
 			?.toInt()
+			// Ignore if zero (so the enhancer will be disabled)
+			?.takeIf { it != 0 }
 
 		Timber.d("Applying gain (targetGain=$targetGain)")
-		loudnessEnhancer?.setEnabled(targetGain != null)
-		loudnessEnhancer?.setTargetGain(targetGain ?: 0)
+		runCatching {
+			loudnessEnhancer?.setTargetGain(targetGain ?: 0)
+		}.onSuccess {
+			loudnessEnhancer?.setEnabled(targetGain != null)
+		}.onFailure { error ->
+			Timber.e(error, "Failed to apply gain of $targetGain")
+			loudnessEnhancer?.setEnabled(false)
+		}
 	}
 }
