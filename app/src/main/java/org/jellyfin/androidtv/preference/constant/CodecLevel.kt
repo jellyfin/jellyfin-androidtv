276113bac62af6e17f6e491b6e7be307edea77f7
Niels van Velzen <git@ndat.nl>
2026-04-18 13:28:28 +0200

    Revert "Replace magic numbers with CodecProfileLevel constants"
    This reverts commit 131ebd4defce13e4c41c34da4f6e3229dce17a06.

--- app/src/main/java/org/jellyfin/androidtv/preference/constant/CodecLevel.kt
+++ app/src/main/java/org/jellyfin/androidtv/preference/constant/CodecLevel.kt
@@ -1,6 +1,5 @@
 package org.jellyfin.androidtv.preference.constant
 
-import android.media.MediaCodecInfo.CodecProfileLevel
 import org.jellyfin.androidtv.R
 import org.jellyfin.preference.PreferenceEnum
 
@@ -13,15 +12,15 @@ enum class AVCLevel(
 	val level: Int,
 ) : PreferenceEnum {
 	AUTO(R.string.auto, -1),
-	LEVEL_6_2(R.string.codec_level_6_2, CodecProfileLevel.AVCLevel62),
-	LEVEL_6_1(R.string.codec_level_6_1, CodecProfileLevel.AVCLevel61),
-	LEVEL_6_0(R.string.codec_level_6_0, CodecProfileLevel.AVCLevel6),
-	LEVEL_5_2(R.string.codec_level_5_2, CodecProfileLevel.AVCLevel52),
-	LEVEL_5_1(R.string.codec_level_5_1, CodecProfileLevel.AVCLevel51),
-	LEVEL_5_0(R.string.codec_level_5_0, CodecProfileLevel.AVCLevel5),
-	LEVEL_4_2(R.string.codec_level_4_2, CodecProfileLevel.AVCLevel42),
-	LEVEL_4_1(R.string.codec_level_4_1, CodecProfileLevel.AVCLevel41),
-	LEVEL_4_0(R.string.codec_level_4_0, CodecProfileLevel.AVCLevel4),
+	LEVEL_6_2(R.string.codec_level_6_2, 62),
+	LEVEL_6_1(R.string.codec_level_6_1, 61),
+	LEVEL_6_0(R.string.codec_level_6_0, 60),
+	LEVEL_5_2(R.string.codec_level_5_2, 52),
+	LEVEL_5_1(R.string.codec_level_5_1, 51),
+	LEVEL_5_0(R.string.codec_level_5_0, 50),
+	LEVEL_4_2(R.string.codec_level_4_2, 42),
+	LEVEL_4_1(R.string.codec_level_4_1, 41),
+	LEVEL_4_0(R.string.codec_level_4_0, 40),
 }
 
 /**
@@ -33,12 +32,12 @@ enum class HEVCLevel(
 	val level: Int,
 ) : PreferenceEnum {
 	AUTO(R.string.auto, -1),
-	LEVEL_6_2(R.string.codec_level_6_2, CodecProfileLevel.HEVCMainTierLevel62),
-	LEVEL_6_1(R.string.codec_level_6_1, CodecProfileLevel.HEVCMainTierLevel61),
-	LEVEL_6_0(R.string.codec_level_6_0, CodecProfileLevel.HEVCMainTierLevel6),
-	LEVEL_5_2(R.string.codec_level_5_2, CodecProfileLevel.HEVCMainTierLevel52),
-	LEVEL_5_1(R.string.codec_level_5_1, CodecProfileLevel.HEVCMainTierLevel51),
-	LEVEL_5_0(R.string.codec_level_5_0, CodecProfileLevel.HEVCMainTierLevel5),
-	LEVEL_4_1(R.string.codec_level_4_1, CodecProfileLevel.HEVCMainTierLevel41),
-	LEVEL_4_0(R.string.codec_level_4_0, CodecProfileLevel.HEVCMainTierLevel4),
+	LEVEL_6_2(R.string.codec_level_6_2, 186),
+	LEVEL_6_1(R.string.codec_level_6_1, 183),
+	LEVEL_6_0(R.string.codec_level_6_0, 180),
+	LEVEL_5_2(R.string.codec_level_5_2, 156),
+	LEVEL_5_1(R.string.codec_level_5_1, 153),
+	LEVEL_5_0(R.string.codec_level_5_0, 150),
+	LEVEL_4_1(R.string.codec_level_4_1, 123),
+	LEVEL_4_0(R.string.codec_level_4_0, 120),
 }
