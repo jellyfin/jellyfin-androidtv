03fdecb0549181389c1562e1854b203d4c5c3ec3
Charlie Harper <144284500+Charlie284@users.noreply.github.com>
2026-07-23 22:44:14 -0500

    Add TV show sort by latest episode

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/BrowseGridFragment.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/BrowseGridFragment.java
@@ -158,15 +158,19 @@ public class BrowseGridFragment extends Fragment implements View.OnKeyListener {
 
         sortOptions = new HashMap<>();
         {
+            boolean isTvShows = mFolder.getCollectionType() == CollectionType.TVSHOWS;
+            int dateAddedLabelRes = isTvShows ? R.string.lbl_date_show_added : R.string.lbl_date_added;
+
             sortOptions.put(0, new SortOption(getString(R.string.lbl_name), ItemSortBy.SORT_NAME, SortOrder.ASCENDING));
-            sortOptions.put(1, new SortOption(getString(R.string.lbl_date_added), ItemSortBy.DATE_CREATED, SortOrder.DESCENDING));
+            sortOptions.put(1, new SortOption(getString(dateAddedLabelRes), ItemSortBy.DATE_CREATED, SortOrder.DESCENDING));
             sortOptions.put(2, new SortOption(getString(R.string.lbl_premier_date), ItemSortBy.PREMIERE_DATE, SortOrder.DESCENDING));
             sortOptions.put(3, new SortOption(getString(R.string.lbl_rating), ItemSortBy.OFFICIAL_RATING, SortOrder.ASCENDING));
             sortOptions.put(4, new SortOption(getString(R.string.lbl_community_rating), ItemSortBy.COMMUNITY_RATING, SortOrder.DESCENDING));
             sortOptions.put(5, new SortOption(getString(R.string.lbl_critic_rating), ItemSortBy.CRITIC_RATING, SortOrder.DESCENDING));
 
-            if (mFolder.getCollectionType() == CollectionType.TVSHOWS) {
+            if (isTvShows) {
                 sortOptions.put(6, new SortOption(getString(R.string.lbl_last_played), ItemSortBy.SERIES_DATE_PLAYED, SortOrder.DESCENDING));
+                sortOptions.put(7, new SortOption(getString(R.string.lbl_date_episode_added), ItemSortBy.DATE_LAST_CONTENT_ADDED, SortOrder.DESCENDING));
             } else {
                 sortOptions.put(6, new SortOption(getString(R.string.lbl_last_played), ItemSortBy.DATE_PLAYED, SortOrder.DESCENDING));
             }
