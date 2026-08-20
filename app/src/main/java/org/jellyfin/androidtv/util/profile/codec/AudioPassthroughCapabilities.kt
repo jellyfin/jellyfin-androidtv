2d59711c4fb136f96526c2fd371253100e2078a5
Blackstorm <blackstorm2282@gmail.com>
2026-08-20 17:00:34 +0200

    - transform main class to function

--- app/src/main/java/org/jellyfin/androidtv/util/profile/codec/AudioPassthroughCapabilities.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/profile/codec/AudioPassthroughCapabilities.kt
@@ -9,33 +9,29 @@ import androidx.media3.common.Format
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.audio.AudioCapabilities
 
-class AudioPassthroughCapabilities (
-	private val context: Context
-){
-	@OptIn(UnstableApi::class)
-	fun isPassthroughAudioAvailable(mimetype: String): Boolean {
-		// Def audio attributes
-		val audioAttributes = AudioAttributes.Builder()
-			.setUsage(C.USAGE_MEDIA)
-			.setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
-			.build()
-		// Get audio capabilities
-		val audioCapabilities = AudioCapabilities.getCapabilities(
-			context,
-			audioAttributes,
-			null,
-			listOf(
-				AudioFormat.CHANNEL_OUT_STEREO,
-				AudioFormat.CHANNEL_OUT_5POINT1
-			)
+@OptIn(UnstableApi::class)
+fun isPassthroughAudioAvailable(context: Context, mimetype: String): Boolean {
+	// Def audio attributes
+	val audioAttributes = AudioAttributes.Builder()
+		.setUsage(C.USAGE_MEDIA)
+		.setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+		.build()
+	// Get audio capabilities
+	val audioCapabilities = AudioCapabilities.getCapabilities(
+		context,
+		audioAttributes,
+		null,
+		listOf(
+			AudioFormat.CHANNEL_OUT_STEREO,
+			AudioFormat.CHANNEL_OUT_5POINT1
 		)
-		// Set audio format for a passthrough 2.0 audio codec check
-		val format = Format.Builder()
-			.setSampleMimeType(mimetype)
-			.setChannelCount(Integer.bitCount(AudioFormat.CHANNEL_OUT_STEREO))
-			.setSampleRate(Format.NO_VALUE)
-			.build()
-		// Test Passthrough Direct Playback
-		return audioCapabilities.isPassthroughPlaybackSupported(format, audioAttributes)
-	}
+	)
+	// Set audio format for a passthrough 2.0 audio codec check
+	val format = Format.Builder()
+		.setSampleMimeType(mimetype)
+		.setChannelCount(Integer.bitCount(AudioFormat.CHANNEL_OUT_STEREO))
+		.setSampleRate(Format.NO_VALUE)
+		.build()
+	// Test Passthrough Direct Playback
+	return audioCapabilities.isPassthroughPlaybackSupported(format, audioAttributes)
 }
