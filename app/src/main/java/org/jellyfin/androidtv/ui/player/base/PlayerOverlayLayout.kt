76afbac950ae9f234b478b9a7120463b13684e18
Niels van Velzen <git@ndat.nl>
2026-08-22 11:04:36 +0200

    Press up to close player overlay

--- app/src/main/java/org/jellyfin/androidtv/ui/player/base/PlayerOverlayLayout.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/player/base/PlayerOverlayLayout.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,18 +21,24 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jellyfin.androidtv.ui.base.JellyfinTheme
 import org.jellyfin.androidtv.ui.composable.modifier.overscan
+import timber.log.Timber
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -46,10 +53,12 @@ fun PlayerOverlayLayout(
 		.fillMaxSize()
 		.focusable()
 		.onPreviewKeyEvent {
+			// Reset hide timer on key presses
 			if (visibilityState.visible) visibilityState.show()
-			false
-		}
-		.onKeyEvent {
+
+			// Otherwise, only act on key down
+			if (it.type != KeyEventType.KeyDown) return@onPreviewKeyEvent false
+
 			if (it.key == Key.Back && visibilityState.visible) {
 				visibilityState.hide()
 				true
@@ -96,6 +105,14 @@ fun PlayerOverlayLayout(
 			enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
 			exit = slideOutVertically(targetOffsetY = { it }) + fadeOut(),
 		) {
+			// Focus trap to detect when controls need to be closed by moving the focus up
+			Box(
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(0.dp)
+					.focusTarget()
+			)
+
 			Box(
 				contentAlignment = Alignment.BottomCenter,
 				modifier = Modifier
@@ -109,7 +126,17 @@ fun PlayerOverlayLayout(
 							)
 						)
 					)
-					.overscan(),
+					.overscan()
+					.focusProperties {
+						// Hide overlay when focus is moved out by going up
+						onExit = {
+							if (requestedFocusDirection == FocusDirection.Up) {
+								Timber.i("Hide reason: focus moved up")
+								visibilityState.hide()
+								cancelFocusChange()
+							}
+						}
+					},
 			) {
 				JellyfinTheme(
 					colorScheme = JellyfinTheme.colorScheme.copy(
@@ -128,21 +155,20 @@ data class PlayerOverlayVisibilityState(
 
 	val toggle: () -> Unit,
 	val show: () -> Unit,
-	val hide: () -> Unit
+	val hide: () -> Unit,
 )
 
 @Composable
 fun rememberPlayerOverlayVisibility(
-	timeout: Duration = 5.seconds
+	timeout: Duration = 5.seconds,
 ): PlayerOverlayVisibilityState {
 	val scope = rememberCoroutineScope()
 	var timerVisible by remember { mutableStateOf(false) }
 	var timerJob by remember { mutableStateOf<Job?>(null) }
-	var visible = timerVisible
 
 	fun show() {
-		timerVisible = true
 		timerJob?.cancel()
+		timerVisible = true
 		timerJob = scope.launch {
 			delay(timeout)
 			timerVisible = false
@@ -150,9 +176,9 @@ fun rememberPlayerOverlayVisibility(
 	}
 
 	fun hide() {
-		timerVisible = false
 		timerJob?.cancel()
 		timerJob = null
+		timerVisible = false
 	}
 
 	fun toggle() {
@@ -163,7 +189,7 @@ fun rememberPlayerOverlayVisibility(
 	// Force visibility when not the active window, reset timer when it changes
 	// to make sure popups keep the overlay visible
 	val windowInfo = LocalWindowInfo.current
-	visible = visible || !windowInfo.isWindowFocused
+	val visible = timerVisible || !windowInfo.isWindowFocused
 
 	var previousIsWindowFocused by remember { mutableStateOf(windowInfo.isWindowFocused) }
 	LaunchedEffect(windowInfo.isWindowFocused) {
@@ -175,6 +201,6 @@ fun rememberPlayerOverlayVisibility(
 		visible = visible,
 		toggle = ::toggle,
 		show = ::show,
-		hide = ::hide
+		hide = ::hide,
 	)
 }
