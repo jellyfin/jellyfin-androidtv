1aa23abf6fbf1dc813aa3b545aaceacd66cc5698
clowncracker <49218426+clowncracker@users.noreply.github.com>
2026-09-03 17:11:36 -0700

    Update SettingsPlaybackHdrOverrideScreen.kt
    Set focusKeys in HDR override screens

--- app/src/main/java/org/jellyfin/androidtv/ui/settings/screen/playback/SettingsPlaybackHdrOverrideScreen.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/settings/screen/playback/SettingsPlaybackHdrOverrideScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.preference.UserPreferences
@@ -14,6 +15,7 @@ import org.jellyfin.androidtv.ui.base.form.RadioButton
 import org.jellyfin.androidtv.ui.base.list.ListButton
 import org.jellyfin.androidtv.ui.base.list.ListSection
 import org.jellyfin.androidtv.ui.navigation.LocalRouter
+import org.jellyfin.androidtv.ui.navigation.focus.focusKey
 import org.jellyfin.androidtv.ui.settings.compat.rememberPreference
 import org.jellyfin.androidtv.ui.settings.composable.SettingsColumn
 import org.koin.compose.koinInject
@@ -42,7 +44,8 @@ fun SettingsPlaybackHdrOverrideScreen(
 				onClick = {
 					mode = entry
 					router.back()
-				}
+				},
+				modifier = Modifier.focusKey("hdr_override_${format.name}_${entry.name}")
 			)
 		}
 	}
